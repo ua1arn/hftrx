5f0b688c7fbee72c4530a0a2e21164e61d0b40f1
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-01-22 15:30:00 +0000

    custom configuration (RA9W) updated - управление передачей с UAB AUDIO кнопкой
    git-svn-id: http://dolphin/svn/svn2/trunk@64 953d4507-ba89-49e6-903b-66a71853bf49

--- boards/arm_stm32f4xx_tqfp144_cpustyle_storch_v3.h
+++ boards/arm_stm32f4xx_tqfp144_cpustyle_storch_v3.h
@@ -58,9 +58,7 @@
 #define WITHNMEA_USART2		1	/* порт подключения GPS/GLONASS */
 
 #define WITHUSBUAC		1	/* использовать виртуальную звуковую плату на USB соединении */
-#if defined (WITHUSBUAC3) && (WITHUSBUAC3 != 0)
-	#define WITHUSBUAC3		1	/* формируются три канала передачи звука */
-#endif /* defined (WITHUSBUAC3) && (WITHUSBUAC3 != 0) */
+#define WITHUSBUAC3		1	/* формируются три канала передачи звука */
 //#define WITHUABUACOUTAUDIO48MONO	1	/* для уменьшения размера буферов в endpoints */
 
 #define WITHUSBCDC		1	/* ACM использовать виртуальный последовательный порт на USB соединении */
