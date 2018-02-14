592d4f3907799c0d8564c607210de0d519b06db1
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-02-14 23:23:46 +0000

    comments
    git-svn-id: http://dolphin/svn/svn2/trunk@146 953d4507-ba89-49e6-903b-66a71853bf49

--- s1d13781.h
+++ s1d13781.h
@@ -67,35 +67,36 @@
 #if (S1D_DISPLAY_BPP == 8) || LCDMODE_LTDC_L8
 
 
-typedef uint_fast8_t COLOR_T;
-typedef uint8_t PACKEDCOLOR_T;
+	typedef uint_fast8_t COLOR_T;
+	typedef uint8_t PACKEDCOLOR_T;
 
-// RRRGGGBB
+	// RRRGGGBB
 
-#define TFTRGB(red, green, blue) \
-	(  (unsigned char) \
-		(	\
-			(((red) >> 0) & 0xe0)  | \
-			(((green) >> 3) & 0x1c) | \
-			(((blue) >> 6) & 0x03) \
-		) \
-	)
+	#define TFTRGB(red, green, blue) \
+		(  (unsigned char) \
+			(	\
+				(((red) >> 0) & 0xe0)  | \
+				(((green) >> 3) & 0x1c) | \
+				(((blue) >> 6) & 0x03) \
+			) \
+		)
 
 
 #elif S1D_DISPLAY_BPP == 16 || ! LCDMODE_LTDC_L8
 
-typedef uint_fast16_t COLOR_T;
-typedef uint16_t PACKEDCOLOR_T;
+	//#define LCDMODE_RGB565 1
+	typedef uint_fast16_t COLOR_T;
+	typedef uint16_t PACKEDCOLOR_T;
 
-// RRRR.RGGG.GGGB.BBBB
-#define TFTRGB(red, green, blue) \
-	(  (uint_fast16_t) \
-		(	\
-			(((uint_fast16_t) (red) << 8) &   0xf800)  | \
-			(((uint_fast16_t) (green) << 3) & 0x07e0) | \
-			(((uint_fast16_t) (blue) >> 3) &  0x001f) \
-		) \
-	)
+	// RRRR.RGGG.GGGB.BBBB
+	#define TFTRGB(red, green, blue) \
+		(  (uint_fast16_t) \
+			(	\
+				(((uint_fast16_t) (red) << 8) &   0xf800)  | \
+				(((uint_fast16_t) (green) << 3) & 0x07e0) | \
+				(((uint_fast16_t) (blue) >> 3) &  0x001f) \
+			) \
+		)
 
 #elif S1D_DISPLAY_BPP == 24
 
