ca72a7c00ee26db8e1a3824e69156ad59892fdd6
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-04-03 20:30:26 +0000

    revert
    git-svn-id: http://dolphin/svn/svn2/trunk@266 953d4507-ba89-49e6-903b-66a71853bf49

--- tools/convert_rbf_to_c.cmd
+++ tools/convert_rbf_to_c.cmd
@@ -1,11 +1,11 @@
 rbf2c.exe ..\fpga_q131\output_files\t1_v3.rbf ..\rbf\rbfimage_v3.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v3_pll.rbf ..\rbf\rbfimage_v3_pll.h
-rbf2c.exe ..\fpga_q131_copy\output_files\t1_v4.rbf ..\rbf\rbfimage_v4.h
+rbf2c.exe ..\fpga_q131\output_files\t1_v4.rbf ..\rbf\rbfimage_v4.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v4_pll.rbf ..\rbf\rbfimage_v4_pll.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v5_2ch.rbf ..\rbf\rbfimage_v5_2ch.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v5_2ch_pll.rbf ..\rbf\rbfimage_v5_2ch_pll.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v6_2ch.rbf ..\rbf\rbfimage_v6_2ch.h
-rbf2c.exe ..\fpga_q131_copy\output_files\t1_v7_1ch.rbf ..\rbf\rbfimage_v7_1ch.h
+rbf2c.exe ..\fpga_q131\output_files\t1_v7_1ch.rbf ..\rbf\rbfimage_v7_1ch.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v7_2ch.rbf ..\rbf\rbfimage_v7_2ch.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v7a_2ch.rbf ..\rbf\rbfimage_v7a_2ch.h
 rbf2c.exe ..\fpga_q131\output_files\t1_v7_oleg4z.rbf ..\rbf\rbfimage_oleg4z.h
