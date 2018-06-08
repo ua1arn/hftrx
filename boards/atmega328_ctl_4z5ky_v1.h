4b277ba11222df86e1572a925292e757d99cb61a
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-06-08 11:39:11 +0000

    Custom configuration update
    git-svn-id: http://dolphin/svn/svn2/trunk@453 953d4507-ba89-49e6-903b-66a71853bf49

--- boards/atmega328_ctl_4z5ky_v1.h
+++ boards/atmega328_ctl_4z5ky_v1.h
@@ -22,9 +22,9 @@
 	// Выбор диапазонных фильтров
 	#define BANDSELSTYLERE_LOCONV32M_NLB	1	/* Down-conversion with working band 1.6..32 MHz */
 	//#define DIRECT_125M0_X1		1
-	//#define DIRECT_80M0_X1		1
+	#define DIRECT_80M0_X1		1
 	//#define DIRECT_50M0_X1		1
-	#define DIRECT_75M0_X1		1
+	//#define DIRECT_75M0_X1		1
 	//#define DIRECT_66M0_X1		1
 	//#define DIRECT_48M0_X1		1
 	//#define DIRECT_44M0_X1		1
@@ -71,7 +71,8 @@
 	#define FQMODEL_DCTRX		1	// прямое преобразование
 	// Выбор диапазонных фильтров
 	#define BANDSELSTYLERE_LOCONV32M_NLB	1	/* Down-conversion with working band 1.6..32 MHz */
-	#define DIRECT_75M0_X1		1
+	#define DIRECT_80M0_X1		1
+	//#define DIRECT_75M0_X1		1
 	//#define DIRECT_50M0_X1		1
 	//#define DIRECT_44M0_X1		1
 	//#define DIRECT_48M0_X1		1
