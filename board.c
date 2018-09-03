c03065a35d2c66f89170fb5f385e99877d4d9137
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-09-03 19:11:06 +0000

    oleg4z: прием WFM
    git-svn-id: http://dolphin/svn/svn2/trunk@617 953d4507-ba89-49e6-903b-66a71853bf49

--- board.c
+++ board.c
@@ -6004,7 +6004,7 @@ static void board_fpga_loader_PS(void)
 	#elif CTLSTYLE_STORCH_V2 && (DDS1_CLK_MUL == 1)
 		#include "rbf/rbfimage_v7a_2ch.h"	// same as CTLSTYLE_RAVENDSP_V7
 	#elif CTLSTYLE_STORCH_V3 && ! WITHUSEDUALWATCH && (DDS1_CLK_MUL == 1)
-		#include "rbf/rbfimage_oleg4z.h"	// same as CTLSTYLE_RAVENDSP_V7, 1 RX & WFM
+		#include "rbf/rbfimage_v7_1ch.h"	//
 	#elif CTLSTYLE_STORCH_V3 && (DDS1_CLK_MUL == 1)
 		#include "rbf/rbfimage_v7a_2ch.h"	// same as CTLSTYLE_RAVENDSP_V7
 	#elif CTLSTYLE_STORCH_V4 && ! WITHUSEDUALWATCH && (DDS1_CLK_MUL == 1)	// modem v2
