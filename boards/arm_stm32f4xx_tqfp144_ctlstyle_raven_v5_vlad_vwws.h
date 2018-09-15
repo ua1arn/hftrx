79b7f8f8324fcdc1048b39b22b4709ca19bf056a
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-09-15 07:03:43 +0000

    custom configurations update
    git-svn-id: http://dolphin/svn/svn2/trunk@630 953d4507-ba89-49e6-903b-66a71853bf49

--- boards/arm_stm32f4xx_tqfp144_ctlstyle_raven_v5_vlad_vwws.h
+++ boards/arm_stm32f4xx_tqfp144_ctlstyle_raven_v5_vlad_vwws.h
@@ -176,7 +176,7 @@
 	//#define LCDMODE_LTDC	1		/* Use STM32F4xxx with LCD-TFT Controller (LTDC), also need LCDMODE_ILI9341 */
 	//#define LCDMODE_LTDC_L8	1	/* используетс€ 8 бит на пиксель представление экрана. »наче - 16 бит. */
 	//#define LCDMODE_WH2002	1	/* тип примен€емого индикатора 20*2, возможно вместе с LCDMODE_HARD_SPI */
-	//#define LCDMODE_WH1602	1	/* тип примен€емого индикатора 16*2 */
+	#define LCDMODE_WH1602	1	/* тип примен€емого индикатора 16*2 */
 	//#define LCDMODE_WH1604	1	/* тип примен€емого индикатора 16*4 */
 	//#define LCDMODE_WH2004	1	/* тип примен€емого индикатора 20*4 */
 	//#define LCDMODE_RDX0077	1	/* »ндикатор 128*64 с контроллером UC1601.  */
@@ -196,9 +196,9 @@
 	//#define LCDMODE_ILI9163_TOPDOWN	1	/* LCDMODE_ILI9163 - перевернуть изображение (дл€ выводов справа, лента диспле€ слева) */
 	//#define LCDMODE_L2F50	1	/* »ндикатор 176*132 с контроллером Epson L2F50126 */
 	//#define LCDMODE_L2F50_TOPDOWN	1	/* ѕереворот изображени € в случае LCDMODE_L2F50 */
-	#define LCDMODE_S1D13781	1	/* »нндикатор 480*272 с контроллером Epson S1D13781 */
+	//#define LCDMODE_S1D13781	1	/* »нндикатор 480*272 с контроллером Epson S1D13781 */
 	//#define LCDMODE_S1D13781_TOPDOWN	1	/* LCDMODE_S1D13781 - перевернуть изображение */
-	#define LCDMODE_S1D13781_REFOSC_MHZ	50	/* „астота генератора, установленного на контроллере диспле€ */
+	//#define LCDMODE_S1D13781_REFOSC_MHZ	50	/* „астота генератора, установленного на контроллере диспле€ */
 	//#define LCDMODE_ILI9225	1	/* »ндикатор 220*176 SF-TC220H-9223A-N_IC_ILI9225C_2011-01-15 с контроллером ILI9225— */
 	//#define LCDMODE_ILI9225_TOPDOWN	1	/* LCDMODE_ILI9225 - перевернуть изображение (дл€ выводов слева от экрана) */
 	//#define LCDMODE_UC1608	1		/* »ндикатор 240*128 с контроллером UC1608.- монохромный */
