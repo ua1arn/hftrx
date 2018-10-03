ae9ff23fad8e69fe69d439e68de1060cdac85613
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-10-03 23:27:55 +0000

    компиляция редко используемых конфигураций
    git-svn-id: http://dolphin/svn/svn2/trunk@714 953d4507-ba89-49e6-903b-66a71853bf49

--- uc1608.h
+++ uc1608.h
@@ -21,9 +21,6 @@
 	typedef uint_fast8_t COLOR_T;	/* цвета не поддерживаются - заглушка */
 	typedef uint8_t PACKEDCOLOR_T;	/* цвета не поддерживаются - заглушка */
 
-	#define TFTRGB565 TFTRGB
-	typedef uint_fast8_t COLOR565_T;	/* цвета не поддерживаются - заглушка */
-
 
 #endif /* LCDMODE_UC1608 */
 
