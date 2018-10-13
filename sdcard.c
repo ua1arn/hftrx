989f54642923a4fd925d83516ab7dcf8b7fb5cc4
Genadi V. Zawidowski <mgs2001@mail.ru>
2018-10-13 20:07:36 +0000

    SDCARD: STM32H7xx: работает чтение и запись по одному сектору
    git-svn-id: http://dolphin/svn/svn2/trunk@756 953d4507-ba89-49e6-903b-66a71853bf49

--- sdcard.c
+++ sdcard.c
@@ -2022,7 +2022,115 @@ static uint_fast8_t sdhost_short_acmd_resp_R3(uint_fast8_t cmd, uint_fast32_t ar
 	return sdhost_get_R3(resp);
 }
 
+///////////////////////////
 
+#if 0
+
+#define SD_CMD_NO_RESP (SDMMC_CMD_WAITRESP_0 * 0)
+#define SD_CMD_SHORT_RESP (SDMMC_CMD_WAITRESP_0 * 1)
+#define SD_CMD_LONG_RESP (SDMMC_CMD_WAITRESP_0 * 3)
+
+#define SD_OK        0
+#define SD_ERROR     1
+
+static uint32_t SDSendCommand(uint32_t cmd, uint32_t arg, uint32_t rsp)
+{
+   PRINTF("\r\nCMD%d ARG=%08x  ", cmd, arg);
+   SDMMC1->ARG = arg;
+   //Clear interrupt flags, unmask interrupts according to cmd response
+   SDMMC1->ICR = (SDMMC_ICR_CMDRENDC | SDMMC_ICR_CMDSENTC | SDMMC_ICR_CCRCFAILC | SDMMC_ICR_CTIMEOUTC);
+   if(rsp == SD_CMD_NO_RESP)
+   {
+      //No response wait for CMDSENT interrupt
+      SDMMC1->MASK |= (SDMMC_MASK_CMDSENTIE | SDMMC_MASK_CCRCFAILIE | SDMMC_MASK_CTIMEOUTIE);
+   }
+   else 
+   {
+      //Short or long response wait for CMDREND interrupt
+      SDMMC1->MASK |= (SDMMC_MASK_CMDRENDIE | SDMMC_MASK_CCRCFAILIE | SDMMC_MASK_CTIMEOUTIE);
+   }
+   SDMMC1->CMD = SDMMC_CMD_CPSMEN | rsp | cmd;
+   if(WaitEvents(EV_SD_READY | EV_SD_ERROR, WAIT_ANY) != EV_SD_READY)
+   { 
+      PRINTF("E1(cmd=%d arg=%x STA=%x)  ",cmd,arg,SDMMC1->STA);
+      return SD_ERROR; 
+   }
+   //ASSERT(WaitEvents(EV_SD_READY | EV_SD_ERROR, WAIT_ANY) == EV_SD_READY);
+   //Check for the correct response (long response and OCR response will result in 0x3F value in RESPCMD)
+   if(rsp == SD_CMD_SHORT_RESP && SDMMC1->RESPCMD != (cmd & SDMMC_CMD_CMDINDEX))
+   {
+      PRINTF("E2(cmd=%d arg=%x STA=%x)  ",cmd,arg,SDMMC1->STA);
+      return SD_ERROR;
+   }
+   //ASSERT(!(rsp == SD_CMD_SHORT_RESP && SDMMC1->RESPCMD != cmd));
+   PRINTF("OK");
+   return SD_OK;
+}
+
+static uint32_t SDWriteBlock(uint32_t address, const void* buffer, uint32_t size)
+{
+
+	//if(SDCardState == 0)return RES_NOTRDY;
+   //Wait until card is ready for the data operations
+   for(;;)
+   {
+      //Make 10 tries to get card status before fail
+      for(int t = 0; SDSendCommand(13, sdhost_sdcard_RCA << 16, SD_CMD_SHORT_RESP); t++)
+      {
+         if(t > 10)return RES_NOTRDY;
+      }
+      if((SDMMC1->RESP1 & (SD_STAT_CURRENT_STATE|SD_STAT_READY_FOR_DATA)) == (SD_STAT_CURRENT_STATE_TRAN|SD_STAT_READY_FOR_DATA))break;
+      local_delay_ms((1));
+   }
+
+   PRINTF(" WR1:%x ", SDMMC1->RESP1);
+ 	arm_hardware_flush_invalidate((uintptr_t) buffer, 512 * size);	// Сейчас эту память будем записывать по DMA, потом содержимое не требуется
+  //SCB_CleanDCache_by_Addr(buffer, size*512);
+
+   //Program data length register
+   SDMMC1->DLEN = size * 512;
+   
+   //Clean DPSM interrupt flags and enable DPSM interrupts
+   SDMMC1->ICR = (SDMMC_ICR_DBCKENDC | SDMMC_ICR_DATAENDC | SDMMC_ICR_DCRCFAILC | SDMMC_ICR_DTIMEOUTC);
+   SDMMC1->MASK |= (SDMMC_MASK_DATAENDIE | SDMMC_MASK_DCRCFAILIE | SDMMC_MASK_DTIMEOUTIE);
+
+   SDMMC1->DTIMER = 12000000 /*0.5sec timeout*/;
+
+   //Program data control register
+   SDMMC1->DCTRL = SDMMC_DCTRL_DBLOCKSIZE_0 * 9 /*512 bytes block*/ |
+                     SDMMC_DCTRL_DTDIR * 0 /* From controller to card*/; /*|
+                     SDMMC_DCTRL_DTEN;*/
+
+   //Program SDMMC1 DMA
+   SDMMC1->IDMABASE0 = (uintptr_t) buffer;
+   SDMMC1->IDMACTRL |= SDMMC_IDMA_IDMAEN;
+
+   
+   uint8_t cmd = (size == 1) ? 24 /*CMD24 single block write*/: 25 /*CMD25 multiple blocks write*/;
+   if(SDSendCommand(cmd | SDMMC_CMD_CMDTRANS, address, SD_CMD_SHORT_RESP))
+   {
+      PRINTF("CMD%d failed!", cmd);
+      //turn off DMA
+      return RES_ERROR;
+   }
+
+
+   //Wait for data transfer finish
+   WaitEvents(EV_SD_DATA, WAIT_ANY);
+   SDMMC1->IDMACTRL &= ~SDMMC_IDMA_IDMAEN;   
+   if(size > 1)
+   {
+      //Send CMD12 STOP command in case of multiple block transfer      
+      if(SDSendCommand(12, 0, SD_CMD_SHORT_RESP))return RES_ERROR;
+   }
+   //Do we need to check FIFOCNT and wait until it is 0?
+   //PRINTF(" FIFO:%d WSTA:%x ", SDMMC1->FIFOCNT, SDMMC1->STA);
+   
+   if((SDMMC1->STA & (SDMMC_STA_DATAEND | SDMMC_STA_DCRCFAIL | SDMMC_STA_DTIMEOUT)) == SDMMC_STA_DATAEND)return RES_OK;
+   return RES_ERROR;
+}
+
+#endif
 // write a size Byte big block beginning at the address.
 static 
 DRESULT SD_disk_write(
@@ -2032,6 +2140,7 @@ DRESULT SD_disk_write(
 	UINT count			/* Number of sectors to write */
 	)
 {
+	//return SDWriteBlock(sector, buff, count);
 	//if ((uintptr_t) buff & 0x1F)
 	//{
 	//	PRINTF(PSTR("SD_disk_write: unalligned: buff=%p, sector=%lu, count=%lu\n"), buff, (unsigned long) sector, (unsigned long) count);
@@ -2259,7 +2368,7 @@ DRESULT SD_disk_read(
 	}
 	else
 	{
-		PRINTF(PSTR("read multiple blocks: count=%d\n"), count);
+		//PRINTF(PSTR("read multiple blocks: count=%d\n"), count);
 		// read multiple blocks
 		if (sdhost_use_cmd23 != 0)
 		{
@@ -2682,6 +2791,7 @@ DRESULT SD_Get_Sector_Count (
 	DWORD  *buff	/* Data buffer to store read data */
 	)
 {
+	//PRINTF(PSTR("SD_Get_Sector_Count: drv=%d\n"), (int) drv);
 	* buff = SD_ReadCardSize() / FF_MAX_SS;
 	return RES_OK;
 }
