8ff7a04901dfc2819cf9aff9b0aa9f3dde1ccf6a
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-03-23 12:32:48 +0300

    more safe macro

--- pio.c
+++ pio.c
@@ -98,9 +98,6 @@ power2(uint_fast16_t v)
 }
 #endif /* CPUSTYLE_STM32F */
 
-
-
-
 #if CPUSTYLE_R7S721
 
 	// Port Register (Pn)
@@ -109,14 +106,16 @@ power2(uint_fast16_t v)
 	// Port Register (Pn)
 	//GPIO.P7 |= (1U << 1);
 
-#define r7s721_pio_inputs(n, ipins) do { \
+#define r7s721_pio_inputs(n, pins) do { \
+		const portholder_t ipins = (pins); \
 		GPIO.PIPC ## n &= ~ (ipins);	/* Port IP Control Register: 0 - direction control from PMn, 1 - from alternative function */ \
 		GPIO.PMC ## n &= ~ (ipins);	/* Port Mode Control Register: 0 - port, 1 - alternative */ \
 		GPIO.PM ## n |= (ipins);	/* Port Mode Register (PMn): 0 - output, 1 - input */ \
 		GPIO.PIBC ## n |= (ipins);	/* Port Input Buffer Control Register (PIBCn): 0 - hiZ, 1 - input */ \
 	} while (0)
 
-#define r7s721_pio_outputs(n, opins, initialstate) do { \
+#define r7s721_pio_outputs(n, pins, initialstate) do { \
+		const portholder_t opins = (pins); \
 		GPIO.PIPC ## n &= ~ (opins);	/* Port IP Control Register: 0 - direction control from PMn, 1 - from alternative function */ \
 		GPIO.PSR ## n = ((opins) * 0x10000UL) | (initialstate); \
 		GPIO.PMC ## n &= ~ (opins);	/* Port Mode Control Register: 0 - port, 1 - alternative */ \
@@ -124,7 +123,9 @@ power2(uint_fast16_t v)
 		GPIO.PIBC ## n &= ~ (opins);	/* Port Input Buffer Control Register (PIBCn): 0 - hiZ, 1 - input */ \
 	} while (0)
 
-#define r7s721_pio_alternative(n, iopins, alt) do { \
+#define r7s721_pio_alternative(n, pins, ioalt) do { \
+		const unsigned alt = (ioalt); \
+		const portholder_t iopins = (pins); \
 		const int pfcae = ((alt) & 0x04) != 0; \
 		const int pfce = ((alt) & 0x02) != 0; \
 		const int pfc = ((alt) & 0x01) != 0; \
