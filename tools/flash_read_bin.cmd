292cdc00fd34e9dd100fd498a6b5ae6f167c4ea9
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-04-09 14:40:24 +0300

    Убрана лишняя опция уазания читаемого размера

--- tools/flash_read_bin.cmd
+++ tools/flash_read_bin.cmd
@@ -1,4 +1,5 @@
 del tc1_r7s721020_rom_old.bin
-dfu-util-static.exe --device ffff:0738 --dfuse-address 0x18000000 --upload-size 0x200000 --upload tc1_r7s721020_rom_old.bin
+dfu-util-static.exe --device ffff:0738 --dfuse-address 0x18000000 --upload tc1_r7s721020_rom_old.bin
 pause
+
 
