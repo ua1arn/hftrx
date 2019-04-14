2fec140e2faf2ae2d560716254b0b14b007ea775
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-04-14 04:08:38 +0300

    DFU UILITES

--- r7s721020/flash_write_bin.cmd
+++ r7s721020/flash_write_bin.cmd
@@ -1,4 +1,5 @@
-pause "Press any key for write FLASH"
-dfu-util-static.exe --device ffff:0738 --dfuse-address 0x18020000 --download tc1_r7s721020_app.bin
-pause
+@echo Press any key for write FLASH
+@pause
+dfu-util-static.exe --device ffff:0738 --alt 0 --dfuse-address 0x18020000 --download tc1_r7s721020_app.bin
+@pause
 
