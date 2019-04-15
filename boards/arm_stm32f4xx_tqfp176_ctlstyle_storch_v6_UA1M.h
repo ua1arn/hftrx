d37b5e427d9578a43eaa9896c94576a17cb9726c
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-04-15 04:50:28 +0300

    custom cinf upd

--- boards/arm_stm32f4xx_tqfp176_ctlstyle_storch_v6_UA1M.h
+++ boards/arm_stm32f4xx_tqfp176_ctlstyle_storch_v6_UA1M.h
@@ -265,9 +265,9 @@
 	//#define WITHLOOPBACKTEST	1	/* прослушивание микрофонного входа, генераторов */
 	//#define WITHMODEMIQLOOPBACK	1	/* модем получает собственные передаваемые квадратуры */
 
-	#define WITHUSESDCARD		1	// Включение поддержки SD CARD
+	//#define WITHUSESDCARD		1	// Включение поддержки SD CARD
 	//#define WITHUSEUSBFLASH		1	// Включение поддержки USB memory stick
-	#define WITHUSEAUDIOREC		1	// Запись звука на SD CARD
+	//#define WITHUSEAUDIOREC		1	// Запись звука на SD CARD
 	//#define WITHUSEAUDIOREC2CH	1	// Запись звука на SD CARD в стерео
 	//#define WITHUSEAUDIORECCLASSIC	1	// стандартный формат записи, без "дыр"
 
