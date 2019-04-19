eea22a01ff503ddf9798b2d1a1b4d615b520e87f
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-04-19 23:30:03 +0300

    only valid date/time values to/from RC chip

--- chip/ds1305.h
+++ chip/ds1305.h
@@ -92,19 +92,31 @@ static uint_fast8_t ds1305_testbit(
 
 #endif
 
+static uint_fast8_t 
+ds1305_min(uint_fast8_t a, uint_fast8_t b)
+{
+	return a < b ? a : b;
+}
+
+static uint_fast8_t 
+ds1305_max(uint_fast8_t a, uint_fast8_t b)
+{
+	return a > b ? a : b;
+}
+
 // input value 0x00..0x99, return value 0..99
 static uint_fast8_t 
-ds1305_bcd2bin(uint_fast8_t v)
+ds1305_bcd2bin(uint_fast8_t v, uint_fast8_t low, uint_fast8_t high)
 {
 	const div_t d = div(v, 16);
-	return d.quot * 10 + d.rem;
+	return ds1305_max(ds1305_min(d.quot * 10 + d.rem, high), low);
 }
 
 // input value: 0..99, return value 0x00..0x99
 static uint_fast8_t 
-ds1305_bin2bcd(uint_fast8_t v)
+ds1305_bin2bcd(uint_fast8_t v, uint_fast8_t low, uint_fast8_t high)
 {
-	const div_t d = div(v, 10);
+	const div_t d = div(ds1305_max(ds1305_min(v, high), low), 10);
 	return d.quot * 16 + d.rem;
 }
 
@@ -119,9 +131,9 @@ void board_rtc_settime(
 	static const uint8_t b_WP [1] = { 0x40 };
 	static const uint8_t b_WE [1] = { 0x00 };
 
-	bt [0] = 0x7f & ds1305_bin2bcd(secounds);	// r=0
-	bt [1] = 0x7f & ds1305_bin2bcd(minutes);	// r=1
-	bt [2] = 0x3f & ds1305_bin2bcd(hours);		// r=2
+	bt [0] = 0x7f & ds1305_bin2bcd(secounds, 0, 59);	// r=0
+	bt [1] = 0x7f & ds1305_bin2bcd(minutes, 0, 59);	// r=1
+	bt [2] = 0x3f & ds1305_bin2bcd(hours, 0, 23);		// r=2
 
 	// write enable
 	//ds1305_writebuff(b_WE, sizeof b_WE / sizeof b_WE [0], DS1305REG_CONTROL);
@@ -145,9 +157,9 @@ void board_rtc_setdatetime(
 	const uint_fast8_t rt = DS1305REG_TIME;	// Addr
 	uint8_t bt [3];
 
-	bt [0] = 0x7f & ds1305_bin2bcd(secounds);	// r=0
-	bt [1] = 0x7f & ds1305_bin2bcd(minutes);	// r=1
-	bt [2] = 0x3f & ds1305_bin2bcd(hours);		// r=2
+	bt [0] = 0x7f & ds1305_bin2bcd(secounds, 0, 59);	// r=0
+	bt [1] = 0x7f & ds1305_bin2bcd(minutes, 0, 59);	// r=1
+	bt [2] = 0x3f & ds1305_bin2bcd(hours, 0, 23);		// r=2
 
 	// write enable
 	//ds1305_writebuff(b_WE, sizeof b_WE / sizeof b_WE [0], DS1305REG_CONTROL);
@@ -161,9 +173,9 @@ void board_rtc_setdatetime(
 	const uint_fast8_t rd = DS1305REG_DAYOFMONTH;	// Addr
 	uint8_t bd [3];
 
-	bd [0] = 0x3f & ds1305_bin2bcd(dayofmonth);	// r=4
-	bd [1] = 0x01f & ds1305_bin2bcd(month);		// r=5
-	bd [2] = ds1305_bin2bcd(year % 100);		// r=6
+	bd [0] = 0x3f & ds1305_bin2bcd(dayofmonth, 1, 31);	// r=4
+	bd [1] = 0x01f & ds1305_bin2bcd(month, 1, 12);		// r=5
+	bd [2] = ds1305_bin2bcd(year % 100, 0, 99);		// r=6
 
 	ds1305_writebuff(bd, sizeof bd / sizeof bd [0], rd);
 	// write protect
@@ -182,9 +194,9 @@ void board_rtc_setdate(
 	static const uint8_t b_WP [1] = { 0x40 };
 	static const uint8_t b_WE [1] = { 0x00 };
 
-	bd [0] = 0x3f & ds1305_bin2bcd(dayofmonth);	// r=4
-	bd [1] = 0x01f & ds1305_bin2bcd(month);		// r=5
-	bd [2] = ds1305_bin2bcd(year % 100);		// r=6
+	bd [0] = 0x3f & ds1305_bin2bcd(dayofmonth, 1, 31);	// r=4
+	bd [1] = 0x01f & ds1305_bin2bcd(month, 1, 12);		// r=5
+	bd [2] = ds1305_bin2bcd(year % 100, 0, 99);		// r=6
 
 	// write enable
 	//ds1305_writebuff(b_WE, sizeof b_WE / sizeof b_WE [0], DS1305REG_CONTROL);
@@ -205,9 +217,9 @@ void board_rtc_getdate(
 
 	ds1305_readbuff(b, sizeof b / sizeof b[0], r);
 
-	* dayofmonth = ds1305_bcd2bin(b [0] & 0x3f);		// r=5
-	* month = ds1305_bcd2bin(b [1] & 0x1f);	// r=6
-	* year = 2000 + ds1305_bcd2bin(b [2]);		// r=7
+	* dayofmonth = ds1305_bcd2bin(b [0] & 0x3f, 1, 31);		// r=5
+	* month = ds1305_bcd2bin(b [1] & 0x1f, 1, 12);	// r=6
+	* year = 2000 + ds1305_bcd2bin(b [2], 0, 99);		// r=7
 }
 
 void board_rtc_gettime(
@@ -223,9 +235,9 @@ void board_rtc_gettime(
 
 	//debug_printf_P(PSTR("board_rtc_gettime: %02X:%02X:%02X\n"), b [0], b [1], b [2]);
 
-	* secounds = ds1305_bcd2bin(b [0] & 0x7f);	// r=1
-	* minute = ds1305_bcd2bin(b [1] & 0x7f);	// r=2
-	* hour = ds1305_bcd2bin(b [2] & 0x3f);		// r=3
+	* secounds = ds1305_bcd2bin(b [0] & 0x7f, 0, 59);	// r=1
+	* minute = ds1305_bcd2bin(b [1] & 0x7f, 0, 59);	// r=2
+	* hour = ds1305_bcd2bin(b [2] & 0x3f, 0, 23);		// r=3
 }
 
 void board_rtc_getdatetime(
@@ -242,12 +254,12 @@ void board_rtc_getdatetime(
 
 	ds1305_readbuff(b, sizeof b / sizeof b[0], r);
 
-	* secounds = ds1305_bcd2bin(b [0] & 0x7f);	// r=0
-	* minute = ds1305_bcd2bin(b [1] & 0x7f);	// r=1
-	* hour = ds1305_bcd2bin(b [2] & 0x3f);		// r=2
-	* dayofmonth = ds1305_bcd2bin(b [4] & 0x3f);		// r=4
-	* month = ds1305_bcd2bin(b [5] & 0x1f);		// r=5
-	* year = 2000 + ds1305_bcd2bin(b [6]);		// r=6
+	* secounds = ds1305_bcd2bin(b [0] & 0x7f, 0, 59);	// r=0
+	* minute = ds1305_bcd2bin(b [1] & 0x7f, 0, 59);	// r=1
+	* hour = ds1305_bcd2bin(b [2] & 0x3f, 0, 23);		// r=2
+	* dayofmonth = ds1305_bcd2bin(b [4] & 0x3f, 1, 31);		// r=4
+	* month = ds1305_bcd2bin(b [5] & 0x1f, 1, 12);		// r=5
+	* year = 2000 + ds1305_bcd2bin(b [6], 0, 88);		// r=6
 }
 
 /* возврат не-0 если требуется начальная загрузка значений */
@@ -270,9 +282,9 @@ uint_fast8_t board_rtc_chip_initialize(void)
 		// Initial application of power,
 
 		// write enable
-		//ds1305_writebuff(b_WE, sizeof b_WE / sizeof b_WE [0], DS1305REG_CONTROL);
+		ds1305_writebuff(b_WE, sizeof b_WE / sizeof b_WE [0], DS1305REG_CONTROL);
 
-		//local_delay_ms(50);
+		local_delay_ms(50);
 
 		// write enable (may be need???)
 		ds1305_writebuff(b0, sizeof b0 / sizeof b0 [0], DS1305REG_CONTROL);
