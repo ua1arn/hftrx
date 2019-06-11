53a2eca92d5362049f1fad63474565b29b1d5190
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-06-12 00:42:43 +0300

    Уточнение выводов управления

--- boards/arm_r7s72_tqfp176_cpustyle_storch_v8.h
+++ boards/arm_r7s72_tqfp176_cpustyle_storch_v8.h
@@ -614,25 +614,25 @@
 
 #if WITHKEYBOARD
 	/* P7_8: second encoder button with pull-up */
-	#define KBD_MASK (1U << 8)	// P7_8
-	#define KBD_TARGET_PIN (R7S721_INPUT_PORT(7))
+	//#define KBD_MASK (1U << 8)	// P7_8
+	//#define KBD_TARGET_PIN (R7S721_INPUT_PORT(7))
 
 
-#if WITHENCODER2
+#if 1//WITHENCODER2
 	// P7_8
-	#define TARGET_ENC2BTN_BIT (1U << 3)	// P7_8 - second encoder button with pull-up
+	#define TARGET_ENC2BTN_BIT (1U << 8)	// P7_8 - second encoder button with pull-up
 	#define TARGET_ENC2BTN_GET	((R7S721_INPUT_PORT(7) & TARGET_ENC2BTN_BIT) == 0)
 #endif /* WITHENCODER2 */
 
-#if WITHPWBUTTON
+#if 1//WITHPWBUTTON
 	// P5_3 - ~CPU_POWER_SW signal
-	#define TARGET_POWER_BIT (1U << 3)	// P5_3 - ~CPU_POWER_SW signal
-	#define TARGET_POWER_GET	((R7S721_INPUT_PORT(5) & TARGET_POWER_BIT) == 0)
+	#define TARGET_POWERBTN_BIT (1U << 3)	// P5_3 - ~CPU_POWER_SW signal
+	#define TARGET_POWERBTN_GET	((R7S721_INPUT_PORT(5) & TARGET_POWERBTN_BIT) == 0)
 #endif /* WITHPWBUTTON */
 
 	#define HARDWARE_KBD_INITIALIZE() do { \
-			arm_hardware_pio7_inputs(KBD_MASK); \
-			arm_hardware_pio5_inputs(TARGET_POWER_BIT); \
+			arm_hardware_pio7_inputs(TARGET_ENC2BTN_BIT); \
+			arm_hardware_pio5_inputs(TARGET_POWERBTN_BIT); \
 		} while (0)
 
 #else /* WITHKEYBOARD */
