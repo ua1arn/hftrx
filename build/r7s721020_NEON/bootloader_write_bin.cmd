08892f92e4a662173db19632deb170c1511b4559
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-07-15 01:28:57 +0300

    Added 'NEON' configuration

---
+++ build/r7s721020_NEON/bootloader_write_bin.cmd
@@ -0,0 +1,5 @@
+@echo Press any key for write BOOTLOADER section of FLASH
+@pause
+dfu-util-static.exe --device ffff:0738 --alt 1 --dfuse-address 0x18000000 --download tc1_r7s721020_boot.bin
+@pause
+
