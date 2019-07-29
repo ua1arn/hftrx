a7c4e0feea42c8b1cc55ee6f8a6f6d541db22906
dolphin\gena <genadi.zawidowski@experimentx.co>
2019-07-29 12:54:56 +0300

    STM32F429I-DISCO custom config update (enable debug log)

--- boards/arm_stm32f4xx_tqfp144_ctlstyle_32F429DISCO.h
+++ boards/arm_stm32f4xx_tqfp144_ctlstyle_32F429DISCO.h
@@ -249,7 +249,7 @@
 	#define WITHSPLIT	1	/* управление режимами расстройки одной кнопкой */
 	//#define WITHSPLITEX	1	/* Трехкнопочное управление режимами расстройки */
 	#define WITHCATEXT	1	/* Расширенный набор команд CAT */
-	#define WITHELKEY	1
+	#define WITHELKEY	(! WITHDEBUG)	// PA9 & PA10 - confilct with debug
 	//#define WITHKBDENCODER 1	// перестройка частоты кнопками
 	#define WITHKEYBOARD 1	/* в данном устройстве есть клавиатура */
 	#define KEYBOARD_USE_ADC	1	/* на одной линии установлено  четыре  клавиши. на vref - 6.8K, далее 2.2К, 4.7К и 13K. */
