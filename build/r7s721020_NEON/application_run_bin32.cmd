7b853096e08054bc7174f27a010eb73c1ef16337
ua1arn <mgs2001@mail.ru>
2019-12-04 20:51:59 +0300

    Uodate DFU scripts

--- build/r7s721020_NEON/application_run_bin32.cmd
+++
@@ -1,3 +0,0 @@
-REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
-dfu-util.exe --device ffff:0750 --alt 2 --dfuse-address 0x20000000 --download tc1_r7s721020_app.bin
-dfu-util.exe --device ffff:0750 --alt 2 --detach
--- build/r7s721020_NEON/flash_read_bin.cmd
+++ build/r7s721020_NEON/flash_read_bin.cmd
@@ -1,6 +1,6 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
-del tc1_r7s721020_rom_old.bin
-dfu-util-static.exe --device ffff:0738 --alt 0 --dfuse-address 0x18020000 --upload tc1_r7s721020_rom_old.bin
+del tc1_r7s721020_app_old.bin
+dfu-util-static.exe --device ffff:0738 --alt 0 --dfuse-address 0x18020000 --upload tc1_r7s721020_app_old.bin
 @pause
 
 
