0c8086014b54caa0c8f1e75613f67f13fb335c5d
ua1arn <mgs2001@mail.ru>
2019-12-05 17:50:45 +0300

    STM32MP1: решились проблемы с загрузкой с QSPI

--- build/stm32mp157axx/mkimage.cmd
+++ build/stm32mp157axx/mkimage.cmd
@@ -1,3 +1,3 @@
-stm32image.exe -s stm32mp157axx_app.bin -d app.stm32 -l 2FFC2400 -e 2FFC2400
-stm32image.exe -s stm32mp157axx_app.bin -d app.stm32.bin -l 2FFC2400 -e 2FFC2400
+stm32image.exe -s stm32mp157axx_app.bin -d app.stm32 -l 2FFC2500 -e 2FFC2500
+stm32image.exe -s stm32mp157axx_app.bin -d app.stm32.bin -l 2FFC2500 -e 2FFC2500
 
