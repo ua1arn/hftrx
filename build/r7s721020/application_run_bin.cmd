651645d859018a5daea158863036cbc31c7faa90
ua1arn <mgs2001@mail.ru>
2019-12-09 21:54:55 +0300

    use non-satic version of DFU UTILITY

--- build/r7s721020/application_run_bin.cmd
+++ build/r7s721020/application_run_bin.cmd
@@ -1,3 +1,3 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
-dfu-util-static.exe --device ffff:0750 --alt 2 --dfuse-address 0x20000000 --download tc1_r7s721020_app.bin
-dfu-util-static.exe --device ffff:0750 --alt 2 --detach
+dfu-util.exe --device ffff:0750 --alt 2 --dfuse-address 0x20000000 --download tc1_r7s721020_app.bin
+dfu-util.exe --device ffff:0750 --alt 2 --detach
