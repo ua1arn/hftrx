8cb313a9dff5032187acd5a89b36a2862acbe27c
ua1arn <mgs2001@mail.ru>
2019-12-14 01:48:12 +0300

    STM32MP1: bin file names update

--- build/stm32mp157axx/bootloader_write_bin.cmd
+++ build/stm32mp157axx/bootloader_write_bin.cmd
@@ -1,6 +1,6 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
 @echo Press any key for write BOOTLOADER section of FLASH
 @pause
-dfu-util.exe --device ffff:0738 --alt 1 --dfuse-address 0x70000000 --download fsbl.stm32
+dfu-util.exe --device ffff:0738 --alt 1 --dfuse-address 0x70000000 --download app.stm32
 @pause
 
