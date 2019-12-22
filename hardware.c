e4d14f62ac577c9246d34d058a9be62cd1d454be
ua1arn <mgs2001@mail.ru>
2019-12-23 00:36:56 +0300

    RENESAS: speed-up initialize

--- hardware.c
+++ hardware.c
@@ -10947,6 +10947,8 @@ ttb_accessbits(uintptr_t a)
 
 #if CPUSTYLE_R7S721020
 
+	if (a >= 0x18000000uL && a < 0x20000000uL)			//
+		return addrbase | TTB_PARA_NORMAL_CACHE;
 	if (a >= 0x00000000uL && a < 0x00A00000uL)			// up to 10 MB
 		return addrbase | TTB_PARA_NORMAL_CACHE;
 	if (a >= 0x20000000uL && a < 0x20A00000uL)			// up to 10 MB
