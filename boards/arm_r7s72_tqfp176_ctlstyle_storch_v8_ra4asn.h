b8454f8d286dabcdb40fceddd74cd1d224908aab
RA4ASN <49730623+RA4ASN@users.noreply.github.com>
2020-02-18 00:07:21 +0300

    Update arm_r7s72_tqfp176_ctlstyle_storch_v8_ra4asn.h

--- boards/arm_r7s72_tqfp176_ctlstyle_storch_v8_ra4asn.h
+++ boards/arm_r7s72_tqfp176_ctlstyle_storch_v8_ra4asn.h
@@ -105,8 +105,8 @@
 
 	#define LCDMODE_LTDC		1	/* Use VDC5 peripheral block */
 	#define LCDMODE_LTDC_L8		1	/* используется 8 бит на пиксель представление экрана. Иначе - 16 бит. */
-	//#define LCDMODE_LTDC_PIP16	1	/* используется PIP с форматом 16 бит - RGB565 */
-	#define LCDMODE_LTDC_PIPL8	1	/* используется PIP с форматом 8 бит - индексные цвета */
+	#define LCDMODE_LTDC_PIP16	1	/* используется PIP с форматом 16 бит - RGB565 */
+	//#define LCDMODE_LTDC_PIPL8	1	/* используется PIP с форматом 8 бит - индексные цвета */
 
 	//#define LCDMODE_LTDC_L24	1	/* 32-bit на пиксель в памяти (3 байта) */
 	//#define WITHFLATLINK 1	/* Работа с TFT панелью через SN75LVDS83B	*/
