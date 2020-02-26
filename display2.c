8d3d61f7163edda0a338a6c90d9cf3e2d7e2e9f2
ua1arn <mgs2001@mail.ru>
2020-02-26 11:29:20 +0300

    Для 800*480 дизайн с сенсорным диспеем выдеден в отдельный раздел

--- display2.c
+++ display2.c
@@ -4420,8 +4420,8 @@ enum
 		p->frame = (uintptr_t) getscratchpip();
 	}
 
-#elif DSTYLE_G_X800_Y480 //&& WITHSPECTRUMWF
-
+#elif DSTYLE_G_X800_Y480 && WITHTOUCHTEST //&& WITHSPECTRUMWF
+	// Вариант с сенсорным экраном
 	// TFT панель AT070TN90
 	// 480/5 = 96, 800/16=50
 
@@ -4437,16 +4437,8 @@ enum
 	enum
 	{
 		BDTH_ALLRXBARS = 30,	// ширина зоны для отображение барграфов на индикаторе
-#if WITHTOUCHTEST
 		BDTH_ALLRX = 50,	// ширина зоны для отображение графического окна на индикаторе
 		BDCV_ALLRX = ROWS2GRID(61),	// количество строк, отведенное под S-метр, панораму, иные отображения
-#elif 1
-		BDTH_ALLRX = 50,	// ширина зоны для отображение графического окна на индикаторе
-		BDCV_ALLRX = ROWS2GRID(55),	// количество строк, отведенное под S-метр, панораму, иные отображения
-#else
-		BDTH_ALLRX = 40,	// ширина зоны для отображение графического окна на индикаторе
-		BDCV_ALLRX = ROWS2GRID(55),	// количество строк, отведенное под S-метр, панораму, иные отображения
-#endif
 
 		BDTH_LEFTRX = 17,	// ширина индикатора баллов (без плюсов)
 		BDTH_RIGHTRX = BDTH_ALLRXBARS - BDTH_LEFTRX,	// ширина индикатора плюсов
@@ -4557,9 +4549,7 @@ enum
 		{	0,	DLES,	dsp_latchwaterfall,	REDRM_BARS,	PGLATCH, },	// формирование данных спектра для последующего отображения спектра или водопада
 		{	0,	DLES,	display2_spectrum,	REDRM_BARS, PGSPE, },// подготовка изображения спектра
 		{	0,	DLES,	display2_waterfall,	REDRM_BARS, PGWFL, },// подготовка изображения водопада
-#if WITHTOUCHTEST
 		{	0,	DLES,	display_pip_update, REDRM_BARS, PGSPE, },
-#endif /* WITHTOUCHTEST */
 		{	0,	DLES,	display2_colorbuff,	REDRM_BARS,	PGWFL | PGSPE, },// Отображение водопада и/или спектра
 	#endif /* WITHSPECTRUMWF */
 #else
@@ -4568,15 +4558,197 @@ enum
 #endif
 
 
-#if !WITHTOUCHTEST
+	#if WITHAMHIGHKBDADJ
+		//{	XX, DLE1,	display_amfmhighcut4,REDRM_MODE, PGALL, },	// 3.70
+	#endif /* WITHAMHIGHKBDADJ */
+		//{	XX,	DLE1,	display_samfreqdelta8, REDRM_BARS, PGALL, },	/* Получить информацию об ошибке настройки в режиме SAM */
+
+	#if WITHMENU
+		{	3,				30,	display_multilinemenu_block_groups,	REDRM_MLBL, REDRSUBSET_MENU, }, //Блок с пунктами меню (группы)
+		{	LABELW + 5,		30,	display_multilinemenu_block_params,	REDRM_MLBL, REDRSUBSET_MENU, }, //Блок с пунктами меню (параметры)
+		{	LABELW*2 + 6,	30,	display_multilinemenu_block_vals,	REDRM_MVAL, REDRSUBSET_MENU, }, //Блок с пунктами меню (значения)
+	#endif /* WITHMENU */
+
+		// sleep mode display
+		{	5,	25,	display_datetime12,	REDRM_BARS, PGSLP, },	// DATE & TIME // DATE&TIME Jan-01 13:40
+		{	20, 25,	display_voltlevelV5, REDRM_VOLT, PGSLP, },	// voltmeter with "V"
+	};
+
+#if WITHMENU
+	void display2_getgridparams(gridparams_t * p)
+	{
+		p->gy2 = DLE1;	// Первая строка (grid) за меню.
+		p->gx2 = CHARS2GRID(DIM_X / 16);	// Правый край за областью меню
+	}
+
+	void display2_getmultimenu(multimenuwnd_t * p)
+	{
+		p->multilinemenu_max_rows = 15;
+		p->ystep = 4;	// количество ячеек разметки на одну строку меню
+		p->reverse = 1;
+	}
+#endif /* WITHMENU */
+
+	/* получить координаты окна с панорамой и/или водопадом. */
+	void display2_getpipparams(pipparams_t * p)
+	{
+		p->x = GRID2X(0);	// позиция верхнего левого угла в пикселях
+		p->y = GRID2Y(DLES);	// позиция верхнего левого угла в пикселях
+		p->w = GRID2X(CHARS2GRID(BDTH_ALLRX));	// размер по горизонтали в пикселях
+		p->h = GRID2Y(BDCV_ALLRX);				// размер по вертикали в пикселях
+		p->frame = (uintptr_t) getscratchpip();
+	}
+
+#elif DSTYLE_G_X800_Y480 //&& WITHSPECTRUMWF
+	// вариант без сенсорного экрана
+	// TFT панель AT070TN90
+	// 480/5 = 96, 800/16=50
+
+	#if WITHSHOWSWRPWR	/* на дисплее одновременно отображаются SWR-meter и PWR-meter */
+		//					"012345678901234567890123456789"
+		#define SWRPWRMAP	"1    2    3    4  0%   |  100%"
+		#define SWRMAX	(SWRMIN * 40 / 10)	// 4.0 - значение на полной шкале
+	#else
+		#warning Should be defined WITHSHOWSWRPWR
+	#endif
+	//						"012345678901234567890123456789"
+	#define SMETERMAP		"1   3   5   7   9  +20 +40 +60"
+	enum
+	{
+		BDTH_ALLRXBARS = 30,	// ширина зоны для отображение барграфов на индикаторе
+#if 0
+		BDTH_ALLRX = 50,	// ширина зоны для отображение графического окна на индикаторе
+		BDCV_ALLRX = ROWS2GRID(49),	// количество строк, отведенное под S-метр, панораму, иные отображения
+#else
+		BDTH_ALLRX = 40,	// ширина зоны для отображение графического окна на индикаторе
+		BDCV_ALLRX = ROWS2GRID(55),	// количество строк, отведенное под S-метр, панораму, иные отображения
+#endif
+
+		BDTH_LEFTRX = 17,	// ширина индикатора баллов (без плюсов)
+		BDTH_RIGHTRX = BDTH_ALLRXBARS - BDTH_LEFTRX,	// ширина индикатора плюсов
+		BDTH_SPACERX = 0,
+		BDTH_ALLSWR = 16,
+		BDTH_SPACESWR = 2,
+		BDTH_ALLPWR = 12,
+		BDTH_SPACEPWR = 0,
+
+		/* совмещение на одном экрание водопада и панорамы */
+		BDCO_SPMRX = ROWS2GRID(0),	// смещение спектра по вертикали в ячейках от начала общего поля
+		BDCV_SPMRX = ROWS2GRID(27),	// вертикальный размер спектра в ячейках
+		BDCO_WFLRX = BDCV_SPMRX,	// смещение водопада по вертикали в ячейках от начала общего поля
+		BDCV_WFLRX = BDCV_ALLRX - BDCV_SPMRX	// вертикальный размер водопада в ячейках
+	};
+	enum {
+		DLES = 35,		// spectrum window upper line
+        DLE1 = 93,		//
+		DLE_unused
+	};
+
+
+	enum
+	{
+		PATTERN_SPACE = 0x00,	/* очищаем место за SWR и PWR метром этим символом */
+		PATTERN_BAR_FULL = 0xFF,
+		PATTERN_BAR_HALF = 0x3c,
+		PATTERN_BAR_EMPTYFULL = 0x00,	//0x00
+		PATTERN_BAR_EMPTYHALF = 0x00	//0x00
+	};
+
+	enum
+	{
+		DPAGE0,					// Страница, в которой отображаются основные (или все)
+		DISPLC_MODCOUNT
+	};
+
+	enum
+	{
+		PG0 = REDRSUBSET(DPAGE0),
+		PGALL = PG0 | REDRSUBSET_MENU,
+		PGWFL = PG0,	// страница отображения водопада
+		PGSPE = PG0,	// страница отображения панорамы
+		PGSWR = PG0,	// страница отоюражения S-meter и SWR-meter
+		PGLATCH = PGALL | REDRSUBSET_SLEEP,	// страницы, на которых возможно отображение водопада или панорамы.
+		PGSLP = REDRSUBSET_SLEEP,
+		PGunused
+	};
+
+	#if 1//TUNE_TOP > 100000000uL
+		#define DISPLC_WIDTH	9	// количество цифр в отображении частоты
+	#else
+		#define DISPLC_WIDTH	8	// количество цифр в отображении частоты
+	#endif
+	#define DISPLC_RJ		0	// количество скрытых справа цифр в отображении частоты
+
+	// 480/5 = 96, 800/16=50
+	// 272/5 = 54, 480/16=30 (old)
+	//#define GRID2X(cellsx) ((cellsx) * 16)	/* перевод ячеек сетки разметки в номер пикселя по горизонталм */
+	//#define GRID2Y(cellsy) ((cellsy) * 5)	/* перевод ячеек сетки разметки в номер пикселя по вертикали */
+	//#define SMALLCHARH 15 /* Font height */
+	//#define SMALLCHARW 16 /* Font width */
+	static const FLASHMEM struct dzone dzones [] =
+	{
+		{	0,	0,	display2_pip_off,	REDRM_MODE,	PGSLP | REDRSUBSET_MENU },	// Выключить PIP если на данной странице не требуется
+		{	0,	0,	display_txrxstate2, REDRM_MODE, PGALL, },
+		{	3,	0,	display_ant5,		REDRM_MODE, PGALL, },
+		{	9,	0,	display_att4,		REDRM_MODE, PGALL, },
+		{	14,	0,	display_preovf3,	REDRM_BARS, PGALL, },
+		{	18,	0,	display_genham1,	REDRM_BARS, PGALL, },	// Отображение режима General Coverage / HAM bands
+		{	21,	0,	display_lockstate4, REDRM_MODE, PGALL, },	// LOCK
+
+	#if WITHENCODER2
+		{	41, 0,	display_fnlabel9,	REDRM_MODE, PGALL, },	// FUNC item label
+		{	41,	4,	display_fnvalue9,	REDRM_MODE, PGALL, },	// FUNC item value
+		{	45, 15,	display_notch5,		REDRM_MODE, PGALL, },	// NOTCH on/off
+	#else /* WITHENCODER2 */
+		{	45, 0,	display_notch5,		REDRM_MODE, PGALL, },	// FUNC item label
+		{	45,	4,	display_notchfreq5,	REDRM_BARS, PGALL, },	// FUNC item value
+	#endif /* WITHENCODER2 */
+
+		{	46, 20,	display_agc3,		REDRM_MODE, PGALL, },	// AGC mode
+		{	46, 25,	display_voxtune3,	REDRM_MODE, PGALL, },	// VOX
+		{	46, 30,	display_datamode3,	REDRM_MODE, PGALL, },	// DATA mode indicator
+//		{	46, 35,	display_nr3,		REDRM_MODE, PGALL, },	// NR
+		{	46, 40,	display_atu3,		REDRM_MODE, PGALL, },	// TUNER state (optional)
+		{	46, 45,	display_byp3,		REDRM_MODE, PGALL, },	// TUNER BYPASS state (optional)
+		{	46, 50,	display_rec3,		REDRM_BARS, PGALL, },	// Отображение режима записи аудио фрагмента
+
+		{	0,	7,	display_freqX_a,	REDRM_FREQ, PGALL, },	// MAIN FREQ Частота (большие цифры)
+		{	21, 10,	display_mode3_a,	REDRM_MODE,	PGALL, },	// SSB/CW/AM/FM/...
+		{	26, 10,	display_rxbw3,		REDRM_MODE, PGALL, },	// 3.1 / 0,5 / WID / NAR
+		{	30, 10,	display_nr3,		REDRM_MODE, PGALL, },	// NR : was: AGC
+		{	21, 15,	display_mainsub3,	REDRM_MODE, PGALL, },	// main/sub RX: A/A, A/B, B/A, etc
+
+		{	26,	15,	display_vfomode3,	REDRM_MODE, PGALL, },	// SPLIT
+		{	9,	20,	display_freqX_b,	REDRM_FRQB, PGALL, },	// SUB FREQ
+		{	21, 20,	display_mode3_b,	REDRM_MODE,	PGALL, },	// SSB/CW/AM/FM/...
+
+#if 1
+		{	0,	25,	display2_legend,	REDRM_MODE, PGSWR, },	// Отображение оцифровки шкалы S-метра
+		{	0,	30,	display2_bars,		REDRM_BARS, PGSWR, },	// S-METER, SWR-METER, POWER-METER
+		//{	0,	35,	display2_legend_tx,	REDRM_MODE, PGSWR, },	// Отображение оцифровки шкалы PWR & SWR-метра
+		//{	0,	40,	display2_bars_tx,	REDRM_BARS, PGSWR, },	// S-METER, SWR-METER, POWER-METER
+		{	31,	30, display_siglevel4, 	REDRM_BARS, PGSWR, },	// signal level dBm
+		{	36, 30,	display_freqdelta8, REDRM_BARS, PGSWR, },	// выход ЧМ демодулятора
+	#if WITHSPECTRUMWF
+		{	0,	DLES,	dsp_latchwaterfall,	REDRM_BARS,	PGLATCH, },	// формирование данных спектра для последующего отображения спектра или водопада
+		{	0,	DLES,	display2_spectrum,	REDRM_BARS, PGSPE, },// подготовка изображения спектра
+		{	0,	DLES,	display2_waterfall,	REDRM_BARS, PGWFL, },// подготовка изображения водопада
+		{	0,	DLES,	display2_colorbuff,	REDRM_BARS,	PGWFL | PGSPE, },// Отображение водопада и/или спектра
+	#endif /* WITHSPECTRUMWF */
+#else
+		{	0,	0,	display2_pip_off,	REDRM_MODE,	PGSWR },	// Выключить PIP если на данной странице не требуется
+		{	0,	25,	display2_adctest,	REDRM_BARS, PGSWR, },	// ADC raw data print
+#endif
+
+
+
 		{	0,	DLE1,	display_datetime12,	REDRM_BARS, PGALL,	},	// DATE&TIME Jan-01 13:40
 		{	13,	DLE1,	display_span9,		REDRM_MODE, PGALL, },	/* Получить информацию об ошибке настройки в режиме SAM */
 		{	23, DLE1,	display_thermo4,	REDRM_VOLT, PGALL, },	// thermo sensor
 		{	28, DLE1,	display_usb3,		REDRM_BARS, PGALL, },	// USB host status
 
 		{	39, DLE1,	display_currlevel5, REDRM_VOLT, PGALL, },	// PA drain current d.dd without "A"
-		{	45, 40,	display_voltlevelV5, REDRM_VOLT, PGALL, },	// voltmeter with "V"
-#endif
+		{	45, DLE1,	display_voltlevelV5, REDRM_VOLT, PGALL, },	// voltmeter with "V"
 	#if WITHAMHIGHKBDADJ
 		//{	XX, DLE1,	display_amfmhighcut4,REDRM_MODE, PGALL, },	// 3.70
 	#endif /* WITHAMHIGHKBDADJ */
