8176b1d62d56ba31cc54e4bfec8472b44d029580
ua1arn <mgs2001@mail.ru>
2020-03-22 00:02:43 +0300

    Custom config update

--- boards/arm_stm32h7xx_tqfp176_ctlstyle_storch_v7_ats52.h
+++ boards/arm_stm32h7xx_tqfp176_ctlstyle_storch_v7_ats52.h
@@ -226,6 +226,7 @@
 	//#define ENCODER_REVERSE	1	/* разводка на плате с перепутаными фазами от валкодера */
 	//#define ENCODER2_REVERSE	1	/* разводка на плате с перепутаными фазами от валкодера */
 	#define WITHENCODER2	1		/* есть второй валкодер */
+	#define BOARD_ENCODER2_DIVIDE 4		/* значение для валкодера PEC16-4220F-n0024 (с трещёткой") */
 	//#define WITHPWBUTTON	1	/* Наличие схемы электронного включения питания */
 	/* Board hardware configuration */
 	//#define CODEC1_TYPE CODEC_TYPE_TLV320AIC23B
