77e5bbda2cd1f6a259f9cb594a89be58dbb1a912
ua1arn <mgs2001@mail.ru>
2020-03-28 07:35:39 +0300

    опечатка

--- inc/audio.h
+++ inc/audio.h
@@ -203,7 +203,7 @@ extern "C" {
 
 	typedef int32_t aubufv_t;
 	typedef int_fast32_t aufastbufv_t;
-	typedef int_fast64_t aufastbufv2x_t;	/* дип для работы ресэмплера при получении среднего арифметического */
+	typedef int_fast64_t aufastbufv2x_t;	/* тип для работы ресэмплера при получении среднего арифметического */
 	/* масштабирование сэмплов */
 	#define AUDIO16TOAUB(v) (((v) * 65536L))	/* не забывать, аргумент может быть FLOAT */
 	#define AUBTOAUDIO16(v) ((v) / 65536L)	/* не забывать, аргумент может быть FLOAT */
@@ -212,7 +212,7 @@ extern "C" {
 
 	typedef int16_t aubufv_t;
 	typedef int_fast16_t aufastbufv_t;
-	typedef int_fast32_t aufastbufv2x_t;	/* дип для работы ресэмплера при получении среднего арифметического */
+	typedef int_fast32_t aufastbufv2x_t;	/* тип для работы ресэмплера при получении среднего арифметического */
 	/* масштабирование сэмплов (заглушки) */
 	#define AUDIO16TOAUB(v) (v)	/* не забывать, аргумент может быть FLOAT */
 	#define AUBTOAUDIO16(v) (v)	/* не забывать, аргумент может быть FLOAT */
