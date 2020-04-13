8ee1239969e212e637ff8e8d7810fb6357cfd64b
ua1arn <mgs2001@mail.ru>
2020-04-13 21:25:28 +0300

    Renesas: добавлена ловушка на обращение к адресам в 0-м мегабайте

--- hardware.c
+++ hardware.c
@@ -10959,6 +10959,9 @@ ttb_accessbits(uintptr_t a)
 
 #if CPUSTYLE_R7S721020
 
+	if (a == 0x00000000uL)
+		return addrbase | TTB_PARA_NO_ACCESS;		// NULL pointers access trap
+
 	if (a >= 0x18000000uL && a < 0x20000000uL)			//
 		return addrbase | TTB_PARA_NORMAL_CACHE;
 	if (a >= 0x00000000uL && a < 0x00A00000uL)			// up to 10 MB
@@ -10969,7 +10972,8 @@ ttb_accessbits(uintptr_t a)
 #elif CPUSTYLE_STM32MP1
 
 	if (a >= 0x00000000uL && a < 0x10000000uL)			// BOOT
-		return addrbase | TTB_PARA_NO_ACCESS;
+		return addrbase | TTB_PARA_NO_ACCESS;			// NULL pointers access trap
+
 	if (a >= 0x10000000uL && a < 0x20000000uL)			// SRAMs ??????
 		return addrbase | TTB_PARA_NORMAL_CACHE;
 	if (a >= 0x20000000uL && a < 0x30000000uL)			// SYSRAM
