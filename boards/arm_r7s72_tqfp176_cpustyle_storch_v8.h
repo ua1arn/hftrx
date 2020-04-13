269ce3f495da4b3a8fc47647894ec6916598dcd8
ua1arn <mgs2001@mail.ru>
2020-04-13 11:32:12 +0300

    Renesas: исправлена альтернативная функция при подключении выводов в QSPI

--- boards/arm_r7s72_tqfp176_cpustyle_storch_v8.h
+++ boards/arm_r7s72_tqfp176_cpustyle_storch_v8.h
@@ -859,13 +859,13 @@
 		#define SPIDF_SOFTINITIALIZE() do { \
 				arm_hardware_pio4_outputs(1U << 2, 1U << 2);				/* P4_2 WP / SPBIO20_0 */ \
 				arm_hardware_pio4_outputs(1U << 3, 1U << 3);				/* P4_3 NC / SPBIO30_0 */ \
-				/* arm_hardware_pio4_alternative(1U << 4, R7S721_PIOALT_4);	*/ /* P4_4 SCLK / SPBCLK_0 */ \
+				/* arm_hardware_pio4_alternative(1U << 4, R7S721_PIOALT_2);	*/ /* P4_4 SCLK / SPBCLK_0 */ \
 				arm_hardware_pio4_outputs(1U << 4, 1U << 4);	/* P4_4 SCLK / SPBCLK_0 */ \
-				/* arm_hardware_pio4_alternative(1U << 5, R7S721_PIOALT_4);	*/ /* P4_5 CS# / SPBSSL_0 */ \
+				/* arm_hardware_pio4_alternative(1U << 5, R7S721_PIOALT_2);	*/ /* P4_5 CS# / SPBSSL_0 */ \
 				arm_hardware_pio4_outputs(1U << 5, 1 * (1U << 5));			/* P4_5 CS# / SPBSSL_0 */ \
-				/* arm_hardware_pio4_alternative(1U << 6, R7S721_PIOALT_4);	*/ /* P4_6 MOSI / SPBIO00_0 */ \
+				/* arm_hardware_pio4_alternative(1U << 6, R7S721_PIOALT_2);	*/ /* P4_6 MOSI / SPBIO00_0 */ \
 				arm_hardware_pio4_outputs(1U << 6, 1U << 6);	/* P4_6 MOSI / SPBIO00_0 */ \
-				/* arm_hardware_pio4_alternative(1U << 7, R7S721_PIOALT_4);	*/ /* P4_7 MISO / SPBIO10_0 */ \
+				/* arm_hardware_pio4_alternative(1U << 7, R7S721_PIOALT_2);	*/ /* P4_7 MISO / SPBIO10_0 */ \
 				arm_hardware_pio4_inputs(1U << 7);	/* P4_7 MISO / SPBIO10_0 */ \
 			} while (0)
 		#define SPIDF_HANGOFF() do { \
@@ -891,10 +891,10 @@
 		#define SPIDF_HARDINITIALIZE() do { \
 				arm_hardware_pio4_outputs(1U << 2, 1U << 2);				/* P4_2 WP / SPBIO20_0 */ \
 				arm_hardware_pio4_outputs(1U << 3, 1U << 3);				/* P4_3 NC / SPBIO30_0 */ \
-				arm_hardware_pio4_alternative(1U << 4, R7S721_PIOALT_4);	/* P4_4 SCLK / SPBCLK_0 */ \
-				arm_hardware_pio4_alternative(1U << 6, R7S721_PIOALT_4);	/* P4_6 MOSI / SPBIO00_0 */ \
-				arm_hardware_pio4_alternative(1U << 7, R7S721_PIOALT_4);	/* P4_7 MISO / SPBIO10_0 */ \
-				arm_hardware_pio4_alternative(1U << 5, R7S721_PIOALT_4);	/* P4_5 CS# / SPBSSL_0 */ \
+				arm_hardware_pio4_alternative(1U << 4, R7S721_PIOALT_2);	/* P4_4 SCLK / SPBCLK_0 */ \
+				arm_hardware_pio4_alternative(1U << 6, R7S721_PIOALT_2);	/* P4_6 MOSI / SPBIO00_0 */ \
+				arm_hardware_pio4_alternative(1U << 7, R7S721_PIOALT_2);	/* P4_7 MISO / SPBIO10_0 */ \
+				arm_hardware_pio4_alternative(1U << 5, R7S721_PIOALT_2);	/* P4_5 CS# / SPBSSL_0 */ \
 			} while (0)
 		#define SPIDF_HANGOFF() do { \
 				arm_hardware_pio4_inputs(0xFC); /* Отключить процессор от SERIAL FLASH */ \
