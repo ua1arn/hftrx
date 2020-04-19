f80f8f2d6e9224f9044597bb5c740134755b7395
ua1arn <mgs2001@mail.ru>
2020-04-19 17:38:32 +0300

    Fix build for Renesas

--- irq_ctrl_gic_forward.c
+++ irq_ctrl_gic_forward.c
@@ -7,10 +7,10 @@
 
 #include "hardware.h"
 
-#if CPUSTYLE_ARM_CA7 || CPUSTYLE_ARM_CA7
+#if CPUSTYLE_ARM_CA7 || CPUSTYLE_ARM_CA9
 
 	// Закомментировать #include CMSIS_device_header
 
 	#include "irq_ctrl_gic.c"
 
-#endif /* CPUSTYLE_ARM_CA7 || CPUSTYLE_ARM_CA7 */
+#endif /* CPUSTYLE_ARM_CA7 || CPUSTYLE_ARM_CA9 */
