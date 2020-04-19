ba36805581c22525cef863bc2068b15674cc312a
ua1arn <mgs2001@mail.ru>
2020-04-19 14:52:57 +0300

    Makefiles update

--- cmsid_dsp_forward.c
+++ cmsid_dsp_forward.c
@@ -9,21 +9,19 @@
 
 #if WITHINTEGRATEDDSP
 
-#include "BasicMathFunctions/BasicMathFunctions.c"
-#include "CommonTables/CommonTables.c"
-//#include "DistanceFunctions/DistanceFunctions.c"
-//#include "MatrixFunctions/MatrixFunctions.c"
-#include "SupportFunctions/SupportFunctions.c"
-//#include "BayesFunctions/BayesFunctions.c"
-#include "ComplexMathFunctions/ComplexMathFunctions.c"
-#include "FastMathFunctions/FastMathFunctions.c"
-#include "SVMFunctions/SVMFunctions.c"
-#include "TransformFunctions/TransformFunctions.c"
-//#include "ControllerFunctions/ControllerFunctions.c"
-#include "FilteringFunctions/FilteringFunctions.c"
-//#include "StatisticsFunctions/StatisticsFunctions.c"
+	#include "BasicMathFunctions/BasicMathFunctions.c"
+	#include "CommonTables/CommonTables.c"
+	//#include "DistanceFunctions/DistanceFunctions.c"
+	//#include "MatrixFunctions/MatrixFunctions.c"
+	#include "SupportFunctions/SupportFunctions.c"
+	//#include "BayesFunctions/BayesFunctions.c"
+	#include "ComplexMathFunctions/ComplexMathFunctions.c"
+	#include "FastMathFunctions/FastMathFunctions.c"
+	//#include "SVMFunctions/SVMFunctions.c"
+	#include "TransformFunctions/TransformFunctions.c"
+	//#include "ControllerFunctions/ControllerFunctions.c"
+	#include "FilteringFunctions/FilteringFunctions.c"
+	//#include "StatisticsFunctions/StatisticsFunctions.c"
 
 #endif  /* WITHINTEGRATEDDSP */
-
-#include "irq_ctrl_gic.c"
 
