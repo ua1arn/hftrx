8aba2198fd9979540e5ec9e2b3fda6a494adac78
ua1arn <mgs2001@mail.ru>
2020-05-02 16:01:12 +0300

    dfu-util: ускорение загрузки образа в память

--- build/r7s721020/application_run_bin.cmd
+++ build/r7s721020/application_run_bin.cmd
@@ -1,3 +1,3 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
-dfu-util.exe --device ffff:0750 --alt 2 --dfuse-address 0x20000000 --download "tc1_r7s721020_app.nec32"
+dfu-util.exe --device ffff:0750 --alt 2 --transfer-size 1024 --dfuse-address 0x20000000 --download "tc1_r7s721020_app.nec32"
 dfu-util.exe --device ffff:0750 --alt 2 --detach
