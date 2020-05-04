5b66f5aaa8f1afb7244ca23d0a3a9c5b7463413e
ua1arn <mgs2001@mail.ru>
2020-05-04 14:43:41 +0300

    custom config update

--- boards/arm_stm32mp1xx_cpustyle_storch_v9_new.h
+++ boards/arm_stm32mp1xx_cpustyle_storch_v9_new.h
@@ -93,8 +93,8 @@
 #else /* WITHISBOOTLOADER */
 
 	#define WITHLTDCHW		1	/* Наличие контроллера дисплея с framebuffer-ом */
-	#define WITHGPUHW	1	/* Graphic processor unit */
-	#define WITHEHCIHW	1	/* USB_EHCI controller */
+	//#define WITHGPUHW	1	/* Graphic processor unit */
+	//#define WITHEHCIHW	1	/* USB_EHCI controller */
 	#define WITHUSBHW	1	/* Используется встроенная в процессор поддержка USB */
 
 	#define WITHUSBHW_DEVICE	USB_OTG_HS	/* на этом устройстве поддерживается функциональность DEVICE	*/
