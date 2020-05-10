0d805a12272109b72c35a5bcceb86ea95e0e7c3e
ua1arn <mgs2001@mail.ru>
2020-05-11 01:26:32 +0300

    Утилиты для программирования загрузчика через автономный программатор

---
+++ build/r7s721020/progdfu_write.cmd
@@ -0,0 +1,6 @@
+REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
+@echo Press any key for write BOOTLOADER section of FLASH
+@pause
+dfu-util.exe --device ffff:073c --alt 0 --dfuse-address 0x50000000 --download "tc1_r7s721020_boot.bin"
+@pause
+
