cd43a453db3e21bb0f60327008c973d629371a6b
ua1arn <mgs2001@mail.ru>
2020-06-03 19:25:58 +0300

    Stm32mp1: stable!

--- boards/arm_stm32mp1xx_ctlstyle_storch_v9_new.h
+++ boards/arm_stm32mp1xx_ctlstyle_storch_v9_new.h
@@ -64,13 +64,13 @@
 		// HSI version (HSI=64 MHz)
 		// PLL1_1600
 		#define PLL1DIVM	5	// ref1_ck = 12.8 MHz
-		//#define PLL1DIVN	50	// x25..x100: 12.8 * 50 = 640 MHz
-		#define PLL1DIVN	62	// x25..x100: 12.8 * 32 = 793.6 MHz
+		#define PLL1DIVN	50	// x25..x100: 12.8 * 50 = 640 MHz
+		//#define PLL1DIVN	62	// x25..x100: 12.8 * 32 = 793.6 MHz
 		#define PLL1DIVP	1	// MPU
 		#define PLL1DIVQ	2
 		#define PLL1DIVR	2
 
-#if 1
+#if 0
 		// PLL2_1600
 		#define PLL2DIVM	5	// ref2_ck = 12.8 MHz
 		#define PLL2DIVN	41	// 12.8 * 41 = 524.8 MHz
@@ -201,8 +201,8 @@
 
 	//#define LCDMODE_V1B	1	/* Обычная конфигурация с PIP на часть экрана, MAIN=L8, PIP=L8 */
 	//#define LCDMODE_V1	1	/* Обычная конфигурация с PIP на часть экрана, MAIN=L8, PIP=RGB565 */
-	#define LCDMODE_V2	1	/* только главный экран с тремя видеобуферами L8, без PIP */
-	//#define LCDMODE_V2A	1	/* только главный экран с тремя видеобуферами RGB565, без PIP */
+	//#define LCDMODE_V2	1	/* только главный экран с тремя видеобуферами L8, без PIP */
+	#define LCDMODE_V2A	1	/* только главный экран с тремя видеобуферами RGB565, без PIP */
 	//#define LCDMODE_V0	1	/* Обычная конфигурация без PIP с L8 на основном экране */
 	//#define LCDMODE_V1A	1	/* Обычная конфигурация с PIP на часть экрана, MAIN=RGB565, PIP=RGB565 */
 
