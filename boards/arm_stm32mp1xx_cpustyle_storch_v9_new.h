f5abcf04cbf885c5ec139a340d9029412fc65e9c
ua1arn <mgs2001@mail.ru>
2020-06-09 12:31:34 +0300

    custom config update: звук пока передаем про USB FS

--- boards/arm_stm32mp1xx_cpustyle_storch_v9_new.h
+++ boards/arm_stm32mp1xx_cpustyle_storch_v9_new.h
@@ -100,7 +100,7 @@
 
 	#define WITHUSBHW_DEVICE	USB_OTG_HS	/* на этом устройстве поддерживается функциональность DEVICE	*/
 	#define WITHUSBDEV_VBUSSENSE	1		/* используется предопределенный вывод OTG_VBUS */
-	#define WITHUSBDEV_HSDESC	1			/* Требуется формировать дескрипторы как для HIGH SPEED */
+	//#define WITHUSBDEV_HSDESC	1			/* Требуется формировать дескрипторы как для HIGH SPEED */
 	//#define WITHUSBDEV_HIGHSPEEDULPI	1
 	#define WITHUSBDEV_HIGHSPEEDPHYC	1	// UTMI -> USB_DP2 & USB_DM2
 	//#define WITHUSBDEV_DMAENABLE 1
