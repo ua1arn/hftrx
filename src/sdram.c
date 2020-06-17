f0ca5ba6d8db78caae42732a3e00fcc26c81d6d0
RA4ASN <49730623+RA4ASN@users.noreply.github.com>
2020-06-17 09:17:27 +0300

    Update sdram.c

--- src/sdram.c
+++ src/sdram.c
@@ -2700,93 +2700,46 @@ void SDRAM_GPIOConfig(void)
    D15 -> PD10*
   */
 
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  /* Enable GPIOs clock */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOE |
-			RCC_AHB1Periph_GPIOF | RCC_AHB1Periph_GPIOG | RCC_AHB1Periph_GPIOH, ENABLE);
-
 	enum { GPIO_AF_FMC = 12 };
 
-	/* Common GPIO configuration */
-	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-
-	/* GPIOC configuration */
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource3 , GPIO_AF_FMC);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-	/* GPIOD configuration */
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource0, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource1, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource10, GPIO_AF_FMC);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15 | GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
-
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	/* GPIOE configuration */
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource7, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource8, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource9, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource10, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource12, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource13, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource14, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource15, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource0, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource1, GPIO_AF_FMC);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15 | GPIO_Pin_0 | GPIO_Pin_1;
-
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
-
-	/* GPIOF configuration */
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource0, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource1, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource2, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource3, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource4, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource5, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource12, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource13, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource14, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource15, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOF, GPIO_PinSource11, GPIO_AF_FMC);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15 | GPIO_Pin_11;
-
-	GPIO_Init(GPIOF, &GPIO_InitStructure);
-
-	/* GPIOG configuration */
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource0, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource1, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource15, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource8, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource4, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource5, GPIO_AF_FMC);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_15 | GPIO_Pin_8 | GPIO_Pin_4 | GPIO_Pin_5;
-
-	GPIO_Init(GPIOG, &GPIO_InitStructure);
-
-	/* GPIOH configuration */
-	GPIO_PinAFConfig(GPIOH, GPIO_PinSource3, GPIO_AF_FMC);
-	GPIO_PinAFConfig(GPIOH, GPIO_PinSource5, GPIO_AF_FMC);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_5;
-
-	GPIO_Init(GPIOH, &GPIO_InitStructure);
+	arm_hardware_pioc_altfn50((1U << 3), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 14), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 15), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 0), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 1), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 8), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 9), GPIO_AF_FMC);
+	arm_hardware_piod_altfn50((1U << 10), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 7), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 8), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 9), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 10), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 11), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 12), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 13), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 14), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 15), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 0), GPIO_AF_FMC);
+	arm_hardware_pioe_altfn50((1U << 1), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 0), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 1), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 2), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 3), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 4), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 5), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 12), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 13), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 14), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 15), GPIO_AF_FMC);
+	arm_hardware_piof_altfn50((1U << 11), GPIO_AF_FMC);
+	arm_hardware_piog_altfn50((1U << 0), GPIO_AF_FMC);
+	arm_hardware_piog_altfn50((1U << 1), GPIO_AF_FMC);
+	arm_hardware_piog_altfn50((1U << 15), GPIO_AF_FMC);
+	arm_hardware_piog_altfn50((1U << 8), GPIO_AF_FMC);
+	arm_hardware_piog_altfn50((1U << 4), GPIO_AF_FMC);
+	arm_hardware_piog_altfn50((1U << 5), GPIO_AF_FMC);
+	arm_hardware_pioh_altfn50((1U << 3), GPIO_AF_FMC);
+	arm_hardware_pioh_altfn50((1U << 5), GPIO_AF_FMC);
 
 #endif
 }
