5e79641634a7a202527ac69a6c5dc13587bab28e
RA4ASN <49730623+RA4ASN@users.noreply.github.com>
2020-07-05 11:16:36 +0300

    Update gui_user.c

--- src/gui/gui_user.c
+++ src/gui/gui_user.c
@@ -542,15 +542,16 @@ static void buttons_options_handler(void)
 		button_t * btn_AUDsett = find_gui_element(TYPE_BUTTON, win, "btn_AUDsett");
 		button_t * btn_SysMenu = find_gui_element(TYPE_BUTTON, win, "btn_SysMenu");
 		button_t * btn_Utils = find_gui_element(TYPE_BUTTON, win, "btn_Utils");
-		button_t * btn_AutoNotch = find_gui_element(TYPE_BUTTON, win, "btn_AutoNotch");
-
-		if (gui->selected_link->link == btn_AutoNotch)
-		{
-			btn_AutoNotch->is_locked = hamradio_get_autonotch() ? BUTTON_NON_LOCKED : BUTTON_LOCKED;
-			local_snprintf_P(btn_AutoNotch->text, ARRAY_SIZE(btn_AutoNotch->text), PSTR("AutoNotch|%s"), btn_AutoNotch->is_locked ? "ON" : "OFF");
-			hamradio_set_autonotch(btn_AutoNotch->is_locked);
-		}
-		else if (gui->selected_link->link == btn_AF)
+//		button_t * btn_AutoNotch = find_gui_element(TYPE_BUTTON, win, "btn_AutoNotch");
+//
+//		if (gui->selected_link->link == btn_AutoNotch)
+//		{
+//			btn_AutoNotch->is_locked = hamradio_get_autonotch() ? BUTTON_NON_LOCKED : BUTTON_LOCKED;
+//			local_snprintf_P(btn_AutoNotch->text, ARRAY_SIZE(btn_AutoNotch->text), PSTR("AutoNotch|%s"), btn_AutoNotch->is_locked ? "ON" : "OFF");
+//			hamradio_set_autonotch(btn_AutoNotch->is_locked);
+//		}
+//		else
+		if (gui->selected_link->link == btn_AF)
 		{
 			window_t * win = get_win(WINDOW_BP);
 			open_window(win);
@@ -611,7 +612,7 @@ static void window_options_process(void)
 			{ 0, 0, 100, 44, buttons_options_handler, CANCELLED, BUTTON_NON_LOCKED, 0, WINDOW_OPTIONS, NON_VISIBLE, UINTPTR_MAX, "btn_AGC",  	  "AGC", },
 			{ 0, 0, 100, 44, buttons_options_handler, CANCELLED, BUTTON_NON_LOCKED, 0, WINDOW_OPTIONS, NON_VISIBLE, UINTPTR_MAX, "btn_SysMenu",   "System|settings", },
 			{ 0, 0, 100, 44, buttons_options_handler, CANCELLED, BUTTON_NON_LOCKED, 0, WINDOW_OPTIONS, NON_VISIBLE, UINTPTR_MAX, "btn_Utils", 	  "Utils", },
-			{ 0, 0, 100, 44, buttons_options_handler, CANCELLED, BUTTON_NON_LOCKED, 0, WINDOW_OPTIONS, NON_VISIBLE, UINTPTR_MAX, "btn_AutoNotch", "AutoNotch|OFF", },
+//			{ 0, 0, 100, 44, buttons_options_handler, CANCELLED, BUTTON_NON_LOCKED, 0, WINDOW_OPTIONS, NON_VISIBLE, UINTPTR_MAX, "btn_AutoNotch", "AutoNotch|OFF", },
 		};
 		win->bh_count = ARRAY_SIZE(buttons);
 		uint_fast16_t buttons_size = sizeof(buttons);
@@ -641,9 +642,9 @@ static void window_options_process(void)
 		}
 		elements_state(win);
 
-		button_t * btn_AutoNotch = find_gui_element(TYPE_BUTTON, win, "btn_AutoNotch");
-		btn_AutoNotch->is_locked = hamradio_get_autonotch();
-		local_snprintf_P(btn_AutoNotch->text, ARRAY_SIZE(btn_AutoNotch->text), PSTR("AutoNotch|%s"), btn_AutoNotch->is_locked ? "ON" : "OFF");
+//		button_t * btn_AutoNotch = find_gui_element(TYPE_BUTTON, win, "btn_AutoNotch");
+//		btn_AutoNotch->is_locked = hamradio_get_autonotch();
+//		local_snprintf_P(btn_AutoNotch->text, ARRAY_SIZE(btn_AutoNotch->text), PSTR("AutoNotch|%s"), btn_AutoNotch->is_locked ? "ON" : "OFF");
 
 		calculate_window_position(win, xmax, ymax);
 		hamradio_disable_keyboard_redirect();
