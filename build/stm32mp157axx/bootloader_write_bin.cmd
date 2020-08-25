c9789ab6d27bca251839d529c3b8f34d4007933a
ua1arn <mgs2001@mail.ru>
2020-08-25 05:08:03 +0300

    stm32mp1: загрузчики пишутся по отдельноси

--- build/stm32mp157axx/bootloader_write_bin.cmd
+++ build/stm32mp157axx/bootloader_write_bin.cmd
@@ -1,7 +1,6 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
-@echo Press any key for write BOOTLOADER section of FLASH
+@echo Press any key for write FIRST BOOTLOADER section of FLASH
 @pause
 dfu-util.exe --device ffff:0738 --alt 1 --dfuse-address 0x70000000 --download "fsbl.stm32"
-dfu-util.exe --device ffff:0738 --alt 1 --dfuse-address 0x70040000 --download "fsbl.stm32"
 @pause
 
