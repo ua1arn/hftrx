c6a80a763934f8df25d78ad8b92a2819b353d810
ua1arn <mgs2001@mail.ru>
2020-09-02 23:37:39 +0300

    stmn32mp1: GUI tests

--- boards/arm_stm32mp1xx_ctlstyle_storch_v9a.h
+++ boards/arm_stm32mp1xx_ctlstyle_storch_v9a.h
@@ -464,13 +464,13 @@
 
 	#if LCDMODE_AT070TNA2 || LCDMODE_AT070TN90
 		#if 0
-			#define WITHTOUCHGUI	1	/* тестирование работы с сенсорным экраном */
+			#define WITHTOUCHGUI		1
 			#define WITHALPHA			64
-			#define WITHENCODER2NOFREQ	1	/* второй валкодер не перестраивает частоту */
+			#define FORMATFROMLIBRARY 	1
 			#define WITHUSEMALLOC	1	/* разрешение поддержки malloc/free/calloc/realloc */
 		#endif
 	#endif /* LCDMODE_AT070TNA2 || LCDMODE_AT070TN90 */
-	//#define WITHUSEMALLOC	1	/* разрешение поддержки malloc/free/calloc/realloc */
+	#define WITHUSEMALLOC	1	/* разрешение поддержки malloc/free/calloc/realloc */
 
 	//#define LO1PHASES	1		/* Прямой синтез первого гетеродина двумя DDS с програмимруемым сдвигом фазы */
 	#define WITHFANTIMER	1	/* выключающийся по таймеру вентилятор в усилителе мощности */
