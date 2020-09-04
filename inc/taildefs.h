63ccb5e9511094f7a715c6860a432ba6d5e358e7
ua1arn <mgs2001@mail.ru>
2020-09-04 15:56:21 +0300

    stm32mp1: валкодер обраьатывается вторым процессором

--- inc/taildefs.h
+++ inc/taildefs.h
@@ -789,8 +789,8 @@ typedef enum
 	/* Пока привязка процессора обрабатывающего прерывание по приоритету. */
 	#define TARGETCPU_SYSTEM (1u << 0)		// CPU #0
 	#define TARGETCPU_RT (1u << 0)			// CPU #0
-	#define TARGETCPU_OVRT (1u << 0)		// CPU #0
-	#define TARGETCPU_EXTIO (1u << 0)		// CPU #0
+	#define TARGETCPU_OVRT (1u << 1)		// CPU #1
+	#define TARGETCPU_EXTIO (1u << 1)		// CPU #1
 
 
 	#define SPINLOCK_t spinlock_t
