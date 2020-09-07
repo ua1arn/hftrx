370a65cd74fbb563521f89fe46531eef414d9c9b
ua1arn <mgs2001@mail.ru>
2020-09-07 16:03:04 +0300

    Cortex-A : добавил возможность устанавливать NO EXECUTE флаг для памяти

--- src/hardware.c
+++ src/hardware.c
@@ -11604,13 +11604,13 @@ M_SIZE_IO_2     EQU     2550            ; [Area11] I/O area 2
 //; setting for Outer and inner not cache normal memory
 //#define	TTB_PARA_NORMAL_NOT_CACHE   0b_0000_0001_1101_1110_0010
 // not used
-#define	TTB_PARA_NORMAL_NOT_CACHE TTB_PARA(TEXval_NOCACHE, Bval_NOCACHE, Cval_NOCACHE, DOMAINval, APRWval, 0)
+#define	TTB_PARA_NORMAL_NOT_CACHE(ro, xn) TTB_PARA(TEXval_NOCACHE, Bval_NOCACHE, Cval_NOCACHE, DOMAINval, (ro) ? APROval : APRWval, (xn) != 0)
 
 //; setting for Outer and inner write back, write allocate normal memory (Cacheable)
 //#define	TTB_PARA_NORMAL_CACHE       0b_0000_0001_1101_1110_1110
-#define	TTB_PARA_NORMAL_CACHE(ro) TTB_PARA(TEXval_WBCACHE, Bval_WBCACHE, Cval_WBCACHE, DOMAINval, (ro) ? APROval : APRWval, 0)
+#define	TTB_PARA_NORMAL_CACHE(ro, xn) TTB_PARA(TEXval_WBCACHE, Bval_WBCACHE, Cval_WBCACHE, DOMAINval, (ro) ? APROval : APRWval, (xn) != 0)
 
-#define	TTB_PARA_DEVICE TTB_PARA(TEXval_DEVICE, Bval_DEVICE, Cval_DEVICE, DOMAINval, APRWval, 1)
+#define	TTB_PARA_DEVICE TTB_PARA(TEXval_DEVICE, Bval_DEVICE, Cval_DEVICE, DOMAINval, APRWval, 1 /* XN=1 */)
 
 #define	TTB_PARA_NO_ACCESS 0
 
@@ -11642,11 +11642,11 @@ ttb_accessbits(uintptr_t a, int ro)
 		return addrbase | TTB_PARA_NO_ACCESS;		// NULL pointers access trap
 
 	if (a >= 0x18000000uL && a < 0x20000000uL)			//
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 	if (a >= 0x00000000uL && a < 0x00A00000uL)			// up to 10 MB
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 	if (a >= 0x20000000uL && a < 0x20A00000uL)			// up to 10 MB
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 
 	return addrbase | TTB_PARA_DEVICE;
 
@@ -11656,22 +11656,22 @@ ttb_accessbits(uintptr_t a, int ro)
 		return addrbase | TTB_PARA_NO_ACCESS;			// NULL pointers access trap
 
 	if (a >= 0x20000000uL && a < 0x30000000uL)			// SYSRAM
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 
 	if (a >= 0x40000000uL && a < 0x60000000uL)			//  peripherials 1, peripherials 2
 		return addrbase | TTB_PARA_DEVICE;
 
 	if (a >= 0x60000000uL && a < 0x70000000uL)			//  FMC NOR
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 
 	if (a >= 0x70000000uL && a < 0xA0000000uL)			//  QUADSPI, FMC NAND, ...
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 
 	if (a >= 0xA0000000uL && a < 0xC0000000uL)			//  GIC
 		return addrbase | TTB_PARA_DEVICE;
 
 	if (a >= 0xC0000000uL && a < 0xE0000000uL)			// DDR memory
-		return addrbase | TTB_PARA_NORMAL_CACHE(ro);
+		return addrbase | TTB_PARA_NORMAL_CACHE(ro, 0);
 
 	if (a >= 0xE0000000uL)								//  DEBUG
 		return addrbase | TTB_PARA_DEVICE;
