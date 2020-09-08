37f2c39078eb5f960ccbaa5f77cec8c48cea6328
ua1arn <mgs2001@mail.ru>
2020-09-09 02:14:48 +0300

    serial port: set initial state before programming (bootloader may use nefore)

--- src/serial.c
+++ src/serial.c
@@ -938,9 +938,14 @@ void hardware_uart1_initialize(uint_fast8_t debug)
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN; // Включение тактирования USART1.
 	(void) RCC->APB2ENR;
 
+	USART1->CR1 = 0;
+
 #if WITHUARTFIFO
 	USART1->CR1 |= USART_CR1_FIFOEN_Msk;
+#else /* WITHUARTFIFO */
+	USART1->CR1 &= ~ USART_CR1_FIFOEN_Msk;
 #endif /* WITHUARTFIFO */
+
 	USART1->CR1 |= (USART_CR1_RE | USART_CR1_TE); // Transmitter Enable & Receiver Enables
 
 	HARDWARE_USART1_INITIALIZE();	/* Присоединить периферию к выводам */
@@ -1138,10 +1143,14 @@ void hardware_uart1_initialize(uint_fast8_t debug)
 	RCC->MP_APB5ENSETR = RCC_MC_APB5ENSETR_USART1EN; // Включение тактирования USART1.
 	(void) RCC->MP_APB5ENSETR;
 
+	USART1->CR1 = 0;
 
 #if WITHUARTFIFO
 	USART1->CR1 |= USART_CR1_FIFOEN_Msk;
+#else /* WITHUARTFIFO */
+	USART1->CR1 &= ~ USART_CR1_FIFOEN_Msk;
 #endif /* WITHUARTFIFO */
+
 	USART1->CR1 |= (USART_CR1_RE | USART_CR1_TE); // Transmitter Enable & Receiver Enables
 
 	HARDWARE_USART1_INITIALIZE();	/* Присоединить периферию к выводам */
@@ -1999,9 +2008,14 @@ void hardware_uart2_initialize(uint_fast8_t debug)
 	RCC->APB1LENR |= RCC_APB1LENR_USART2EN; // Включение тактирования USART2.
 	(void) RCC->APB1LENR;
 
+	USART2->CR1 = 0;
+
 #if WITHUARTFIFO
 	USART2->CR1 |= USART_CR1_FIFOEN_Msk;
+#else /* WITHUARTFIFO */
+	USART2->CR1 &= ~ USART_CR1_FIFOEN_Msk;
 #endif /* WITHUARTFIFO */
+
 	USART2->CR1 |= (USART_CR1_RE | USART_CR1_TE); // Transmitter Enable & Receiver Enables
 
 	HARDWARE_USART2_INITIALIZE();	/* Присоединить периферию к выводам */
@@ -2212,10 +2226,14 @@ xxxx!;
 	RCC->MP_APB1ENSETR = RCC_MC_APB1ENSETR_USART2EN; // Включение тактирования USART2.
 	(void) RCC->MP_APB1ENSETR;
 
+	USART2->CR1 = 0;
 
 #if WITHUARTFIFO
 	USART2->CR1 |= USART_CR1_FIFOEN_Msk;
+#else /* WITHUARTFIFO */
+	USART2->CR1 &= ~ USART_CR1_FIFOEN_Msk;
 #endif /* WITHUARTFIFO */
+
 	USART2->CR1 |= (USART_CR1_RE | USART_CR1_TE); // Transmitter Enable & Receiver Enables
 
 	HARDWARE_USART2_INITIALIZE();	/* Присоединить периферию к выводам */
