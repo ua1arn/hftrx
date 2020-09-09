f542910d85a0c40c2fde534d3b66dc9c7ad014d6
ua1arn <mgs2001@mail.ru>
2020-09-10 01:02:15 +0300

    stm32mp1: распределил по процессорам

--- inc/taildefs.h
+++ inc/taildefs.h
@@ -119,9 +119,9 @@ typedef struct spinlock_tag {
 
 #if WITHSMPSYSTEM
 	/* Пока привязка процессора обрабатывающего прерывание по приоритету. */
-	#define TARGETCPU_SYSTEM (1u << 1)		// CPU #1
-	#define TARGETCPU_RT 	(1u << 0)		// CPU #0
-	#define TARGETCPU_OVRT 	(1u << 1)		// CPU #1
+	#define TARGETCPU_SYSTEM (1u << 0)		// CPU #0
+	#define TARGETCPU_RT 	(1u << 1)		// CPU #1
+	#define TARGETCPU_OVRT 	(1u << 0)		// CPU #0
 
 	#define SPIN_LOCK(p) do { spin_lock(p, __FILE__, __LINE__); } while (0)
 	#define SPIN_UNLOCK(p) do { spin_unlock(p); } while (0)
