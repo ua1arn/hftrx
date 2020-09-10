c2c708ab6dffd15f1564f1b022d8052929deb7de
ua1arn <mgs2001@mail.ru>
2020-09-10 06:14:01 +0300

    stm32mp1: Блок отмены ASSERT для компиляции под платы с неправильной разводкой памяти

--- src/display2.c
+++ src/display2.c
@@ -18,6 +18,25 @@
 
 #define WITHPLACEHOLDERS 1	//  отображение макета с еще незанятыми полями
 
+#if 0
+
+	#undef PRINTF
+	#undef ASSERT
+	#undef VERIFY
+
+	#define PRINTF(...)	do {} while (0)
+	#define ASSERT(v) ((void) (0))
+	#define VERIFY(v) ((void) (v))
+
+//	#define TRACE0(f)			do {} while (0)
+//	#define TRACE1(f,a1)			do {} while (0)
+//	#define TRACE2(f,a1,a2)			do {} while (0)
+//	#define TRACE3(f,a1,a2,a3)		do {} while (0)
+//	#define TRACE4(f,a1,a2,a3,a4)		do {} while (0)
+//	#define TRACE5(f,a1,a2,a3,a4,a5)		do {} while (0)
+
+#endif
+
 #if LCDMODE_LTDC
 
 #include "display/fontmaps.h"
