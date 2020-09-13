f3c26a77011efbab42d1ce9ca44b3f8e208a442f
ua1arn <mgs2001@mail.ru>
2020-09-13 19:50:17 +0300

    stm32mp1: уточнил параметры для работы без внешнего кварца

--- boards/arm_stm32mp1xx_ctlstyle_storch_v9a.h
+++ boards/arm_stm32mp1xx_ctlstyle_storch_v9a.h
@@ -81,11 +81,12 @@
 		// HSI version (HSI=64 MHz)
 		// PLL1_1600
 		#define PLL1DIVM	5	// ref1_ck = 12.8 MHz
-		#define PLL1DIVN	50	// x25..x100: 12.8 * 50 = 640 MHz
-		//#define PLL1DIVN	62	// x25..x100: 12.8 * 62 = 793.6 MHz
 		#define PLL1DIVP	1	// MPU
 		#define PLL1DIVQ	2
 		#define PLL1DIVR	2
+		//#define PLL1DIVN	50	// x25..x100: 12.8 * 50 = 640 MHz
+		//#define PLL1DIVN	62	// x25..x100: 12.8 * 62 = 793.6 MHz
+		#define PLL1DIVN	(stm32mp1_overdrived() ? 62 : 50)	// Auto select
 
 #if 1
 		// PLL2_1600
