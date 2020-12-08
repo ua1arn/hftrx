a29b5743cef56009e71bf0e9c756980a133293d8
ua1arn <mgs2001@mail.ru>
2020-12-09 00:24:49 +0300

    tests update

--- src/tests.c
+++ src/tests.c
@@ -6054,7 +6054,7 @@ void hightests(void)
 		PRINTF("sizeof time_t == %u, t = %lu\n", sizeof (time_t), (unsigned long) t);
 	}
 #endif
-#if 0 && WITHDEBUG
+#if 0 && WITHDEBUG && WITHOPENVG
 	{
 		board_set_bglight(0, WITHLCDBACKLIGHTMAX);	// включить подсветку
 		board_update();
