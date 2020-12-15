4502ef1f5024d2cf32d7502356fb62f338333ee4
ua1arn <mgs2001@mail.ru>
2020-12-16 01:48:46 +0300

    Добавлена утилита манипуляций с обращами

---
+++ build/stm32mp157axx/image_test.cmd
@@ -0,0 +1,5 @@
+SET fn=image_test_M.img
+copy image.raw.backup /b %fn%
+wtitefsbl.exe %fn% 0x04400 fsbl.stm32 -w
+wtitefsbl.exe %fn% 0x44400 fsbl.stm32 -w
+wtitefsbl.exe %fn% 0x84400 tc1_stm32mp157axx_app.stm32 -w
