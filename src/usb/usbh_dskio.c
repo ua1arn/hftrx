3bc528db58b5d699639a05c2d316833d49303f0a
ua1arn <mgs2001@mail.ru>
2021-03-06 15:24:04 +0300

    fix :inux build

--- src/usb/usbh_dskio.c
+++ src/usb/usbh_dskio.c
@@ -18,7 +18,7 @@
 #include "usb_core.h"
 #include "src/fatfs/ff.h"
 #include "src/fatfs/diskio.h"		/* FatFs lower layer API */
-#include "MSC/inc/usbh_msc.h"
+#include "MSC/Inc/usbh_msc.h"
 
 
 #if WITHUSEUSBFLASH
