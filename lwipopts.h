f85b0e478e21038991d7338004481e5770393982
ua1arn <mgs2001@mail.ru>
2021-03-10 16:40:20 +0300

    update to lwip 2

--- lwipopts.h
+++ lwipopts.h
@@ -143,7 +143,7 @@ typedef unsigned sys_prot_t;
 #define LWIP_DBG_MIN_LEVEL     LWIP_DBG_LEVEL_ALL
 #define LWIP_DBG_TYPES_ON      (LWIP_DBG_TRACE | LWIP_DBG_STATE | LWIP_DBG_FRESH | LWIP_DBG_HALT)
 
-//#define HTTPD_DEBUG           LWIP_DBG_ON
+#define HTTPD_DEBUG           LWIP_DBG_ON
 //#define ETHARP_DEBUG           LWIP_DBG_ON
 #define NETIF_DEBUG            LWIP_DBG_ON
 //#define PBUF_DEBUG             LWIP_DBG_ON
@@ -155,20 +155,20 @@ typedef unsigned sys_prot_t;
 #define INET_DEBUG             LWIP_DBG_ON
 #define IP_DEBUG               LWIP_DBG_ON
 #define IP_REASS_DEBUG         LWIP_DBG_ON
-//#define RAW_DEBUG              LWIP_DBG_ON
-//#define MEM_DEBUG              LWIP_DBG_ON
-//#define MEMP_DEBUG             LWIP_DBG_ON
-//#define SYS_DEBUG              LWIP_DBG_ON
-//#define TIMERS_DEBUG           LWIP_DBG_ON
-//#define TCP_DEBUG              LWIP_DBG_ON
-//#define TCP_INPUT_DEBUG        LWIP_DBG_ON
-//#define TCP_FR_DEBUG           LWIP_DBG_ON
-//#define TCP_RTO_DEBUG          LWIP_DBG_ON
-//#define TCP_CWND_DEBUG         LWIP_DBG_ON
-//#define TCP_WND_DEBUG          LWIP_DBG_ON
-//#define TCP_OUTPUT_DEBUG       LWIP_DBG_ON
-//#define TCP_RST_DEBUG          LWIP_DBG_ON
-//#define TCP_QLEN_DEBUG         LWIP_DBG_ON
+#define RAW_DEBUG              LWIP_DBG_ON
+#define MEM_DEBUG              LWIP_DBG_ON
+#define MEMP_DEBUG             LWIP_DBG_ON
+#define SYS_DEBUG              LWIP_DBG_ON
+#define TIMERS_DEBUG           LWIP_DBG_ON
+#define TCP_DEBUG              LWIP_DBG_ON
+#define TCP_INPUT_DEBUG        LWIP_DBG_ON
+#define TCP_FR_DEBUG           LWIP_DBG_ON
+#define TCP_RTO_DEBUG          LWIP_DBG_ON
+#define TCP_CWND_DEBUG         LWIP_DBG_ON
+#define TCP_WND_DEBUG          LWIP_DBG_ON
+#define TCP_OUTPUT_DEBUG       LWIP_DBG_ON
+#define TCP_RST_DEBUG          LWIP_DBG_ON
+#define TCP_QLEN_DEBUG         LWIP_DBG_ON
 #define UDP_DEBUG              LWIP_DBG_ON
 #define TCPIP_DEBUG            LWIP_DBG_ON
 #define SLIP_DEBUG             LWIP_DBG_ON
