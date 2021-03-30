e4a819ef199a9e72b1a3840a4f584e44771b4b0f
ua1arn <mgs2001@mail.ru>
2021-03-30 21:45:47 +0300

    fix pl310 test

--- src/tests.c
+++ src/tests.c
@@ -6159,8 +6159,8 @@ void hightests(void)
 		//PRINTF("L2C_310->CACHE_ID Part number=%02lX\n", (L2C_GetID() >> 6) & 0x0F);
 		PRINTF("L2C_310->CACHE_ID RTL release=%02lX\n", (L2C_GetID() >> 0) & 0x3F);
 
-		PRINTF("L2C Data RAM latencies: %08lX\n", (volatile uint32_t *) ((uintptr_t) L2C_310 + 0x010C)); // reg1_data_ram_control
-		PRINTF("L2C Tag RAM latencies: %08lX\n", (volatile uint32_t *) ((uintptr_t) L2C_310 + 0x0108)); // reg1_tag_ram_control
+		PRINTF("L2C Data RAM latencies: %08lX\n", * (volatile uint32_t *) ((uintptr_t) L2C_310 + 0x010C)); // reg1_data_ram_control
+		PRINTF("L2C Tag RAM latencies: %08lX\n", * (volatile uint32_t *) ((uintptr_t) L2C_310 + 0x0108)); // reg1_tag_ram_control
 	}
 #endif
 #if 0 && defined(__GIC_PRESENT) && (__GIC_PRESENT == 1U)
