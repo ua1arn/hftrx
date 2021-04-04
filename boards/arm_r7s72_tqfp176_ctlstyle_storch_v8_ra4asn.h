42fb0008573d81f596b16127fd3d64450a5088e3
ua1arn <mgs2001@mail.ru>
2021-04-04 16:07:52 +0300

    TSC interrupts mode control: style update

--- boards/arm_r7s72_tqfp176_ctlstyle_storch_v8_ra4asn.h
+++ boards/arm_r7s72_tqfp176_ctlstyle_storch_v8_ra4asn.h
@@ -451,7 +451,7 @@
 	//#define RTC1_TYPE RTC_TYPE_M41T81	/* ST M41T81M6 RTC clock chip with I2C interface */
 	//#define TSC1_TYPE TSC_TYPE_TSC2046	/* Resistive touch screen controller TI TSC2046 */
 	//#define TSC1_TYPE TSC_TYPE_STMPE811	/* touch screen controller */
-	//#define WITHTSCINT	1
+	//#define WITH_STMPE811_INTERRUPTS	1
 	#define WITH_GT911_INTERRUPTS	1
 	#define TSC1_TYPE	TSC_TYPE_GT911		/* Capacitive touch screen with controller Goodix GT911 */
 	//#define TSC_TYPE_STMPE811_USE_SPI	1
--- boards/arm_stm32mp1xx_ctlstyle_storch_v9a.h
+++ boards/arm_stm32mp1xx_ctlstyle_storch_v9a.h
@@ -571,11 +571,12 @@
 	/* Board hardware configuration */
 	#define DDS1_TYPE DDS_TYPE_FPGAV1
 	#define RTC1_TYPE RTC_TYPE_STM32F4xx	/* STM32F4xx/STM32F7xx internal RTC peripherial */
-	//#define WITHTSCINT	1				/* тестирование без кварца 32.768 кГц */
+	//#define WITHRTCLSI	1				/* тестирование без кварца 32.768 кГц */
 
 	//#define TSC1_TYPE TSC_TYPE_TSC2046	/* Resistive touch screen controller TI TSC2046 */
 	#define TSC1_TYPE TSC_TYPE_STMPE811	/* touch screen controller */
 	//#define TSC_TYPE_STMPE811_USE_SPI	1
+	//#define WITH_STMPE811_INTERRUPTS	1
 	//#define TSC1_TYPE	TSC_TYPE_GT911		/* Capacitive touch screen with controller Goodix GT911 */
 	//#define WITH_GT911_INTERRUPTS	1
 	//#define DAC1_TYPE	99999		/* наличие ЦАП для подстройки тактовой частоты */
