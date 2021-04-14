1196395d48e248513f0f8939ad9aad2b50bd12c3
RA4ASN <49730623+RA4ASN@users.noreply.github.com>
2021-04-14 20:22:19 +0300

    zynq: удалены лишние файлы библиотеки

--- lib/zynq/src/xdebug.h
+++ lib/zynq/src/xdebug.h
@@ -5,33 +5,16 @@
 extern "C" {
 #endif
 
-#if defined(DEBUG) && !defined(NDEBUG)
+#include "formats.h"
 
-#ifndef XDEBUG_WARNING
-#define XDEBUG_WARNING
-#warning DEBUG is enabled
-#endif
-
-int printf(const char *format, ...);
+#define xil_printf	debug_printf_P
+#define xdbg_printf(type, ...) (((type) & xdbg_current_types) ? debug_printf_P (__VA_ARGS__) : 0)
 
 #define XDBG_DEBUG_ERROR             0x00000001U    /* error  condition messages */
 #define XDBG_DEBUG_GENERAL           0x00000002U    /* general debug  messages */
 #define XDBG_DEBUG_ALL               0xFFFFFFFFU    /* all debugging data */
 
-#define xdbg_current_types (XDBG_DEBUG_GENERAL)
-
-#define xdbg_stmnt(x)  x
-
-#define xdbg_printf(type, ...) (((type) & xdbg_current_types) ? printf (__VA_ARGS__) : 0)
-
-
-#else /* defined(DEBUG) && !defined(NDEBUG) */
-
-#define xdbg_stmnt(x)
-
-#define xdbg_printf(...)
-
-#endif /* defined(DEBUG) && !defined(NDEBUG) */
+#define xdbg_current_types (XDBG_DEBUG_ERROR)
 
 #ifdef __cplusplus
 }
