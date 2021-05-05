c802bf19fc2cc200fc5f409379a25dc852c68af7
ua1arn <mgs2001@mail.ru>
2021-05-05 18:17:00 +0300

    renesas: подготовка к работе с hal usb lib

---
+++ src/hal/rza1xx_hal.h
@@ -0,0 +1,15 @@
+/*
+ * rza1xx_hal.h
+ *
+ *  Created on: May 5, 2021
+ *      Author: gena
+ */
+
+#ifndef SRC_HAL_RZA1XX_HAL_H_
+#define SRC_HAL_RZA1XX_HAL_H_
+
+
+
+
+
+#endif /* SRC_HAL_RZA1XX_HAL_H_ */
