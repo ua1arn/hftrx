b9b019401116e88645c7f8de7ab13342c59024cb
ua1arn <mgs2001@mail.ru>
2021-05-17 23:38:14 +0300

    stm32mp1: speed optimizations

--- src/display2.c
+++ src/display2.c
@@ -7253,72 +7253,6 @@ static RAMBIGDTCM union states zoomfft_st;
 
 #endif /* (CPUSTYLE_R7S721 || 0) */
 
-void arm_biquad_cascade_df2T_f32_rolled(const arm_biquad_cascade_df2T_instance_f32 * S,const float32_t * pSrc,float32_t * pDst,uint32_t blockSize)
-{
-  const float32_t *pIn = pSrc;                         /* Source pointer */
-        float32_t *pOut = pDst;                        /* Destination pointer */
-        float32_t *pState = S->pState;                 /* State pointer */
-  const float32_t *pCoeffs = S->pCoeffs;               /* Coefficient pointer */
-        float32_t acc1;                                /* Accumulator */
-        float32_t b0, b1, b2, a1, a2;                  /* Filter coefficients */
-        float32_t Xn1;                                 /* Temporary input */
-        float32_t d1, d2;                              /* State variables */
-        uint32_t sample, stage = S->numStages;         /* Loop counters */
-
-  do
-  {
-     /* Reading the coefficients */
-     b0 = pCoeffs[0];
-     b1 = pCoeffs[1];
-     b2 = pCoeffs[2];
-     a1 = pCoeffs[3];
-     a2 = pCoeffs[4];
-
-     /* Reading the state values */
-     d1 = pState[0];
-     d2 = pState[1];
-
-     pCoeffs += 5U;
-
-      /* Initialize blkCnt with number of samples */
-      sample = blockSize;
-
-      while (sample > 0U) {
-        Xn1 = *pIn++;
-
-        acc1 = b0 * Xn1 + d1;
-
-        d1 = b1 * Xn1 + d2;
-        d1 += a1 * acc1;
-
-        d2 = b2 * Xn1;
-        d2 += a2 * acc1;
-
-        *pOut++ = acc1;
-
-        /* decrement loop counter */
-        sample--;
-      }
-
-      /* Store the updated state variables back into the state array */
-      pState[0] = d1;
-      pState[1] = d2;
-
-      pState += 2U;
-
-      /* The current stage output is given as the input to the next stage */
-      pIn = pDst;
-
-      /* Reset the output working pointer */
-      pOut = pDst;
-
-      /* decrement loop counter */
-      stage--;
-
-   } while (stage > 0U);
-
-}
-
 static void fftzoom_filer_decimate_ifspectrum(
 	const struct zoom_param * const prm,
 	float32_t * buffer
@@ -7339,8 +7273,7 @@ static void fftzoom_filer_decimate_ifspectrum(
 	arm_biquad_cascade_df2T_compute_coefs_f32(& c.iir_config, ZOOMFFT_DECIM_STAGES_IIR, prm->pIIRCoeffs);
 #endif /* defined(ARM_MATH_NEON) */
 	// TODO: ask UA3REO for comments about arm_biquad_cascade_df2T_f32_rolled
-	arm_biquad_cascade_df2T_f32_rolled(& c.iir_config, buffer, buffer, usedSize);
-	//arm_biquad_cascade_df2T_f32(& c.iir_config, buffer, buffer, usedSize);
+	arm_biquad_cascade_df2T_f32(& c.iir_config, buffer, buffer, usedSize);
 
 	// Дециматор
 	VERIFY(ARM_MATH_SUCCESS == arm_fir_decimate_init_f32(& c.fir_config,
