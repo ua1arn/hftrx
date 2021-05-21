186a6b04a6694459c13c1b0a1418272dab13ec92
ua1arn <mgs2001@mail.ru>
2021-05-21 11:31:50 +0300

    fpga: подавляем перезолные процессы прием/пеередача

--- fpga_q131/lpm_counter_ovf_1024.vhd
+++ fpga_q131/lpm_counter_ovf_1024.vhd
@@ -46,7 +46,7 @@ ENTITY lpm_counter_ovf_1024 IS
 		cnt_en		: IN STD_LOGIC ;
 		sclr		: IN STD_LOGIC ;
 		cout		: OUT STD_LOGIC ;
-		q		: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+		q		: OUT STD_LOGIC_VECTOR (21 DOWNTO 0)
 	);
 END lpm_counter_ovf_1024;
 
@@ -54,7 +54,7 @@ END lpm_counter_ovf_1024;
 ARCHITECTURE SYN OF lpm_counter_ovf_1024 IS
 
 	SIGNAL sub_wire0	: STD_LOGIC ;
-	SIGNAL sub_wire1	: STD_LOGIC_VECTOR (9 DOWNTO 0);
+	SIGNAL sub_wire1	: STD_LOGIC_VECTOR (21 DOWNTO 0);
 
 
 
@@ -70,20 +70,20 @@ ARCHITECTURE SYN OF lpm_counter_ovf_1024 IS
 			sclr	: IN STD_LOGIC ;
 			cnt_en	: IN STD_LOGIC ;
 			cout	: OUT STD_LOGIC ;
-			q	: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+			q	: OUT STD_LOGIC_VECTOR (21 DOWNTO 0)
 	);
 	END COMPONENT;
 
 BEGIN
 	cout    <= sub_wire0;
-	q    <= sub_wire1(9 DOWNTO 0);
+	q    <= sub_wire1(21 DOWNTO 0);
 
 	LPM_COUNTER_component : LPM_COUNTER
 	GENERIC MAP (
 		lpm_direction => "UP",
 		lpm_port_updown => "PORT_UNUSED",
 		lpm_type => "LPM_COUNTER",
-		lpm_width => 10
+		lpm_width => 22
 	)
 	PORT MAP (
 		clock => clock,
@@ -117,23 +117,23 @@ END SYN;
 -- Retrieval info: PRIVATE: SSET NUMERIC "0"
 -- Retrieval info: PRIVATE: SSET_ALL1 NUMERIC "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
--- Retrieval info: PRIVATE: nBit NUMERIC "10"
+-- Retrieval info: PRIVATE: nBit NUMERIC "22"
 -- Retrieval info: PRIVATE: new_diagram STRING "1"
 -- Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 -- Retrieval info: CONSTANT: LPM_DIRECTION STRING "UP"
 -- Retrieval info: CONSTANT: LPM_PORT_UPDOWN STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_COUNTER"
--- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "10"
+-- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "22"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT NODEFVAL "clock"
 -- Retrieval info: USED_PORT: cnt_en 0 0 0 0 INPUT NODEFVAL "cnt_en"
 -- Retrieval info: USED_PORT: cout 0 0 0 0 OUTPUT NODEFVAL "cout"
--- Retrieval info: USED_PORT: q 0 0 10 0 OUTPUT NODEFVAL "q[9..0]"
+-- Retrieval info: USED_PORT: q 0 0 22 0 OUTPUT NODEFVAL "q[21..0]"
 -- Retrieval info: USED_PORT: sclr 0 0 0 0 INPUT NODEFVAL "sclr"
 -- Retrieval info: CONNECT: @clock 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: @cnt_en 0 0 0 0 cnt_en 0 0 0 0
 -- Retrieval info: CONNECT: @sclr 0 0 0 0 sclr 0 0 0 0
 -- Retrieval info: CONNECT: cout 0 0 0 0 @cout 0 0 0 0
--- Retrieval info: CONNECT: q 0 0 10 0 @q 0 0 10 0
+-- Retrieval info: CONNECT: q 0 0 22 0 @q 0 0 22 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_counter_ovf_1024.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_counter_ovf_1024.inc FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_counter_ovf_1024.cmp TRUE
