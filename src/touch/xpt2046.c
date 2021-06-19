b06025d596a2571e12b218ad9c37bd49f453ec54
ua1arn <mgs2001@mail.ru>
2021-06-19 20:07:21 +0300

    Resistive TSC XPT2046: save state

--- src/touch/xpt2046.c
+++ src/touch/xpt2046.c
@@ -14,15 +14,20 @@
 
 #include "spi.h"
 #include "xpt2046.h"
+
 // Resistive touch screen controller SHENZHEN XPTEK TECHNOLOGY CO.,LTD http://www.xptek.com.cn
 // SPI interface used
-#define targettsc targetext1	/* PE8 ext1 on front panel */
+
+// See https://github.com/ikeji/Ender3Firmware/blob/ef1f9d25eb2cd084ce929e1ad4163ef0a3e88142/Marlin/src/feature/touch/xpt2046.cpp
+// https://github.com/Bodmer/TFT_Touch/blob/master/TFT_Touch.cpp
+
+#define targettsc1 targetext1	/* PE8 ext1 on front panel */
 
 
 #if WITHSPIHW || WITHSPISW
 
 // Read XPT2046 ADC
-static uint_fast16_t
+static uint_fast32_t
 xpt2046_read(
 	spitarget_t target,
 	uint_fast8_t cmd
@@ -33,16 +38,20 @@ xpt2046_read(
 	// где формируется время выборки, не попадала на паузу между байтами.
 	uint_fast32_t rv;
 
-	enum { LSBPOS = 0 };
-	const spi_speeds_t adcspeed = SPIC_SPEED400k;
-	const spi_modes_t adcmode = SPIC_MODE3;
+	enum { CMDPOS = 16 };
+	enum { DATAPOS = 0 };
+	const spi_speeds_t tscspeed = SPIC_SPEED4M;
+	const spi_modes_t tscmode = SPIC_MODE0;
+
+	// PD0=1 & PD1=1: Device is always powered. Reference is on and ADC is on.
+	cmd |= XPT2046_CONTROL | XPT2046_PD0 | XPT2046_PD1;
 
 #if WITHSPI32BIT
 
-	hardware_spi_connect_b32(adcspeed, adcmode);
+	hardware_spi_connect_b32(tscspeed, tscmode);
 	prog_select(target);
 
-	hardware_spi_b32_p1((uint_fast32_t) cmd << (LSBPOS + 14));
+	hardware_spi_b32_p1((uint_fast32_t) cmd << CMDPOS);
 	rv = hardware_spi_complete_b32();
 
 	prog_unselect(target);
@@ -51,10 +60,10 @@ xpt2046_read(
 
 #elif WITHSPI16BIT
 
-	hardware_spi_connect_b16(adcspeed, adcmode);
+	hardware_spi_connect_b16(tscspeed, tscmode);
 	prog_select(target);
 
-	hardware_spi_b16_p1((uint_fast32_t) cmd1 << (LSBPOS + 14) >> 16);
+	hardware_spi_b16_p1((uint_fast32_t) cmd1 << CMDPOS >> 16);
 	v0 = hardware_spi_complete_b16();
 	hardware_spi_b16_p1(0);
 	v1 = hardware_spi_complete_b16();
@@ -66,11 +75,11 @@ xpt2046_read(
 
 #else
 
-	spi_select2(target, adcmode, adcspeed);	// for 50 kS/S and 24 bit words
+	spi_select2(target, tscmode, tscspeed);	// for 50 kS/S and 24 bit words
 
-	v0 = spi_read_byte(target, (uint_fast32_t) cmd1 << (LSBPOS + 14) >> 24);
-	v1 = spi_read_byte(target, (uint_fast32_t) cmd1 << (LSBPOS + 14) >> 16);
-	v2 = spi_read_byte(target, (uint_fast32_t) cmd1 << (LSBPOS + 14) >> 8);
+	v0 = spi_read_byte(target, (uint_fast32_t) cmd1 << CMDPOS >> 24);
+	v1 = spi_read_byte(target, (uint_fast32_t) cmd1 << CMDPOS >> 16);
+	v2 = spi_read_byte(target, (uint_fast32_t) cmd1 << CMDPOS >> 8);
 	v3 = spi_read_byte(target, 0x00);
 
 	spi_unselect(target);
@@ -79,17 +88,9 @@ xpt2046_read(
 
 #endif
 
-	return (rv >> LSBPOS) & 0xFFF;
+	return (rv >> DATAPOS);// & 0x0FFF;	// 12 bit ADC
 }
 #endif /* WITHSPIHW || WITHSPISW */
-
-
-/* top left raw data values */
-static uint_fast16_t xrawmin = 70;
-static uint_fast16_t yrawmin = 3890;
-/* bottom right raw data values */
-static uint_fast16_t xrawmax = 3990;
-static uint_fast16_t yrawmax = 150;
 
 static uint_fast16_t
 tcsnormalize(
@@ -125,10 +126,17 @@ tcsnormalize(
 
 uint_fast8_t xpt2046_getxy(uint_fast16_t * xr, uint_fast16_t * yr)
 {
+	/* top left raw data values */
+	static uint_fast16_t xrawmin = 70;
+	static uint_fast16_t yrawmin = 3890;
+	/* bottom right raw data values */
+	static uint_fast16_t xrawmax = 3990;
+	static uint_fast16_t yrawmax = 150;
+
 	uint_fast16_t x, y;
 
-	x = xpt2046_read(targettsc, 0x00);
-	y = xpt2046_read(targettsc, 0x00);
+	x = xpt2046_read(targettsc1, 0x00);
+	y = xpt2046_read(targettsc1, 0x00);
 
 	* xr = tcsnormalize(x, xrawmin, xrawmax, DIM_X - 1);
 	* yr = tcsnormalize(y, yrawmin, yrawmax, DIM_Y - 1);
@@ -136,9 +144,21 @@ uint_fast8_t xpt2046_getxy(uint_fast16_t * xr, uint_fast16_t * yr)
 	return 0;
 }
 
+// MKS Robin Mini/firmware/Marlin2.0-MKS-Robin_mini/Marlin/src/HAL/HAL_STM32F1/xpt2046.h
+
 void xpt2046_initialize(void)
 {
-	xpt2046_read(targettsc, 0x00);
+	PRINTF("xpt2046_initialize start.\n");
+	for (;;)
+	{
+		unsigned x = xpt2046_read(targettsc1, XPT2046_X | XPT2046_DFR_MODE);
+		unsigned y = xpt2046_read(targettsc1, XPT2046_Y | XPT2046_DFR_MODE);
+		unsigned z1 = xpt2046_read(targettsc1, XPT2046_Z1 | XPT2046_DFR_MODE);
+		//unsigned z2 = xpt2046_read(targettsc1, XPT2046_Z2);
+		int st = z1 > XPT2046_Z1_THRESHOLD;
+		PRINTF("xpt2046: x=%08X, y=%08X z1=%08X, st=%d\n", x, y, z1, st);
+	}
+	PRINTF("xpt2046_initialize done.\n");
 }
 
 #endif /* defined (TSC1_TYPE) && (TSC1_TYPE == TSC_TYPE_XPT2046) */
--- src/touch/xpt2046.h
+++ src/touch/xpt2046.h
@@ -8,6 +8,33 @@
 #ifndef SRC_TOUCH_XPT2046_H_
 #define SRC_TOUCH_XPT2046_H_
 
+/*
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ */
+									  
+// Relies on XPT2046-compatible mode of ADS7843,
+// hence no Z1 / Z2 measurements are possible.
+
+// https://github.com/MarlinFirmware/Marlin/blob/2.0.x/Marlin/src/lcd/touch/touch_buttons.cpp
+
+#define XPT2046_DFR_MODE 0x00
+#define XPT2046_SER_MODE 0x04
+#define XPT2046_CONTROL  0x80
+#define XPT2046_PD0  	0x01	// full-power (PD0 = 1)
+#define XPT2046_PD1  	0x02	
+
+enum XPTCoordinate {
+  XPT2046_X  = 1 * 0x10,	// Длинная сторона на 320x240
+  XPT2046_Y  = 5 * 0x10,	// Короткая сторона на 320x240
+  XPT2046_Z1 = 3 * 0x10,
+  XPT2046_Z2 = 4 * 0x10
+};
+
+#ifndef XPT2046_Z1_THRESHOLD
+  #define XPT2046_Z1_THRESHOLD 10
+#endif
+
 void xpt2046_initialize(void);
 uint_fast8_t xpt2046_getxy(uint_fast16_t * xr, uint_fast16_t * yr);
 
