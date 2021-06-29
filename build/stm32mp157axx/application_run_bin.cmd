923167b1b55ddfc6963f522d62bf30ac88393fbe
ua1arn <mgs2001@mail.ru>
2021-06-29 03:24:29 +0300

    Переход на dfu-util-0.10-binaries: корректировки Makefile

--- build/stm32mp157axx/application_run_bin.cmd
+++ build/stm32mp157axx/application_run_bin.cmd
@@ -1,3 +1,3 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
-dfu-util.exe --alt 2 --transfer-size 4096 --dfuse-address 0xC0000000:force --download "tc1_stm32mp157axx_app.stm32"
-dfu-util.exe --alt 2 --detach
+dfu-util --alt 2 --transfer-size 4096 --dfuse-address 0xC0000000 --download "tc1_stm32mp157axx_app.stm32"
+dfu-util --alt 2 --detach
