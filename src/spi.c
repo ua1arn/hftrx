78068293897e3f585a5592b54b0afb404b8fb0ea
ua1arn <mgs2001@mail.ru>
2021-09-29 23:05:54 +0300

    stm32mp1: qspi - style

--- src/spi.c
+++ src/spi.c
@@ -980,7 +980,7 @@ void spidf_initialize(void)
 	//SPIDF_HARDINITIALIZE();
 
 	//PRINTF("QUADSPI->IPIDR=%08x\n", QUADSPI->IPIDR);
-
+	const unsigned qspipre = 0x04uL;
 	QUADSPI->CR &= ~ QUADSPI_CR_EN_Msk;
 	(void) QUADSPI->CR;
 
@@ -997,7 +997,7 @@ void spidf_initialize(void)
 
 	QUADSPI->CR = ((QUADSPI->CR & ~ (QUADSPI_CR_PRESCALER_Msk | QUADSPI_CR_FTHRES_Msk))) |
 		(0x00uL << QUADSPI_CR_FTHRES_Pos) | // FIFO threshold level - one byte
-		(0x00uL << QUADSPI_CR_PRESCALER_Pos) | // 1: FCLK = Fquadspi_ker_ck/2
+		((unsigned long) qspipre << QUADSPI_CR_PRESCALER_Pos) | // 1: FCLK = Fquadspi_ker_ck/2
 		0;
 	(void) QUADSPI->CR;
 
