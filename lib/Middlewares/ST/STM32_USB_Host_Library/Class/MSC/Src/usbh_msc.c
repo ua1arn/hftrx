baaeae6cdbfd30833e41af07f2600e59338ec748
ua1arn <mgs2001@mail.ru>
2021-10-02 00:44:50 +0300

    comments, mistype

--- lib/Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Src/usbh_msc.c
+++ lib/Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Src/usbh_msc.c
@@ -186,7 +186,7 @@ static USBH_StatusTypeDef USBH_MSC_InterfaceInit(USBH_HandleTypeDef *phost)
     USBH_DbgLog("Cannot allocate memory for MSC Handle");
     return USBH_FAIL;
   }
-  USBH_UsrLog("CMSC Handle at %p", & tt);
+  USBH_UsrLog("MSC Handle at %p", & tt);
 
   /* Initialize msc handler */
   (void)USBH_memset(MSC_Handle, 0, sizeof(MSC_HandleTypeDef));
