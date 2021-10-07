b7a1440ce0d4acbef2d97b37f4c4f9dd28f280ce
ua1arn <mgs2001@mail.ru>
2021-10-07 15:46:57 +0300

    ehci: save state

--- src/usb/usbh_ehci.c
+++ src/usb/usbh_ehci.c
@@ -275,8 +275,18 @@ static void asynclist_item2(EHCI_HCTypeDef * hc, volatile struct ehci_queue_head
 			EHCI_CAP_TT_HUB(hc->tt_hubaddr) |
 			EHCI_CAP_TT_PORT(hc->tt_prtaddr);
 
-	if (hc->ep_type == EP_TYPE_INTR)
-		cap |= EHCI_CAP_SPLIT_SCHED_DEFAULT;
+	if (hc->ep_type == EP_TYPE_INTR) {
+		unsigned i;
+		for (i = 0; i < 8; ++ i) {
+			cap |= EHCI_CAP_INTR_SCHED ( i );
+		}
+
+	}
+
+	if (hc->tt_hubaddr != 0 && hc->tt_hubaddr != 0) {
+		if (hc->ep_type == EP_TYPE_INTR)
+			cap |= EHCI_CAP_SPLIT_SCHED_DEFAULT;
+	}
 
 	// RL, C, Maximum Packet Length, H, dtc, EPS, EndPt, I, Device Address
 	p->chr = cpu_to_le32(chr | EHCI_CHR_HEAD * Head);
