7c88d17c606a597756ac825f991d93a901c2bcba
ua1arn <mgs2001@mail.ru>
2021-10-09 04:31:06 +0300

    custom config update

--- boards/arm_stm32f4xx_tqfp144_ctlstyle_storch_v3_hansolo.h
+++ boards/arm_stm32f4xx_tqfp144_ctlstyle_storch_v3_hansolo.h
@@ -163,6 +163,7 @@
 	#define WITHDATAMODE	1	/* управление с клавиатуры передачей с USB AUDIO канала */
 
 	// +++ Одна из этих строк определяет тип дисплея, для которого компилируется прошивка
+	//#define LCDMODE_DUMMY	1		/* заглушка - без отображения */
 	//#define LCDMODE_HARD_SPI	1	/* LCD over SPI line */
 	//#define LCDMODE_WH2002	1	/* тип применяемого индикатора 20*2, возможно вместе с LCDMODE_HARD_SPI */
 	//#define LCDMODE_WH1602	1	/* тип применяемого индикатора 16*2 */
