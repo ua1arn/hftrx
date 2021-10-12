855b8a6e89fc43905211f9cf21e100a604fac1d0
ua1arn <mgs2001@mail.ru>
2021-10-12 19:46:16 +0300

    tests: decode barometric sensor

--- src/tests.c
+++ src/tests.c
@@ -6335,12 +6335,12 @@ void hightests(void)
 		        float Z = atan2f(t3, t4);
 
 		        //PRINTF("X=%f, Y=%f, Z=%f\n", X * (180 / M_PI), Y * (180 / M_PI), Z * (180 / M_PI));
-		        PRINTF("HABS=%f, ROLL=%f, PITCH=%f, YAW=%f, BAROM=%08lX\n",
+		        PRINTF("HABS=%f, ROLL=%f, PITCH=%f, YAW=%f, BAROM=%f\n",
 						(int32_t) module_read32(0x01, 0x14) / 65536.0f,
 		        		ROLL_C23_OUT / 32768.0f * 180.0f,
 						PITCH_C31_OUT / 32768.0f * 180.0f,
 						YAW_C32_OUT / 32768.0f * 180.0f,
-						module_read32(0x00, 0x2E)
+						(int32_t) module_read32(0x00, 0x2E) / 65536.0f / 25000
 						);
 
 			}
