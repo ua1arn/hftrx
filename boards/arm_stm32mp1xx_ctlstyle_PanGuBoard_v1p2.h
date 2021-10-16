128cdbb9b87fbd990caf25edfdbd7abc9eb91892
ua1arn <mgs2001@mail.ru>
2021-10-16 13:39:20 +0300

    custom config update

--- boards/arm_stm32mp1xx_ctlstyle_PanGuBoard_v1p2.h
+++ boards/arm_stm32mp1xx_ctlstyle_PanGuBoard_v1p2.h
@@ -321,7 +321,9 @@
 	//#define WITHSMPSYSTEM	1	/* разрешение поддержки SMP, Symmetric Multiprocessing */
 	#define WITHNESTEDINTERRUPTS	1	/* используется при наличии real-time части. */
 	//#define WITHUSEMALLOC	1	/* разрешение поддержки malloc/free/calloc/realloc */
-	#define WITHUSESDCARD		1	// Включение поддержки SD CARD
+	//#define WITHUSESDCARD		1	// Включение поддержки SD CARD
+	//#define WITHUSESDCARD		1	// Включение поддержки SD CARD
+	#define WITHUSEUSBFLASH		1	// Включение поддержки USB memory stick
 
 #else /* WITHISBOOTLOADER */
 
