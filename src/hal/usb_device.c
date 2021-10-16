ae6fa9eaca7f9760fb0b5196da8fa8774b63fbb2
ua1arn <mgs2001@mail.ru>
2021-10-16 23:33:51 +0300

    bootloaders update. add USB device reset

--- src/hal/usb_device.c
+++ src/hal/usb_device.c
@@ -281,7 +281,7 @@ void MX_USB_HOST_Process(void)
 
 void board_usb_initialize(void)
 {
-	//PRINTF("board_usb_initialize\n");
+	PRINTF("board_usb_initialize\n");
 #if WITHUSBDEV_HSDESC
 	usbd_descriptors_initialize(1);
 
@@ -296,7 +296,7 @@ void board_usb_initialize(void)
 #if defined (WITHUSBHW_HOST) || defined (WITHUSBHW_EHCI)
 	MX_USB_HOST_Init();
 #endif /* defined (WITHUSBHW_HOST) || defined (WITHUSBHW_EHCI) */
-	//PRINTF("board_usb_initialize done\n");
+	PRINTF("board_usb_initialize done\n");
 }
 
 void board_usb_deinitialize(void)
