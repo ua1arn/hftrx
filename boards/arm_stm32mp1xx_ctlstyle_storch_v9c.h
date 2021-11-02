37910d7f7dd076f7df9333bb043d80a8e7b33f80
ua1arn <mgs2001@mail.ru>
2021-11-02 15:21:05 +0300

    costom config update: use 512 MB RAM

--- boards/arm_stm32mp1xx_ctlstyle_storch_v9c.h
+++ boards/arm_stm32mp1xx_ctlstyle_storch_v9c.h
@@ -68,8 +68,8 @@
 			#define PLL2DIVP	2	// AXISS_CK div2=minimum 528/2 = 264 MHz PLL2 selected as AXI sub-system clock (pll2_p_ck) - 266 MHz max for all CPU revisions
 			#define PLL2DIVQ	1	// GPU clock divider = 528 MHz - 533 MHz max for all CPU revisions
 			#define PLL2DIVR	1	// DDR clock divider = 528 MHz
-			//#include "src/sdram/stm32mp15-mx_2G_x2.dtsi"	// 2x128k*16
-			#include "src/sdram/stm32mp15-mx_4G_x2.dtsi"	// 2x256k*16 2 x MT41K256M16TW-107 IT:P (FBGA Code D9SHG)
+			#include "src/sdram/stm32mp15-mx_2G_x2.dtsi"	// 2x128k*16
+			//#include "src/sdram/stm32mp15-mx_4G_x2.dtsi"	// 2x256k*16 2 x MT41K256M16TW-107 IT:P (FBGA Code D9SHG)
 
 			// PLL3_800
 
@@ -100,8 +100,8 @@
 			#define PLL2DIVP	2	// div2=minimum PLL2 selected as AXI sub-system clock (pll2_p_ck)
 			#define PLL2DIVQ	1	// GPU clock divider
 			#define PLL2DIVR	1	// DDR clock divider
-			//#include "src/sdram/stm32mp15-mx_2G_x2.dtsi"	// 2x128k*16
-			#include "src/sdram/stm32mp15-mx_4G_x2.dtsi"	// 2x256k*16 2 x MT41K256M16TW-107 IT:P (FBGA Code D9SHG)
+			#include "src/sdram/stm32mp15-mx_2G_x2.dtsi"	// 2x128k*16
+			//#include "src/sdram/stm32mp15-mx_4G_x2.dtsi"	// 2x256k*16 2 x MT41K256M16TW-107 IT:P (FBGA Code D9SHG)
 
 			// PLL3_800
 			// pll3_p_ck -> mcuss_ck - 209 MHz Max
