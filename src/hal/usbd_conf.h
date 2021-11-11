0404637dd622af6ff919be03ddff70fba646d555
ua1arn <mgs2001@mail.ru>
2021-11-11 13:51:05 +0300

    usbd config update

--- src/hal/usbd_conf.h
+++ src/hal/usbd_conf.h
@@ -96,7 +96,7 @@
 /*---------- -----------*/
 #define USBD_LPM_ENABLED     0U
 /*---------- -----------*/
-#define USBD_SELF_POWERED     1U
+#define USBD_SELF_POWERED     0U
 
 /****************************************/
 /* #define for FS and HS identification */
