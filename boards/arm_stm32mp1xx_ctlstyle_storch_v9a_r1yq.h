b87bab1f6d75a73d011615bf06e5fca2c991cbc3
ua1arn <mgs2001@mail.ru>
2022-03-13 02:11:09 +0300

    custom config update

--- boards/arm_stm32mp1xx_ctlstyle_storch_v9a_r1yq.h
+++ boards/arm_stm32mp1xx_ctlstyle_storch_v9a_r1yq.h
@@ -265,7 +265,7 @@
 	//#define LCDMODE_V5A	1	/* только главный экран с тремя видеобуферами 32 бит ARGB888, без PIP */
 
 	//#define WITHFLATLINK 1	/* Работа с TFT панелью через преобразователь RGB->FlatLink SN75LVDS83B	*/
-	//#define WITHLCDDEMODE	1	/* DE MODE: MODE="1", VS and HS must pull high. */
+	#define WITHLCDDEMODE	1	/* DE MODE: MODE="1", VS and HS must pull high. */
 
 #elif 1
 
