5e286d201228a5b90e24f7b01dd98283da07c882
ua1arn <mgs2001@mail.ru>
2022-03-16 15:08:55 +0300

    save state (removed absolute path to Vitis)

--- build/xc7Z010/qspi_write_app.cmd
+++ build/xc7Z010/qspi_write_app.cmd
@@ -1,1 +1,1 @@
-D:\Xilinx\Vitis\2021.2\bin\program_flash -f tc1_xc7z010_app.xyl32 -verify  -offset 0x80000 -flash_type qspi-x4-single -fsbl executable.elf
+program_flash -f tc1_xc7z010_app.xyl32 -verify  -offset 0x80000 -flash_type qspi-x4-single -fsbl executable.elf
