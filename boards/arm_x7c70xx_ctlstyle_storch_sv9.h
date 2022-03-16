8a1b8a4e220e8c838a62e89a20f52ded5b8b5710
ua1arn <mgs2001@mail.ru>
2022-03-16 21:46:21 +0300

    custom config update

--- boards/arm_x7c70xx_ctlstyle_storch_sv9.h
+++ boards/arm_x7c70xx_ctlstyle_storch_sv9.h
@@ -153,7 +153,7 @@
 	#define LCDMODE_AT070TN90 1	/* AT070TN90 panel (800*480) - 7" display */
 	//#define LCDMODE_AT070TNA2 1	/* AT070TNA2 panel (1024*600) - 7" display */
 	//#define WITHLCDDEMODE	1	/* DE MODE: MODE="1", VS and HS must pull high. */
-	#define WITHTFT_OVER_LVDS	1	// LVDS receiver THC63LVDF84B
+	//#define WITHTFT_OVER_LVDS	1	// LVDS receiver THC63LVDF84B
 
 	//#define LCDMODE_WH2002	1	/* тип применяемого индикатора 20*2, возможно вместе с LCDMODE_HARD_SPI */
 	//#define LCDMODE_WH1602	1	/* тип применяемого индикатора 16*2 */
