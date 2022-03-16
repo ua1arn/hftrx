843d2361a4e0deea2777e50e91eddd1f64c5492a
ua1arn <mgs2001@mail.ru>
2022-03-16 18:26:10 +0300

    zynq jtag: limit freq to 750 kHz

--- build/xc7Z010/qspi_write_boot.cmd
+++ build/xc7Z010/qspi_write_boot.cmd
@@ -1,1 +1,2 @@
-program_flash -f BOOT.bin -verify  -offset 0x00000 -flash_type qspi-x4-single -fsbl executable.elf
+# dep acces point jsn-DLC10-13724327082a01-4ba00477-0
+program_flash -f BOOT.bin -verify  -offset 0x00000 -flash_type qspi-x4-single -fsbl executable.elf -frequency 750000
