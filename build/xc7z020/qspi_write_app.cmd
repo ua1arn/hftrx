bcf0e747af0348bb5a180a445390673bfe1f6b1a
ua1arn <mgs2001@mail.ru>
2022-03-25 16:44:26 +0300

    zynq: 7020 config update

---
+++ build/xc7z020/qspi_write_app.cmd
@@ -0,0 +1,1 @@
+program_flash -f tc1_xc7z010_app_xyl32.bin -offset 0x80000 -flash_type qspi-x4-single -fsbl fsbl_Y7Z020.elf
---
+++ build/xc7z020/qspi_write_boot.cmd
@@ -0,0 +1,1 @@
+program_flash -f BOOT.bin -verify -offset 0x00000 -flash_type qspi-x4-single -fsbl fsbl_Y7Z020.elf
