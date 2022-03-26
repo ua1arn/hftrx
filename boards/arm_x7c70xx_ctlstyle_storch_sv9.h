7e194d2aed6ae53b4fcb2ab8ae905adc22d2b7d5
ua1arn <mgs2001@mail.ru>
2022-03-26 11:45:57 +0300

    Custom config update: RTC enabled

--- boards/arm_x7c70xx_ctlstyle_storch_sv9.h
+++ boards/arm_x7c70xx_ctlstyle_storch_sv9.h
@@ -476,6 +476,7 @@
 	//#define DDS1_TYPE DDS_TYPE_AD9951
 	//#define PLL1_TYPE PLL_TYPE_ADF4001
 	//#define DDS2_TYPE DDS_TYPE_AD9834
+	#define RTC1_TYPE RTC_TYPE_DS1305	/* MAXIM DS1305EN RTC clock chip with SPI interface */
 	//#define RTC1_TYPE RTC_TYPE_M41T81	/* ST M41T81M6 RTC clock chip with I2C interface */
 	//#define RTC1_TYPE RTC_TYPE_STM32F4xx	/* STM32F4xx/STM32F7xx internal RTC peripherial */
 	#define TSC1_TYPE TSC_TYPE_STMPE811	/* touch screen controller */
