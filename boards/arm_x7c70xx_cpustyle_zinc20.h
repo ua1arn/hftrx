575566f92297f7dd847173c99a6e2146a3ae78fc
ua1arn <mgs2001@mail.ru>
2022-04-07 14:16:28 +0300

    custom config update

--- boards/arm_x7c70xx_cpustyle_zinc20.h
+++ boards/arm_x7c70xx_cpustyle_zinc20.h
@@ -868,7 +868,7 @@
 	// MIO_PIN_VALUE(disablercvr, pullup, io_type, speed, l3_sel, l2_sel, l1_sel, l0_sel, tri_enable)
 	#define USB_ULPI_INITIALIZE() do { \
 		enum { IOTYPE = GPIO_IOTYPE_501 }; \
-		enum { IOTYPE_RST = GPIO_IOTYPE_501 }; \
+		enum { IOTYPE_RST = GPIO_IOTYPE_500 }; \
 		enum { L3_SEL = 0x00, L2_SEL = 0x00, L1_SEL = 0x01, L0_SEL = 0x00 }; \
 		const portholder_t pinmode_ulpi_data = MIO_PIN_VALUE(1, 0, IOTYPE, 1, L3_SEL, L2_SEL, L1_SEL, L0_SEL, 0); \
 		const portholder_t pinmode_ulpi_input = MIO_PIN_VALUE(1, 0, IOTYPE, 1, L3_SEL, L2_SEL, L1_SEL, L0_SEL, 1); \
