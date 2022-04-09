9e9ee1e2e7a0721d84fbab15297bd4bdf8a7a58c
ua1arn <mgs2001@mail.ru>
2022-04-09 23:13:38 +0300

    Custom config update: no gui (broken glass on touch screen)

--- boards/arm_x7c70xx_ctlstyle_storch_sv9.h
+++ boards/arm_x7c70xx_ctlstyle_storch_sv9.h
@@ -315,7 +315,7 @@
 		#define WITHSPECBETA_DEFAULT	30
 		#define WITHAFSPECTRE		1		/* показ спктра прослушиваемого НЧ сигнала. */
 		#define WITHFFTSIZEAF 		512		/* Отображение спектра НЧ сигнвлв */
-		#if 1
+		#if 0
 			#define WITHDISPLAY_FPS		30
 			#define WITHDISPLAYSWR_FPS	30
 			#define WITHTOUCHGUI		1
