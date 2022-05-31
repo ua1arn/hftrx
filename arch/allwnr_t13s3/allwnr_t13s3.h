192496eeeebe28992d0bb47be8d2de7fff05ced2
ua1arn <mgs2001@mail.ru>
2022-05-31 12:56:32 +0300

    formatting, IRQ numbers update

--- arch/allwnr_t13s3/allwnr_t13s3.h
+++ arch/allwnr_t13s3/allwnr_t13s3.h
@@ -12,37 +12,37 @@
  * @brief STM32MP1XX Interrupt Number Definition, according to the selected device
  *        in @ref Library_configuration_section
  */
- typedef enum IRQn
- {
-   /******  Cortex-A Processor Specific Interrupt Numbers ***************************************************************/
-   /* Software Generated Interrupts                                                                                     */
-   SGI0_IRQn                        =  0,     /*!< Software Generated Interrupt  0                                      */
-   SGI1_IRQn                        =  1,     /*!< Software Generated Interrupt  1                                      */
-   SGI2_IRQn                        =  2,     /*!< Software Generated Interrupt  2                                      */
-   SGI3_IRQn                        =  3,     /*!< Software Generated Interrupt  3                                      */
-   SGI4_IRQn                        =  4,     /*!< Software Generated Interrupt  4                                      */
-   SGI5_IRQn                        =  5,     /*!< Software Generated Interrupt  5                                      */
-   SGI6_IRQn                        =  6,     /*!< Software Generated Interrupt  6                                      */
-   SGI7_IRQn                        =  7,     /*!< Software Generated Interrupt  7                                      */
-   SGI8_IRQn                        =  8,     /*!< Software Generated Interrupt  8                                      */
-   SGI9_IRQn                        =  9,     /*!< Software Generated Interrupt  9                                      */
-   SGI10_IRQn                       = 10,     /*!< Software Generated Interrupt 10                                      */
-   SGI11_IRQn                       = 11,     /*!< Software Generated Interrupt 11                                      */
-   SGI12_IRQn                       = 12,     /*!< Software Generated Interrupt 12                                      */
-   SGI13_IRQn                       = 13,     /*!< Software Generated Interrupt 13                                      */
-   SGI14_IRQn                       = 14,     /*!< Software Generated Interrupt 14                                      */
-   SGI15_IRQn                       = 15,     /*!< Software Generated Interrupt 15                                      */
-   /* Private Peripheral Interrupts                                                                                     */
-   VirtualMaintenanceInterrupt_IRQn = 25,     /*!< Virtual Maintenance Interrupt                                        */
-   HypervisorTimer_IRQn             = 26,     /*!< Hypervisor Timer Interrupt                                           */
-   VirtualTimer_IRQn                = 27,     /*!< Virtual Timer Interrupt                                              */
-   Legacy_nFIQ_IRQn                 = 28,     /*!< Legacy nFIQ Interrupt                                                */
-   SecurePhysicalTimer_IRQn         = 29,     /*!< Secure Physical Timer Interrupt                                      */
-   NonSecurePhysicalTimer_IRQn      = 30,     /*!< Non-Secure Physical Timer Interrupt                                  */
-   Legacy_nIRQ_IRQn                 = 31,     /*!< Legacy nIRQ Interrupt                                                */
-   /******  STM32 specific Interrupt Numbers ****************************************************************************/
+typedef enum IRQn
+{
+	/******  Cortex-A Processor Specific Interrupt Numbers ***************************************************************/
+	/* Software Generated Interrupts                                                                                     */
+	SGI0_IRQn                        =  0,     /*!< Software Generated Interrupt  0                                      */
+	SGI1_IRQn                        =  1,     /*!< Software Generated Interrupt  1                                      */
+	SGI2_IRQn                        =  2,     /*!< Software Generated Interrupt  2                                      */
+	SGI3_IRQn                        =  3,     /*!< Software Generated Interrupt  3                                      */
+	SGI4_IRQn                        =  4,     /*!< Software Generated Interrupt  4                                      */
+	SGI5_IRQn                        =  5,     /*!< Software Generated Interrupt  5                                      */
+	SGI6_IRQn                        =  6,     /*!< Software Generated Interrupt  6                                      */
+	SGI7_IRQn                        =  7,     /*!< Software Generated Interrupt  7                                      */
+	SGI8_IRQn                        =  8,     /*!< Software Generated Interrupt  8                                      */
+	SGI9_IRQn                        =  9,     /*!< Software Generated Interrupt  9                                      */
+	SGI10_IRQn                       = 10,     /*!< Software Generated Interrupt 10                                      */
+	SGI11_IRQn                       = 11,     /*!< Software Generated Interrupt 11                                      */
+	SGI12_IRQn                       = 12,     /*!< Software Generated Interrupt 12                                      */
+	SGI13_IRQn                       = 13,     /*!< Software Generated Interrupt 13                                      */
+	SGI14_IRQn                       = 14,     /*!< Software Generated Interrupt 14                                      */
+	SGI15_IRQn                       = 15,     /*!< Software Generated Interrupt 15                                      */
+	/* Private Peripheral Interrupts                                                                                     */
+	VirtualMaintenanceInterrupt_IRQn = 25,     /*!< Virtual Maintenance Interrupt                                        */
+	HypervisorTimer_IRQn             = 26,     /*!< Hypervisor Timer Interrupt                                           */
+	VirtualTimer_IRQn                = 27,     /*!< Virtual Timer Interrupt                                              */
+	Legacy_nFIQ_IRQn                 = 28,     /*!< Legacy nFIQ Interrupt                                                */
+	SecurePhysicalTimer_IRQn         = 29,     /*!< Secure Physical Timer Interrupt                                      */
+	NonSecurePhysicalTimer_IRQn      = 30,     /*!< Non-Secure Physical Timer Interrupt                                  */
+	Legacy_nIRQ_IRQn                 = 31,     /*!< Legacy nIRQ Interrupt                                                */
+	/******  STM32 specific Interrupt Numbers ****************************************************************************/
 
-
+	CPUX_MSGBOX_R = (32),
 	UART0_IRQn = (34), /*  Peripherial */
 	UART1_IRQn = (35), /*  Peripherial */
 	UART2_IRQn = (36), /*  Peripherial */
@@ -87,8 +87,8 @@
 	HSTIME1_IRQn = (88), /*  Peripherial */
 	GPADC_IRQn = (89), /*  Peripherial */
 	THS_IRQn = (90), /*  Peripherial */
-	////TIMER0_IRQn = (91), /*  Peripherial */
-	////TIMER1_IRQn = (92), /*  Peripherial */
+	TIMER0_base_IRQn = (91), /*  Peripherial */
+	TIMER1_base_IRQn = (92), /*  Peripherial */
 	LRADC_IRQn = (93), /*  Peripherial */
 	TPADC_IRQn = (94), /*  Peripherial */
 	WATCHDOG_IRQn = (95), /*  Peripherial */
@@ -120,11 +120,12 @@
 	DSP_WDG_IRQn = (154), /*  Peripherial */
 	DSP_MBOX_RISCV_W_IRQn = (155), /*  Peripherial */
 	DSP_TZMA_IRQn = (157), /*  Peripherial */
+	/* CPUs related */
 	NMI_IRQn = (168), /*  Peripherial */
 	PPU_IRQn = (169), /*  Peripherial */
 	TWD_IRQn = (170), /*  Peripherial */
-	////TIMER0_IRQn = (172), /*  Peripherial */
-	////TIMER1_IRQn = (173), /*  Peripherial */
+	TIMER0_IRQn = (172), /*  Peripherial */
+	TIMER1_IRQn = (173), /*  Peripherial */
 	TIMER2_IRQn = (174), /*  Peripherial */
 	TIMER3_IRQn = (175), /*  Peripherial */
 	ALARM0_IRQn = (176), /*  Peripherial */
@@ -221,8 +222,8 @@
 	GPIOG14_IRQn = (384 + 14), /*  Peripherial */
 	GPIOG15_IRQn = (384 + 15), /*  Peripherial */
 
-   MAX_IRQ_n,
-   Force_IRQn_enum_size             = 1048    /* Dummy entry to ensure IRQn_Type is more than 8 bits. Otherwise GIC init loop would fail */
+	MAX_IRQ_n,
+	Force_IRQn_enum_size             = 1048    /* Dummy entry to ensure IRQn_Type is more than 8 bits. Otherwise GIC init loop would fail */
  } IRQn_Type;
 
 /* =========================================================================================================================== */
