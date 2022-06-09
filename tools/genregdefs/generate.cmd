936e7bed47757f9c9a0ac67d015976292cf8e3e8
ua1arn <mgs2001@mail.ru>
2022-06-09 21:23:55 +0300

    cmsis hearer utility added

---
+++ tools/genregdefs/generate.cmd
@@ -0,0 +1,27 @@
+rem .\Debug\genregdefs.exe i2s_pcm.txt I2S_PCM > tt.txt
+rem .\Debug\genregdefs.exe dmic.txt DMIC > tt.txt
+rem .\Debug\genregdefs.exe owa.txt OWA >> tt.txt
+rem .\Debug\genregdefs.exe audio_codec.txt AUDIO_CODEC >> tt.txt
+
+rem .\Debug\genregdefs.exe twi.txt TWI > tt.txt
+rem .\Debug\genregdefs.exe uart.txt UART >> tt.txt
+rem .\Debug\genregdefs.exe spi.txt SPI >> tt.txt
+rem .\Debug\genregdefs.exe spi_bdi.txt SPI_BDI >> tt.txt
+rem .\Debug\genregdefs.exe uart.txt UART > tt.txt
+
+rem .\Debug\genregdefs.exe cir_rx.txt CIR_RX > tt.txt
+rem .\Debug\genregdefs.exe cir_tx.txt CIR_TX >> tt.txt
+rem .\Debug\genregdefs.exe ledc.txt LEDC >> tt.txt
+rem .\Debug\genregdefs.exe tpadc.txt TPADC >> tt.txt
+rem .\Debug\genregdefs.exe gpadc.txt GPADC >> tt.txt
+
+rem .\Debug\genregdefs.exe rtc.txt RTC > tt.txt
+rem .\Debug\genregdefs.exe iommu.txt IOMMU >> tt.txt
+rem .\Debug\genregdefs.exe ths.txt THS >> tt.txt
+rem .\Debug\genregdefs.exe timer.txt TIMER >> tt.txt
+rem .\Debug\genregdefs.exe hstimer.txt HSTIMER >> tt.txt
+
+
+.\Debug\genregdefs.exe can.txt CAN > tt.txt
+
+
