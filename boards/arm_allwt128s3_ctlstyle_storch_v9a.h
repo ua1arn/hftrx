6b72c0b50508b9d1f97540f4cdb48abee2b5707b
ua1arn <mgs2001@mail.ru>
2022-06-10 20:11:56 +0300

    Allwinner: test @1.2 GHz

--- boards/arm_allwt128s3_ctlstyle_storch_v9a.h
+++ boards/arm_allwt128s3_ctlstyle_storch_v9a.h
@@ -29,8 +29,9 @@
 	#define WITHCPUXTAL 24000000uL	/* На процессоре установлен кварц 24.000 МГц */
 	//#define WITHCPUXOSC 24000000uL	/* На процессоре установлен генератор 24.000 МГц */
 
+	#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
 	//#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
-	#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+	//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
 
 	#if 1//WITHISBOOTLOADER
 		// Варианты конфигурации тактирования
