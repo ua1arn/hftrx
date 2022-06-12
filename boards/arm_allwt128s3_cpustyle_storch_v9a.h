2a6507e7058b85e1430a91a4cee3339b82ddac27
ua1arn <mgs2001@mail.ru>
2022-06-12 11:55:04 +0300

    Renames

--- boards/arm_allwt128s3_cpustyle_storch_v9a.h
+++ boards/arm_allwt128s3_cpustyle_storch_v9a.h
@@ -129,7 +129,7 @@
 //	#define WITHUSBHOST_DMAENABLE 1
 
 	#define WITHEHCIHW	1	/* USB_EHCI controller */
-	#define WITHUSBHW_EHCI		EHCI1
+	#define WITHUSBHW_EHCI		USB1_EHCI
 	#define WITHEHCIHW_EHCIPORT 0	// 0 - use 1st PHY port, 1 - 2nd PHY port (shared with USB_OTG_HS). See also USBPHYC_MISC_SWITHOST_VAL
 
 	#define WITHCAT_CDC		1	/* использовать виртуальный последовательный порт на USB соединении */
