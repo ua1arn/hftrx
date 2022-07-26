b3ee1198b8095a49302b6899cb47de3192056a73
ua1arn <mgs2001@mail.ru>
2022-07-26 21:28:47 +0300

    mistype

--- src/main.c
+++ src/main.c
@@ -10750,7 +10750,7 @@ updateboardZZZ(
 		#if WITHUSBUAC
 			board_set_uacplayer((gtx && gdatamode) || guacplayer);	/* режим прослушивания выхода компьютера в наушниках трансивера - отладочный режим */
 			#if WITHRTS96 || WITHRTS192 || WITHTRANSPARENTIQ
-				glob_swaprts(gswapiq);	/* Поменять местами I и Q сэмплы в потоке RTS96 */
+				board_set_swaprts(gswapiq);	/* Поменять местами I и Q сэмплы в потоке RTS96 */
 			#endif /* WITHRTS96 || WITHRTS192 || WITHTRANSPARENTIQ */
 			#if WITHTX
 				board_set_datavox(gdatavox);	/* автоматический переход на передачу при появлении звука со стороны компьютера */
