ad2123c2abf7a19679314f84e4a08cbf7b2db8d9
ua1arn <mgs2001@mail.ru>
2022-08-10 12:40:43 +0300

    Renesas: fix build, style

--- inc/clocks.h
+++ inc/clocks.h
@@ -355,31 +355,10 @@ calcdivider(
 		R7S721_RSPI_SPBR_WIDTH = 8,		R7S721_RSPI_SPBR_TAPS = (16 | 8 | 4 | 2),
 	};
 
-	// scemr:
-	// b0=1: 1: Base clock frequency is 8 times the bit rate,
-	// b0=0: 0: Base clock frequency is 16 times the bit rate
-	// scmsr:
-	// b1..b0: 0: /1, 1: /4, 2: /16, 3: /64
-	enum
-	{
-		SCEMR_x16 = 0x00,	// ABCS=0
-		SCEMR_x8 = 0x01,	// ABCS=1
-		SCSMR_DIV1 = 0x00,
-		SCSMR_DIV4 = 0x01,
-		SCSMR_DIV16 = 0x02,
-		SCSMR_DIV64 = 0x03,
-	};
-	static const FLASHMEM struct spcr_spsr_tag { uint_fast8_t scemr, scsmr; } scemr_scsmr [] =
-	{
-		{ SCEMR_x8, 	SCSMR_DIV1, },		/* /8 = 8 * 1 */
-		{ SCEMR_x16, 	SCSMR_DIV1, }, 		/* /16 = 16 * 1 */
-		{ SCEMR_x8, 	SCSMR_DIV4, },		/* /32 = 8 * 4 */
-		{ SCEMR_x16, 	SCSMR_DIV4, },		/* /64 = 16 * 4 */
-		{ SCEMR_x8, 	SCSMR_DIV16, }, 	/* /128 = 8 * 16 */
-		{ SCEMR_x16, 	SCSMR_DIV16, }, 	/* /256 = 16 * 16 */
-		{ SCEMR_x8, 	SCSMR_DIV64, },  	/* /512 = 8 * 64 */
-		{ SCEMR_x16, 	SCSMR_DIV64, }, 	/* /1024 = 16 * 64 */
-	};
+	uint_fast32_t
+	calcdivround_p1clock(
+		uint_fast32_t freq		/* требуемая частота на выходе делителя, в герцах. */
+		);
 
 #elif CPUSTYLE_XC7Z || CPUSTYLE_XCZU
 
