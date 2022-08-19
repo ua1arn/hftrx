b089f8935da31a78172e7e9eb26eccd30c8e0494
RA4ASN <ra4asn@rambler.ru>
2022-08-19 21:45:56 +0300

    Revert "style"
    This reverts commit 31f4302f88938700b47291815bb0bf1f8bfe0d56.

--- src/gui/gui_user.c
+++ src/gui/gui_user.c
@@ -4230,13 +4230,13 @@ void parse_ft8_answer(char * str, COLORMAIN_T * color, uint8_t * cq_flag)
 	* cq_flag = 0;
 	char tmpstr [TEXT_ARRAY_SIZE];
 	char * saveptr;
-	char lexem [10][10]; // time; freq; snr; text 2 - 4 pcs
 
 	strcpy(tmpstr, str);
 
+	char lexem [10][10]; // time; freq; snr; text 2 - 4 pcs
 
 	char * l = strtok_r(tmpstr, " ", & saveptr);
-	for (uint8_t i = 0; i < ARRAY_SIZE(lexem); i ++)
+	for (uint8_t i = 0; i < 10; i ++)
 	{
 		if (l == NULL)
 			break;
@@ -4249,7 +4249,7 @@ void parse_ft8_answer(char * str, COLORMAIN_T * color, uint8_t * cq_flag)
 	{
 		* color = COLORMAIN_GREEN;
 		* cq_flag = 1;
-		if (idx_cqcall < ARRAY_SIZE(cq_call))
+		if (idx_cqcall < 6)
 			strcpy(cq_call [idx_cqcall ++], lexem[4]);
 	}
 	else if (! strcmp(lexem[3], gui_nvram.ft8_callsign))
