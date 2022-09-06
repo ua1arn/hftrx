4dec1f6611ac25c310bcd9087624eae2257aea14
ua1arn <mgs2001@mail.ru>
2022-09-06 17:25:59 +0300

    usbd mtp: добавление заглушек

--- src/usb/usbd_mtp.c
+++ src/usb/usbd_mtp.c
@@ -13,11 +13,56 @@
 #include "usbd_core.h"
 #include "usb200.h"
 #include "usbch9.h"
+#include "usbd_mtp.h"
 
+typedef enum
+{
+  DATA_TYPE = 0x00,
+  REP_TYPE = 0x01,
+} MTP_CONTAINER_TYPE;
+
+
+typedef enum
+{
+  READ_FIRST_DATA = 0x00,
+  READ_REST_OF_DATA = 0x01,
+} MTP_READ_DATA_STATUS;
+
+uint8_t USBD_MTP_STORAGE_Init(USBD_HandleTypeDef  *pdev)
+{
+	return 0;
+}
+
+uint8_t USBD_MTP_STORAGE_DeInit(USBD_HandleTypeDef  *pdev)
+{
+	return 0;
+}
+void USBD_MTP_STORAGE_Cancel(USBD_HandleTypeDef  *pdev, MTP_ResponsePhaseTypeDef MTP_ResponsePhase)
+{
+}
+uint8_t USBD_MTP_STORAGE_ReadData(USBD_HandleTypeDef  *pdev)
+{
+	return 0;
+}
+uint8_t USBD_MTP_STORAGE_SendContainer(USBD_HandleTypeDef  *pdev, MTP_CONTAINER_TYPE CONT_TYPE)
+{
+	return 0;
+}
+uint8_t USBD_MTP_STORAGE_ReceiveOpt(USBD_HandleTypeDef  *pdev)
+{
+	return 0;
+}
+uint8_t USBD_MTP_STORAGE_ReceiveData(USBD_HandleTypeDef  *pdev)
+{
+	return 0;
+}
+
+//////////////
 static const uint8_t MTPInEpAdd = USBD_EP_MTP_IN;
 static const uint8_t MTPOutEpAdd = USBD_EP_MTP_OUT;
 static const uint8_t MTPCmdEpAdd = USBD_EP_MTP_INT;
 
+static __ALIGN_BEGIN USBD_MTP_HandleTypeDef mtpHandle __ALIGN_END;
 
 /**
   * @}
@@ -37,19 +82,20 @@ static const uint8_t MTPCmdEpAdd = USBD_EP_MTP_INT;
 static uint8_t USBD_MTP_Init(USBD_HandleTypeDef *pdev, uint_fast8_t cfgidx)
 {
   UNUSED(cfgidx);
-  USBD_MTP_HandleTypeDef *hmtp;
-
-  hmtp = (USBD_MTP_HandleTypeDef *)USBD_malloc(sizeof(USBD_MTP_HandleTypeDef));
-
-  if (hmtp == NULL)
-  {
-    pdev->pClassDataCmsit[pdev->classId] = NULL;
-    return (uint8_t)USBD_EMEM;
-  }
-
-  /* Setup the pClassData pointer */
-  pdev->pClassDataCmsit[pdev->classId] = (void *)hmtp;
-  pdev->pClassData = pdev->pClassDataCmsit[pdev->classId];
+  USBD_MTP_HandleTypeDef * const hmtp = & mtpHandle;
+//  USBD_MTP_HandleTypeDef *hmtp;
+//
+//  hmtp = (USBD_MTP_HandleTypeDef *)USBD_malloc(sizeof(USBD_MTP_HandleTypeDef));
+//
+//  if (hmtp == NULL)
+//  {
+//    pdev->pClassDataCmsit[pdev->classId] = NULL;
+//    return (uint8_t)USBD_EMEM;
+//  }
+//
+//  /* Setup the pClassData pointer */
+//  pdev->pClassDataCmsit[pdev->classId] = (void *)hmtp;
+//  pdev->pClassData = pdev->pClassDataCmsit[pdev->classId];
 
 #ifdef USE_USBD_COMPOSITE
   /* Get the Endpoints addresses allocated for this class instance */
@@ -119,17 +165,17 @@ static uint8_t USBD_MTP_DeInit(USBD_HandleTypeDef *pdev, uint_fast8_t cfgidx)
   /* Close EP Command */
   (void)USBD_LL_CloseEP(pdev, MTPCmdEpAdd);
   pdev->ep_in[MTPCmdEpAdd & 0xFU].is_used = 0U;
-
-  /* Free MTP Class Resources */
-  if (pdev->pClassDataCmsit[pdev->classId] != NULL)
-  {
-    /* De-Init the MTP layer */
-    (void)USBD_MTP_STORAGE_DeInit(pdev);
-
-    (void)USBD_free(pdev->pClassDataCmsit[pdev->classId]);
-    pdev->pClassDataCmsit[pdev->classId] = NULL;
-    pdev->pClassData = NULL;
-  }
+//
+//  /* Free MTP Class Resources */
+//  if (pdev->pClassDataCmsit[pdev->classId] != NULL)
+//  {
+//    /* De-Init the MTP layer */
+//    (void)USBD_MTP_STORAGE_DeInit(pdev);
+//
+//    (void)USBD_free(pdev->pClassDataCmsit[pdev->classId]);
+//    pdev->pClassDataCmsit[pdev->classId] = NULL;
+//    pdev->pClassData = NULL;
+//  }
 
   return (uint8_t)USBD_OK;
 }
@@ -143,7 +189,8 @@ static uint8_t USBD_MTP_DeInit(USBD_HandleTypeDef *pdev, uint_fast8_t cfgidx)
   */
 static uint8_t USBD_MTP_Setup(USBD_HandleTypeDef *pdev, const USBD_SetupReqTypedef *req)
 {
-  USBD_MTP_HandleTypeDef *hmtp = (USBD_MTP_HandleTypeDef *)pdev->pClassDataCmsit[pdev->classId];
+  //USBD_MTP_HandleTypeDef *hmtp = (USBD_MTP_HandleTypeDef *)pdev->pClassDataCmsit[pdev->classId];
+  USBD_MTP_HandleTypeDef * const hmtp = & mtpHandle;
   USBD_StatusTypeDef ret = USBD_OK;
   uint16_t len = 0U;
 
@@ -266,7 +313,8 @@ static uint8_t USBD_MTP_Setup(USBD_HandleTypeDef *pdev, const USBD_SetupReqTyped
 static uint8_t USBD_MTP_DataIn(USBD_HandleTypeDef *pdev, uint_fast8_t epnum)
 {
   UNUSED(epnum);
-  USBD_MTP_HandleTypeDef *hmtp = (USBD_MTP_HandleTypeDef *)pdev->pClassDataCmsit[pdev->classId];
+  //USBD_MTP_HandleTypeDef *hmtp = (USBD_MTP_HandleTypeDef *)pdev->pClassDataCmsit[pdev->classId];
+  USBD_MTP_HandleTypeDef * const hmtp = & mtpHandle;
   uint16_t len;
 
 #ifdef USE_USBD_COMPOSITE
@@ -322,7 +370,8 @@ static uint8_t USBD_MTP_DataIn(USBD_HandleTypeDef *pdev, uint_fast8_t epnum)
 static uint8_t USBD_MTP_DataOut(USBD_HandleTypeDef *pdev, uint_fast8_t epnum)
 {
   UNUSED(epnum);
-  USBD_MTP_HandleTypeDef *hmtp = (USBD_MTP_HandleTypeDef *)pdev->pClassDataCmsit[pdev->classId];
+  //USBD_MTP_HandleTypeDef *hmtp = (USBD_MTP_HandleTypeDef *)pdev->pClassDataCmsit[pdev->classId];
+  USBD_MTP_HandleTypeDef * const hmtp = & mtpHandle;
   uint16_t len;
 
 #ifdef USE_USBD_COMPOSITE
