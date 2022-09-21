53af2b6dd54c06b34eba90cb40a29a0cdd7a2b78
ua1arn <mgs2001@mail.ru>
2022-09-21 20:18:20 +0300

    genregdefs: изменил формат файла с описаниями решистров (теперь ,csv)

--- tools/genregdefs/t113-s3/cmsis_t113s3.cmd
+++ tools/genregdefs/t113-s3/cmsis_t113s3.cmd
@@ -2,87 +2,87 @@ SET CONVERTER=..\Debug\genregdefs.exe
 SET OUTFILE=cmsis_t113s3.h
 
 @echo /* Generated section start */ > %OUTFILE%
-%CONVERTER% cpusubsysctrl.txt >> %OUTFILE%
-%CONVERTER% ccu.txt >> %OUTFILE%
-%CONVERTER% sys_cfg.txt >> %OUTFILE%
-%CONVERTER% uart.txt >> %OUTFILE%
-%CONVERTER% gpio.txt >> %OUTFILE%
-%CONVERTER% gpioint.txt >> %OUTFILE%
-%CONVERTER% gpioblock.txt >> %OUTFILE%
-%CONVERTER% smhc.txt >> %OUTFILE%
-%CONVERTER% i2s_pcm.txt >> %OUTFILE%
-%CONVERTER% dmic.txt >> %OUTFILE%
-%CONVERTER% owa.txt >> %OUTFILE%
-%CONVERTER% audio_codec.txt >> %OUTFILE%
-%CONVERTER% twi.txt >> %OUTFILE%
-%CONVERTER% spi.txt >> %OUTFILE%
-%CONVERTER% cir_rx.txt >> %OUTFILE%
-%CONVERTER% cir_tx.txt >> %OUTFILE%
-%CONVERTER% ledc.txt >> %OUTFILE%
-%CONVERTER% tpadc.txt >> %OUTFILE%
-%CONVERTER% gpadc.txt >> %OUTFILE%
-%CONVERTER% spi_dbi.txt >> %OUTFILE%
-%CONVERTER% ce.txt >> %OUTFILE%
-%CONVERTER% rtc.txt >> %OUTFILE%
-%CONVERTER% iommu.txt >> %OUTFILE%
-%CONVERTER% ths.txt >> %OUTFILE%
-%CONVERTER% timer.txt >> %OUTFILE%
-%CONVERTER% hstimer.txt >> %OUTFILE%
-%CONVERTER% can.txt CAN >> %OUTFILE%
-%CONVERTER% usbehci.txt >> %OUTFILE%
-%CONVERTER% usbotg_fifo.txt >> %OUTFILE%
-%CONVERTER% usbotg.txt >> %OUTFILE%
-%CONVERTER% usbphyc.txt >> %OUTFILE%
+%CONVERTER% cpusubsysctrl.csv >> %OUTFILE%
+%CONVERTER% ccu.csv >> %OUTFILE%
+%CONVERTER% sys_cfg.csv >> %OUTFILE%
+%CONVERTER% uart.csv >> %OUTFILE%
+%CONVERTER% gpio.csv >> %OUTFILE%
+%CONVERTER% gpioint.csv >> %OUTFILE%
+%CONVERTER% gpioblock.csv >> %OUTFILE%
+%CONVERTER% smhc.csv >> %OUTFILE%
+%CONVERTER% i2s_pcm.csv >> %OUTFILE%
+%CONVERTER% dmic.csv >> %OUTFILE%
+%CONVERTER% owa.csv >> %OUTFILE%
+%CONVERTER% audio_codec.csv >> %OUTFILE%
+%CONVERTER% twi.csv >> %OUTFILE%
+%CONVERTER% spi.csv >> %OUTFILE%
+%CONVERTER% cir_rx.csv >> %OUTFILE%
+%CONVERTER% cir_tx.csv >> %OUTFILE%
+%CONVERTER% ledc.csv >> %OUTFILE%
+%CONVERTER% tpadc.csv >> %OUTFILE%
+%CONVERTER% gpadc.csv >> %OUTFILE%
+%CONVERTER% spi_dbi.csv >> %OUTFILE%
+%CONVERTER% ce.csv >> %OUTFILE%
+%CONVERTER% rtc.csv >> %OUTFILE%
+%CONVERTER% iommu.csv >> %OUTFILE%
+%CONVERTER% ths.csv >> %OUTFILE%
+%CONVERTER% timer.csv >> %OUTFILE%
+%CONVERTER% hstimer.csv >> %OUTFILE%
+%CONVERTER% can.csv CAN >> %OUTFILE%
+%CONVERTER% usbehci.csv >> %OUTFILE%
+%CONVERTER% usbotg_fifo.csv >> %OUTFILE%
+%CONVERTER% usbotg.csv >> %OUTFILE%
+%CONVERTER% usbphyc.csv >> %OUTFILE%
 
-%CONVERTER% de.txt >> %OUTFILE% 
-%CONVERTER% di.txt >> %OUTFILE% 
+%CONVERTER% de.csv >> %OUTFILE% 
+%CONVERTER% di.csv >> %OUTFILE% 
 
-%CONVERTER% g2d_top.txt >> %OUTFILE%
-%CONVERTER% g2d_mixer.txt >> %OUTFILE%
-%CONVERTER% g2d_lay.txt >> %OUTFILE%
-%CONVERTER% g2d_ui.txt >> %OUTFILE%
-%CONVERTER% g2d_vsu.txt >> %OUTFILE%
-%CONVERTER% g2d_bld.txt >> %OUTFILE%
-%CONVERTER% g2d_wb.txt >> %OUTFILE%
-%CONVERTER% g2d_rot.txt >> %OUTFILE%
-rem %CONVERTER% g2d.txt >> %OUTFILE%
+%CONVERTER% g2d_top.csv >> %OUTFILE%
+%CONVERTER% g2d_mixer.csv >> %OUTFILE%
+%CONVERTER% g2d_lay.csv >> %OUTFILE%
+%CONVERTER% g2d_ui.csv >> %OUTFILE%
+%CONVERTER% g2d_vsu.csv >> %OUTFILE%
+%CONVERTER% g2d_bld.csv >> %OUTFILE%
+%CONVERTER% g2d_wb.csv >> %OUTFILE%
+%CONVERTER% g2d_rot.csv >> %OUTFILE%
+rem %CONVERTER% g2d.csv >> %OUTFILE%
 
-%CONVERTER% dsi.txt >> %OUTFILE%
-%CONVERTER% display_top.txt >> %OUTFILE%
-%CONVERTER% tcon_lcd.txt >> %OUTFILE%
-%CONVERTER% tve_top.txt >> %OUTFILE%
-%CONVERTER% tve.txt >> %OUTFILE%
+%CONVERTER% dsi.csv >> %OUTFILE%
+%CONVERTER% display_top.csv >> %OUTFILE%
+%CONVERTER% tcon_lcd.csv >> %OUTFILE%
+%CONVERTER% tve_top.csv >> %OUTFILE%
+%CONVERTER% tve.csv >> %OUTFILE%
 
-%CONVERTER% csic_ccu.txt >> %OUTFILE%
-%CONVERTER% csic_top.txt >> %OUTFILE%
-%CONVERTER% csic_parser.txt >> %OUTFILE%
-%CONVERTER% csic_dma.txt >> %OUTFILE%
+%CONVERTER% csic_ccu.csv >> %OUTFILE%
+%CONVERTER% csic_top.csv >> %OUTFILE%
+%CONVERTER% csic_parser.csv >> %OUTFILE%
+%CONVERTER% csic_dma.csv >> %OUTFILE%
 
-%CONVERTER% tvd_adc.txt >> %OUTFILE%
-%CONVERTER% tvd_top.txt >> %OUTFILE%
-%CONVERTER% tvd.txt >> %OUTFILE%
+%CONVERTER% tvd_adc.csv >> %OUTFILE%
+%CONVERTER% tvd_top.csv >> %OUTFILE%
+%CONVERTER% tvd.csv >> %OUTFILE%
 
-rem %CONVERTER% tv_full.txt >> %OUTFILE%
-rem %CONVERTER% tcon_tv.txt >> %OUTFILE%
+rem %CONVERTER% tv_full.csv >> %OUTFILE%
+rem %CONVERTER% tcon_tv.csv >> %OUTFILE%
 
-%CONVERTER% c0_cpux_cfg.txt >> %OUTFILE%
-rem %CONVERTER% c0_cpux_mbist.txt >> %OUTFILE%
-%CONVERTER% ddrphyc.txt >> %OUTFILE%
-%CONVERTER% msi_memc.txt >> %OUTFILE%
-%CONVERTER% sid.txt >> %OUTFILE%
+%CONVERTER% c0_cpux_cfg.csv >> %OUTFILE%
+rem %CONVERTER% c0_cpux_mbist.csv >> %OUTFILE%
+%CONVERTER% ddrphyc.csv >> %OUTFILE%
+%CONVERTER% msi_memc.csv >> %OUTFILE%
+%CONVERTER% sid.csv >> %OUTFILE%
 
-%CONVERTER% usb_ehci_capability.txt >> %OUTFILE%
+%CONVERTER% usb_ehci_capability.csv >> %OUTFILE%
 
-%CONVERTER% spinlock.txt >> %OUTFILE%
+%CONVERTER% spinlock.csv >> %OUTFILE%
 
-%CONVERTER% dmacchannels.txt >> %OUTFILE%
-%CONVERTER% dmac.txt >> %OUTFILE%
+%CONVERTER% dmacchannels.csv >> %OUTFILE%
+%CONVERTER% dmac.csv >> %OUTFILE%
 
-%CONVERTER% pwmchannels.txt >> %OUTFILE%
-%CONVERTER% pwm.txt >> %OUTFILE%
+%CONVERTER% pwmchannels.csv >> %OUTFILE%
+%CONVERTER% pwm.csv >> %OUTFILE%
 
-%CONVERTER% emac_addr.txt >> %OUTFILE%
-%CONVERTER% emac.txt >> %OUTFILE%
+%CONVERTER% emac_addr.csv >> %OUTFILE%
+%CONVERTER% emac.csv >> %OUTFILE%
 
 @echo /* Generated section end */ >> %OUTFILE%
 
