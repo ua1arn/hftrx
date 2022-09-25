0c3e52a64ea9a26f9f98278fe288eea0d76fe48a
ua1arn <mgs2001@mail.ru>
2022-09-25 13:29:47 +0300

    revert some USB CDC experements

--- src/usb/usbd_cdc.c
+++ src/usb/usbd_cdc.c
@@ -384,7 +384,7 @@ static USBD_StatusTypeDef USBD_CDC_DataIn(USBD_HandleTypeDef *pdev, uint_fast8_t
 			{
 				HARDWARE_CDC_ONTXCHAR(offset, pdev);	// при отсутствии данных usbd_cdc_txenabled устанавливается в 0
 			}
-#if 0
+#if 1
 			USBD_LL_Transmit(pdev, USB_ENDPOINT_IN(epnum), cdcXbuffin [offset], cdcXbuffinlevel [offset]);
 			cdcXbuffinlevel [offset] = 0;
 #else
