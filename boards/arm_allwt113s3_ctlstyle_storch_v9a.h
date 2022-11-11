6db48a2b9633f2b70cb9f6f3941aeb5352e162e6
ua1arn <mgs2001@mail.ru>
2022-11-11 17:11:43 +0300

    Custom config update: Allwinner F133-A woork at 1008 MHz

--- boards/arm_allwt113s3_ctlstyle_storch_v9a.h
+++ boards/arm_allwt113s3_ctlstyle_storch_v9a.h
@@ -31,9 +31,15 @@
 	#define WITHCPUXTAL 24000000uL	/* На процессоре установлен кварц 24.000 МГц */
 	//#define WITHCPUXOSC 24000000uL	/* На процессоре установлен генератор 24.000 МГц */
 
-	#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
-	//#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
-	//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+	#if CPUSTYLE_F133
+		//#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
+		#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
+		//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+	#elif CPUSTYLE_T113
+		#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
+		//#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
+		//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+	#endif
 
 	#if 1//WITHISBOOTLOADER
 		// Варианты конфигурации тактирования
