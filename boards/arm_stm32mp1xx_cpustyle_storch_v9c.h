a1f8c3d9d8f0a789d72ba71295ca5c9433e381db
ua1arn <mgs2001@mail.ru>
2022-11-28 23:26:48 +0300

    custom config update

--- boards/arm_stm32mp1xx_cpustyle_storch_v9c.h
+++ boards/arm_stm32mp1xx_cpustyle_storch_v9c.h
@@ -95,8 +95,9 @@
 
 	//#define WITHUSBHID	1	/* HID использовать Human Interface Device на USB соединении */
 	#define WITHUSBDFU	1	/* DFU USB Device Firmware Upgrade support */
-	
 	#define WITHUSBWCID	1
+
+	#define WITHUSBDMTP	1	/* MTP USB Device */
 
 	//#define WITHLWIP 1
 	//#define WITHUSBCDCEEM	1	/* EEM использовать Ethernet Emulation Model на USB соединении */
@@ -167,7 +168,6 @@
 	//#define WITHUSBHID	1	/* HID использовать Human Interface Device на USB соединении */
 
 	#define WITHUSBDFU	1	/* DFU USB Device Firmware Upgrade support */
-	
 	#define WITHUSBWCID	1
 	//#define WITHUSBDMTP	1	/* MTP USB Device */
 
