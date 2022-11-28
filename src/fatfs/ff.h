240b5eb5e7cb56268bd8948fd2fa181fb4b2e7ae
ua1arn <mgs2001@mail.ru>
2022-11-28 23:30:02 +0300

    fix fatfs compile warnings

--- src/fatfs/ff.h
+++ src/fatfs/ff.h
@@ -231,7 +231,7 @@ typedef struct {
 	DWORD	clust;			/* Current cluster */
 	LBA_t	sect;			/* Current sector (0:Read operation has terminated) */
 	BYTE*	dir;			/* Pointer to the directory item in the win[] */
-	BYTE	fn[12];			/* SFN (in/out) {body[8],ext[3],status[1]} */
+	BYTE	fn[12 + 8];			/* SFN (in/out) {body[8],ext[3],status[1]} */
 #if FF_USE_LFN
 	DWORD	blk_ofs;		/* Offset of current entry block being processed (0xFFFFFFFF:Invalid) */
 #endif
