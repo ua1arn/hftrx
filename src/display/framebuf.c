7a4147921959dfa117108aee27cc988ad1d0fb68
Genadi V. Zawidowski <mgs201@mail.ru>
2023-01-30 08:10:47 +0300

    COMMRNTS

--- src/display/framebuf.c
+++ src/display/framebuf.c
@@ -44,7 +44,7 @@ static unsigned awxx_get_ui_attr(void)
 	unsigned ui_attr = 0;
 	ui_attr = 255 << 24;
 	//	if (img->bpremul)
-	//		ui_attr |= 0x1 << 17;
+	//		vi_attr |= 0x2 << 16;	/* LAY_PREMUL_CTL */
 	ui_attr |= DstImageFormat << 8;
 	//ui_attr |= G2D_GLOBAL_ALPHA << 1; // linux sample use G2D_PIXEL_ALPHA -> 0xFF000401
 	ui_attr |= G2D_PIXEL_ALPHA << 1; // нужно для работы color key linux sample use G2D_PIXEL_ALPHA -> 0xFF000401
@@ -57,8 +57,7 @@ static unsigned awxx_get_vi_attr(void)
 {
 	unsigned vi_attr = 0;
 	vi_attr = 255 << 24;
-	//	if (img->bpremul)
-	//		vi_attr |= 0x1 << 17;
+	//vi_attr |= (1u << 15);	/* Video_UI_Sel: 1: UI Overlay(using UI Overlay Layer Input data format) */
 	vi_attr |= DstImageFormat << 8;
 	//vi_attr |= G2D_GLOBAL_ALPHA << 1; // linux sample use G2D_PIXEL_ALPHA -> 0xFF000401
 	vi_attr |= G2D_PIXEL_ALPHA << 1; // нужно для работы color key linux sample use G2D_PIXEL_ALPHA -> 0xFF000401
