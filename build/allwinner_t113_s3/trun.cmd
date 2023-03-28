f9484fccfef2b0ac20adc119ecf0772932c849a7
Genadi V. Zawidowski <mgs201@mail.ru>
2023-03-29 00:16:17 +0300

    allwinner: использование исправленной утилиты

--- build/allwinner_t113_s3/trun.cmd
+++ build/allwinner_t113_s3/trun.cmd
@@ -1,3 +1,3 @@
-..\..\tools\xfel-windows-v1.2.9\xfel ddr t113-s3
-..\..\tools\xfel-windows-v1.2.9\xfel write 0x40000000 tc1_t113s3_app.alw32
-..\..\tools\xfel-windows-v1.2.9\xfel exec 0x40000100 
+xfel_fixed_t113-s3 ddr t113-s3
+xfel_fixed_t113-s3 write 0x40000000 tc1_t113s3_app.alw32
+xfel_fixed_t113-s3 exec 0x40000100 
