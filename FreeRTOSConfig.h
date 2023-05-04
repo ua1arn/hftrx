db65c158e19beb8a7c5b76c96bb9cf36b990c8cd
Genadi V. Zawidowski <mgs201@mail.ru>
2023-05-04 23:23:19 +0300

    build in non-rtos configs

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -55,7 +55,7 @@
 #define configIDLE_SHOULD_YIELD					1
 #define configUSE_MUTEXES						1
 #define configQUEUE_REGISTRY_SIZE				8
-#define configCHECK_FOR_STACK_OVERFLOW			1
+#define configCHECK_FOR_STACK_OVERFLOW			0//1
 #define configUSE_RECURSIVE_MUTEXES				1
 #define configUSE_MALLOC_FAILED_HOOK			1
 #define configUSE_APPLICATION_TASK_TAG			0
