3a3d50c83aef8a49d7c0269974ec3ac7b49ef444
Genadi V. Zawidowski <mgs201@mail.ru>
2023-05-07 10:25:11 +0300

    Allwinner: update registers naming style

--- src/tests.c
+++ src/tests.c
@@ -6452,6 +6452,32 @@ void hightests(void)
 #if WITHLTDCHW && LCDMODE_LTDC
 	hardware_ltdc_main_set((uintptr_t) colmain_fb_draw());
 #endif /* WITHLTDCHW && LCDMODE_LTDC */
+#if 0
+	{
+		PRINTF("DE_UI1->OVL_SIZE=%08" PRIX32 "\n", DE_UI1->OVL_SIZE);
+		//PRINTF("DE_UI2->OVL_SIZE=%08" PRIX32 "\n", DE_UI2->OVL_SIZE);
+		//PRINTF("DE_UI3->OVL_SIZE=%08" PRIX32 "\n", DE_UI3->OVL_SIZE);
+		DE_UI1->OVL_SIZE = ~ 0u;
+		//DE_UI2->OVL_SIZE = ~ 0u;
+		//DE_UI3->OVL_SIZE = ~ 0u;
+		PRINTF("DE_UI1->OVL_SIZE=%08" PRIX32 "\n", DE_UI1->OVL_SIZE);
+		//PRINTF("DE_UI2->OVL_SIZE=%08" PRIX32 "\n", DE_UI2->OVL_SIZE);
+		//PRINTF("DE_UI3->OVL_SIZE=%08" PRIX32 "\n", DE_UI3->OVL_SIZE);
+
+		PRINTF("G2D_UI0->UI_ATTR=%08" PRIX32 "\n", G2D_UI0->UI_ATTR);
+		PRINTF("G2D_UI1->UI_ATTR=%08" PRIX32 "\n", G2D_UI1->UI_ATTR);
+		PRINTF("G2D_UI2->UI_ATTR=%08" PRIX32 "\n", G2D_UI2->UI_ATTR);
+		G2D_UI0->UI_ATTR = ~ 0u;
+		G2D_UI1->UI_ATTR = ~ 0u;
+		G2D_UI2->UI_ATTR = ~ 0u;
+		PRINTF("G2D_UI0->UI_ATTR=%08" PRIX32 "\n", G2D_UI0->UI_ATTR);
+		PRINTF("G2D_UI1->UI_ATTR=%08" PRIX32 "\n", G2D_UI1->UI_ATTR);
+		PRINTF("G2D_UI2->UI_ATTR=%08" PRIX32 "\n", G2D_UI2->UI_ATTR);
+
+		for (;;)
+			;
+	}
+#endif
 #if 1 && (CPUSTYLE_T113 || CPUSTYLE_F133) && WITHDEBUG && 0
 	{
 		//	The Hotplug Flag Register is 0x070005DC.
