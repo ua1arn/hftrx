fc56af85223c8302ca2ef05eaa58c4ee324bfc2f
Genadi V. Zawidowski <mgs201@mail.ru>
2023-06-01 06:36:58 +0300

    fix build

--- boards/arm_x7c70xx_ctlstyle_storch_sv9.h
+++ boards/arm_x7c70xx_ctlstyle_storch_sv9.h
@@ -644,4 +644,7 @@
 	#define KI_COUNT 5	// количество используемых под клавиатуру входов АЦП
 	#define KI_LIST	KI4, KI3, KI2, KI1, KI0,	// инициализаторы для функции перекодировки
 
+	#define BOARDPOWERMIN    0    // Нижний предел регулировки (показываемый на дисплее)
+	#define BOARDPOWERMAX    100    // Верхний предел регулировки (показываемый на дисплее)
+
 #endif /* ARM_XC7ZXX_BGAXXX_CTLSTYLE_STORCH_SV9_H_INCLUDED */
