bd36fb1eb26105efbac37726531bb00b6e7d2010
ua1arn <mgs2001@mail.ru>
2023-06-26 17:46:23 +0300

    resampler placeholder added

---
+++ src/resampler.c
@@ -0,0 +1,16 @@
+//
+// Проект HF Dream Receiver (КВ приёмник мечты)
+// автор Гена Завидовский mgs2001@mail.ru
+// UA1ARN
+//
+
+#include "hardware.h"	/* зависящие от процессора функции работы с портами */
+#include "board.h"
+#include "audio.h"
+#include "formats.h"	// for debug prints
+
+#if WITHINTEGRATEDDSP
+
+#endif /* WITHINTEGRATEDDSP */
+
+
