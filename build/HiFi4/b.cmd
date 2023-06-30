3b3a3a60f872c5d3299b07deef59536939939ca0
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-06-30 09:21:57 +0300

    save tests

--- build/HiFi4/b.cmd
+++ build/HiFi4/b.cmd
@@ -1,5 +1,6 @@
 xtensa-hifi4-elf-gcc -Os -c "c0.S"
-xtensa-hifi4-elf-gcc -I d:/user/svn/hftrx/CMSIS_5/CMSIS/Core_A/Include -mauto-litpools -Os -omit-frame -c "t0.c"
+rem xtensa-hifi4-elf-gcc -I d:/user/svn/hftrx/CMSIS_5/CMSIS/Core_A/Include -mauto-litpools -Os -omit-frame -c "t0.c"
+xtensa-hifi4-elf-gcc -I d:/user/svn/hftrx/CMSIS_5/CMSIS/Core_A/Include -Os -c "t0.c"
 xtensa-hifi4-elf-gcc -nostartfiles -nodefaultlibs -T"hifi4.ld"  -Wl,-Map=t0.map,--cref -o"t0.elf" "c0.o" "t0.o"
 
 xtensa-hifi4-elf-objcopy -O binary -R .noinit -R .bss -R .vram -R .heap "t0.elf"  "t0.bin"
