b4c8c38b2a1400397fe7faa1f67de68c21c826fd
ua1arn <mgs2001@mail.ru>
2023-06-30 16:45:45 +0300

    HiFi4 tests save

--- build/HiFi4/t0.c
+++ build/HiFi4/t0.c
@@ -3,16 +3,37 @@
 void dbg_puts(const char * s);
 void dbg_putchar(uint_fast8_t c);
 
+static void usdelay(int v)
+{
+	volatile int i;
+	for (i = 0; i < v; ++ i)
+		;
+}
 void dbg_putchar(uint_fast8_t c)
 {
 	if (c == '\n')
 		dbg_putchar('\r');
-
+	//usdelay(8);
 	while ((UART0->UART_USR & (1u << 1)) == 0)	// TX FIFO Not Full
 		;
+	//usdelay(8);
 	UART0->UART_RBR_THR_DLL = c;
+	usdelay(8);
 }
 
+void printv32(uintptr_t v)
+{
+	static const char hex [16] = "0123456789ABCDEF";
+
+	dbg_putchar(hex [(v >> 28) & 0x0F]);
+	dbg_putchar(hex [(v >> 24) & 0x0F]);
+	dbg_putchar(hex [(v >> 20) & 0x0F]);
+	dbg_putchar(hex [(v >> 16) & 0x0F]);
+	dbg_putchar(hex [(v >> 12) & 0x0F]);
+	dbg_putchar(hex [(v >> 8) & 0x0F]);
+	dbg_putchar(hex [(v >> 4) & 0x0F]);
+	dbg_putchar(hex [(v >> 0) & 0x0F]);
+}
 
 void dbg_puts(const char * s)
 {
@@ -23,11 +44,14 @@ void dbg_puts(const char * s)
 }
 
 //char msg0 [] = "Hello, Word (I am HiFi4 DSP)!\n";
-char msg0 [] = "Hello, Word!\n";
+//char msg0 [] = "Hello, Word!\n";
+char msg0 [] = "eHello qqqqqqqqqqqewwwwwww ";
 void xmain(int v1, int v2)
 {
+	int z;
 	(void) v1;
 	(void) v2;
+
 
 	dbg_putchar('H');
 	dbg_putchar('e');
@@ -35,6 +59,9 @@ void xmain(int v1, int v2)
 	dbg_putchar('l');
 	dbg_putchar('o');
 	dbg_putchar('!');
+	dbg_putchar('\n');
+
+	printv32((uintptr_t) & z);
 	dbg_putchar('\n');
 
 	dbg_putchar('H');
