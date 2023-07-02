86edf41196ddd71a2867662451a05e162dd5dfe8
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-07-02 09:32:47 +0300

    Allwiner: частота тактирования DDR вынесена в board configuration file

--- boards/arm_allwt113s3_ctlstyle_ra4asn.h
+++ boards/arm_allwt113s3_ctlstyle_ra4asn.h
@@ -34,10 +34,12 @@
 		//#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
 		#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
 		//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+		#define PLL_DDR_N	22	/* 24 MHz * 22 = 528 MHz DDR2 clock	*/
 	#elif CPUSTYLE_T113
 		#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz https://linux-sunxi.org/T113-s3 say about 1.2 GHz */
 		//#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
 		//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+		#define PLL_DDR_N	33	/* 24 MHz * 33 = 792 MHz DDR3 clock	*/
 	#endif
 
 	/* модели синтезаторов - схемы частотообразования */
--- boards/arm_allwt113s3_ctlstyle_tboard_v0.h
+++ boards/arm_allwt113s3_ctlstyle_tboard_v0.h
@@ -32,14 +32,15 @@
 	//#define WITHCPUXOSC 24000000u	/* На процессоре установлен генератор 24.000 МГц */
 
 	#if CPUSTYLE_F133
-		//#define PLL_CPU_N 20	/* 24 MHz * 20 = 480 MHz */
-		#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
-		//#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
+		//#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
+		#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
 		//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+		#define PLL_DDR_N	22	/* 24 MHz * 22 = 528 MHz DDR2 clock	*/
 	#elif CPUSTYLE_T113
-		#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz */
+		#define PLL_CPU_N 50	/* 24 MHz * 50 = 1200 MHz https://linux-sunxi.org/T113-s3 say about 1.2 GHz */
 		//#define PLL_CPU_N 42	/* 24 MHz * 42 = 1008 MHz */
 		//#define PLL_CPU_N 33	/* 24 MHz * 33 = 792 MHz */
+		#define PLL_DDR_N	33	/* 24 MHz * 33 = 792 MHz DDR3 clock	*/
 	#endif
 
 	#if 1//WITHISBOOTLOADER
