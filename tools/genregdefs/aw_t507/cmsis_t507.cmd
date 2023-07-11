d742036f2d20b033b288f149d23b1ee0732e135e
ua1arn <mgs2001@mail.ru>
2023-07-11 13:08:42 +0300

    t507: update i/o

--- tools/genregdefs/aw_t507/cmsis_t507.cmd
+++ tools/genregdefs/aw_t507/cmsis_t507.cmd
@@ -1,6 +1,6 @@
 SET CONVERTER=..\Debug\genregdefs.exe
 SET OUTFILE=..\..\..\arch\aw_t507\cmsis_t507.h
-SET FILELIST= ccu.csv gic.csv gpio.csv gpu.csv twi.csv uart.csv usbehci.csv usbotg.csv usbphyc.csv usb_ehci_capability.csv usb_ohci_capability.csv
+SET FILELIST= ccu.csv gic.csv gpio.csv gpu.csv twi.csv uart.csv usbehci.csv usbotg.csv usbphyc.csv usb_ehci_capability.csv usb_ohci_capability.csv g2d.csv
 
 %CONVERTER% %FILELIST% > %OUTFILE%
 
