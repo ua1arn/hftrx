f42318c4ed379f84a9bc2e53a095fb7efa2b6ebd
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-07-13 22:26:07 +0300

    Switch to new xfel version

--- build/allwinner_a64/trun.cmd
+++ build/allwinner_a64/trun.cmd
@@ -1,2 +1,2 @@
-..\..\tools\xfel-windows-v1.2.9\xfel write 0x00018000 fsbl.alw32
-..\..\tools\xfel-windows-v1.2.9\xfel exec 0x00018100
+..\..\tools\/xfel-windows-v1.3.2\xfel write 0x00018000 fsbl.alw32
+..\..\tools\/xfel-windows-v1.3.2\xfel exec 0x00018100
