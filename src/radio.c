a84504990fca344d794ed82f735234409b1a0ad1
ua1arn <mgs2001@mail.ru>
2023-07-20 14:49:32 +0300

    fix build

--- src/radio.c
+++ src/radio.c
@@ -21628,11 +21628,15 @@ void display2_set_page_temp(uint_fast8_t page)
 
 #endif /* WITHTOUCHGUI */
 
+#if WITHDSPEXTDDC
+
 void hamradio_set_gdactest(uint8_t v)
 {
 	gdactest = v != 0;
 	updateboard(1, 1);
 }
+
+#endif /* WITHDSPEXTDDC */
 
 #if WITHUSEDUALWATCH
 uint_fast8_t hamradio_split_toggle(void)
