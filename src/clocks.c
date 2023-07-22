4da6a56070639acf6156e6e9bec5ba9831eed86e
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-07-22 05:16:56 +0300

    t113: update spi & sdcard clocks

--- src/clocks.c
+++ src/clocks.c
@@ -3623,10 +3623,11 @@ uint_fast32_t allwnrt113_get_twi_freq(void)
 
 uint_fast32_t allwnrt113_get_spi0_freq(void)
 {
-	const uint_fast32_t pgdiv = 1; //= 4 * 2;	// post-gate dividers: clkdiv4 and clkdiv2y
 	const uint_fast32_t clkreg = CCU->SPI0_CLK_REG;
-//	const uint_fast32_t N = 1u << ((clkreg >> 8) & 0x03);
-//	const uint_fast32_t M = 1u + ((clkreg >> 0) & 0x0F);
+	const uint_fast32_t N = 1u << ((clkreg >> 8) & 0x03);
+	const uint_fast32_t M = 1u + ((clkreg >> 0) & 0x0F);
+	// SCLK = Clock Source/M/N.
+	const uint_fast32_t pgdiv = M * N;
 	switch ((clkreg >> 24) & 0x07)
 	{
 	default:
@@ -3650,10 +3651,11 @@ uint_fast32_t allwnrt113_get_spi0_freq(void)
 
 uint_fast32_t allwnrt113_get_spi1_freq(void)
 {
-	const uint_fast32_t pgdiv = 4 * 2;	// post-gate dividers: clkdiv4 and clkdiv2y
 	const uint_fast32_t clkreg = CCU->SPI1_CLK_REG;
 	const uint_fast32_t N = 1u << ((clkreg >> 8) & 0x03);
 	const uint_fast32_t M = 1u + ((clkreg >> 0) & 0x0F);
+	// SCLK = Clock Source/M/N.
+	const uint_fast32_t pgdiv = M * N;
 	switch ((clkreg >> 24) & 0x07)
 	{
 	default:
@@ -3677,10 +3679,11 @@ uint_fast32_t allwnrt113_get_spi1_freq(void)
 
 uint_fast32_t allwnrt113_get_smhc0_freq(void)
 {
-	const uint_fast32_t pgdiv = 4 * 2;	// post-gate dividers: clkdiv4 and clkdiv2y
-	const uint_fast32_t clkreg = CCU->SPI1_CLK_REG;
+	const uint_fast32_t clkreg = CCU->SMHC0_CLK_REG;
 	const uint_fast32_t N = 1u << ((clkreg >> 8) & 0x03);
 	const uint_fast32_t M = 1u + ((clkreg >> 0) & 0x0F);
+	// SMHC0_CLK = Clock Source/M/N.
+	const uint_fast32_t pgdiv = M * N;	// post-gate dividers: clkdiv4 and clkdiv2y
 	switch ((clkreg >> 24) & 0x07)
 	{
 	default:
@@ -3696,24 +3699,57 @@ uint_fast32_t allwnrt113_get_smhc0_freq(void)
 	case 0x03:
 		/* 011: PLL_AUDIO1(DIV2) */
 		return allwnrt113_get_audio1pll_div2_freq() / pgdiv;
-	case 0x04:
-		/* 100: PLL_AUDIO1(DIV5) */
-		return allwnrt113_get_audio1pll_div5_freq() / pgdiv;
 	}
 }
 
 uint_fast32_t allwnrt113_get_smhc1_freq(void)
 {
 	const uint_fast32_t clkreg = CCU->SMHC1_CLK_REG;
-
-	return return allwnrt113_get_hosc_freq();
+	const uint_fast32_t N = 1u << ((clkreg >> 8) & 0x03);
+	const uint_fast32_t M = 1u + ((clkreg >> 0) & 0x0F);
+	// SMHC1_CLK = Clock Source/M/N.
+	const uint_fast32_t pgdiv = M * N;
+	switch ((clkreg >> 24) & 0x07)
+	{
+	default:
+	case 0x00:
+		/* 000: HOSC */
+		return allwnrt113_get_hosc_freq() / pgdiv;
+	case 0x01:
+		/* 001: PLL_PERI(1X) */
+		return allwnrt113_get_peripll1x_freq() / pgdiv;
+	case 0x02:
+		/* 010: PLL_PERI(2X) */
+		return allwnrt113_get_peripll2x_freq() / pgdiv;
+	case 0x03:
+		/* 011: PLL_AUDIO1(DIV2) */
+		return allwnrt113_get_audio1pll_div2_freq() / pgdiv;
+	}
 }
 
 uint_fast32_t allwnrt113_get_smhc2_freq(void)
 {
 	const uint_fast32_t clkreg = CCU->SMHC2_CLK_REG;
-
-	return return allwnrt113_get_hosc_freq();
+	const uint_fast32_t N = 1u << ((clkreg >> 8) & 0x03);
+	const uint_fast32_t M = 1u + ((clkreg >> 0) & 0x0F);
+	// SMHC2_CLK = Clock Source/M/N.
+	const uint_fast32_t pgdiv = M * N;
+	switch ((clkreg >> 24) & 0x07)
+	{
+	default:
+	case 0x00:
+		/* 000: HOSC */
+		return allwnrt113_get_hosc_freq() / pgdiv;
+	case 0x01:
+		/* 001: PLL_PERI(1X) */
+		return allwnrt113_get_peripll1x_freq() / pgdiv;
+	case 0x02:
+		/* 010: PLL_PERI(2X) */
+		return allwnrt113_get_peripll2x_freq() / pgdiv;
+	case 0x03:
+		/* 011: PLL_AUDIO1(DIV2) */
+		return allwnrt113_get_audio1pll_div2_freq() / pgdiv;
+	}
 }
 
 uint_fast32_t allwnrt113_get_arm_freq(void)
