350b0ba4c101806f2267e32a44297787b764f456
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-07-23 15:14:42 +0300

    save sate

--- tools/genregdefs/aw_t507/cmsis_t507.cmd
+++ tools/genregdefs/aw_t507/cmsis_t507.cmd
@@ -1,6 +1,6 @@
 SET CONVERTER=..\Debug\genregdefs.exe
 SET OUTFILE=..\..\..\arch\aw_t507\cmsis_t507.h
-SET FILELIST= ccu.csv gic.csv gpio.csv gpu.csv twi.csv uart.csv usbehci.csv usbotg.csv usbphyc.csv usb_ehci_capability.csv usb_ohci_capability.csv g2d.csv spi.csv timer.csv c0_cpux_cfg.csv cpusubsysctrl.csv pwm.csv smhc.csv
+SET FILELIST= ccu.csv gic.csv gpio.csv gpu.csv twi.csv uart.csv usbehci.csv usbotg.csv usbphyc.csv usb_ehci_capability.csv usb_ohci_capability.csv g2d.csv spi.csv timer.csv c0_cpux_cfg.csv cpusubsysctrl.csv pwm.csv smhc.csv de33.csv
 
 %CONVERTER% %FILELIST% > %OUTFILE%
 
