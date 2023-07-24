3ffa9528ad7331577f32ed24fce3539fb541f58c
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-07-24 22:06:11 +0300

    T507 board: PMIC diagnostics

--- src/chip/axp803.c
+++ src/chip/axp803.c
@@ -655,22 +655,350 @@ int axp305_initialize(void)
 	return 0;
 }
 
+/////////////
+///
+/// https://github.com/iuncuim/u-boot/blob/4faa8b19966e3741dcf6bbb398262c746a9c9782/drivers/power/axp858.c#L259
+///
+///
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * AXP858 driver based on AXP221 driver
+ *
+ *
+ * (C) Copyright 2015 Vishnu Patekar <vishnuptekar0510@gmail.com>
+ *
+ * Based on axp221.c
+ * (C) Copyright 2014 Hans de Goede <hdegoede@redhat.com>
+ * (C) Copyright 2013 Oliver Schinagl <oliver@schinagl.nl>
+ */
+//
+//#include <common.h>
+//#include <command.h>
+//#include <errno.h>
+//#include <asm/arch/gpio.h>
+//#include <asm/arch/pmic_bus.h>
+//#include <axp_pmic.h>
+
+
+#define AXP858_CHIP_ID		    0x03
+
+#define AXP858_OUTPUT_CTRL1	    0x10
+#define AXP858_OUTPUT_CTRL1_DCDC1_EN	(1 << 0)
+#define AXP858_OUTPUT_CTRL1_DCDC2_EN	(1 << 1)
+#define AXP858_OUTPUT_CTRL1_DCDC3_EN	(1 << 2)
+#define AXP858_OUTPUT_CTRL1_DCDC4_EN	(1 << 3)
+#define AXP858_OUTPUT_CTRL1_DCDC5_EN	(1 << 4)
+#define AXP858_OUTPUT_CTRL1_DCDC6_EN	(1 << 5)
+#define AXP858_OUTPUT_CTRL2	    0x11
+#define AXP858_OUTPUT_CTRL2_ALDO1_EN	(1 << 0)
+#define AXP858_OUTPUT_CTRL2_ALDO2_EN	(1 << 1)
+#define AXP858_OUTPUT_CTRL2_ALDO3_EN	(1 << 2)
+#define AXP858_OUTPUT_CTRL2_ALDO4_EN	(1 << 3)
+#define AXP858_OUTPUT_CTRL2_ALDO5_EN	(1 << 4)
+#define AXP858_OUTPUT_CTRL2_BLDO1_EN	(1 << 5)
+#define AXP858_OUTPUT_CTRL2_BLDO2_EN	(1 << 6)
+#define AXP858_OUTPUT_CTRL2_BLDO3_EN	(1 << 7)
+#define AXP858_OUTPUT_CTRL3	    0x12
+#define AXP858_OUTPUT_CTRL3_BLDO4_EN	(1 << 0)
+#define AXP858_OUTPUT_CTRL3_BLDO5_EN	(1 << 1)
+#define AXP858_OUTPUT_CTRL3_CLDO1_EN	(1 << 2)
+#define AXP858_OUTPUT_CTRL3_CLDO2_EN	(1 << 3)
+#define AXP858_OUTPUT_CTRL3_CLDO3_EN	(1 << 4)
+#define AXP858_OUTPUT_CTRL3_CLDO4_EN	(1 << 5)
+#define AXP858_OUTPUT_CTRL3_CPUSLDO4_EN	(1 << 6)
+#define AXP858_OUTPUT_CTRL3_SW_EN	    (1 << 7)
+
+#define AXP858_DCDC1_CTRL	    0x13
+#define AXP858_DCDC2_CTRL	    0x14
+#define AXP858_DCDC3_CTRL	    0x15
+#define AXP858_DCDC4_CTRL	    0x16
+#define AXP858_DCDC5_CTRL	    0x17
+#define AXP858_DCDC6_CTRL	    0x18
+
+#define AXP858_ALDO1_CTRL	    0x19
+#define AXP858_ALDO2_CTRL	    0x20
+#define AXP858_ALDO3_CTRL	    0x21
+#define AXP858_ALDO4_CTRL	    0x22
+#define AXP858_ALDO5_CTRL	    0x23
+
+#define AXP858_BLDO1_CTRL	    0x24
+#define AXP858_BLDO2_CTRL	    0x25
+#define AXP858_BLDO3_CTRL	    0x26
+#define AXP858_BLDO4_CTRL	    0x27
+#define AXP858_BLDO5_CTRL	    0x28
+
+#define AXP858_CLDO1_CTRL	    0x24
+#define AXP858_CLDO2_CTRL	    0x25
+#define AXP858_CLDO3_CTRL	    0x26
+#define AXP858_CLDO4_GPIO1_CTRL	0x27
+#define AXP858_CLDO4_GPIO2_CTRL	0x28
+
+#define AXP858_SHUTDOWN		0x32
+#define AXP858_SHUTDOWN_POWEROFF	(1 << 7)
+
+/* For axp_gpio.c */
+// #define AXP_POWER_STATUS		0x00
+// #define AXP_POWER_STATUS_VBUS_PRESENT		(1 << 5)
+// #define AXP_VBUS_IPSOUT			0x30
+// #define AXP_VBUS_IPSOUT_DRIVEBUS		(1 << 2)
+// #define AXP_MISC_CTRL			0x8f
+// #define AXP_MISC_CTRL_N_VBUSEN_FUNC		(1 << 4)
+// #define AXP_GPIO0_CTRL			0x90
+// #define AXP_GPIO1_CTRL			0x92
+// #define AXP_GPIO_CTRL_OUTPUT_LOW	0x00 /* Drive pin low */
+// #define AXP_GPIO_CTRL_OUTPUT_HIGH	0x01 /* Drive pin high */
+// #define AXP_GPIO_CTRL_INPUT		0x02 /* Input */
+// #define AXP_GPIO_STATE			0x94
+// #define AXP_GPIO_STATE_OFFSET		0
+static uint8_t axp858_mvolt_to_cfg(int mvolt, int min, int max, int div)
+{
+	if (mvolt < min)
+		mvolt = min;
+	else if (mvolt > max)
+		mvolt = max;
+
+	return  (mvolt - min) / div;
+}
+
+int axp_set_dcdc1(unsigned int mvolt)
+{
+	int ret;
+	uint8_t cfg = axp858_mvolt_to_cfg(mvolt, 1500, 3400, 100);
+
+	if (mvolt == 0)
+		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL1,
+					AXP858_OUTPUT_CTRL1_DCDC1_EN);
+
+	ret = pmic_bus_write(AXP858_DCDC1_CTRL, cfg);
+	if (ret)
+		return ret;
+
+	return pmic_bus_setbits(AXP858_OUTPUT_CTRL1,
+				AXP858_OUTPUT_CTRL1_DCDC1_EN);
+}
+
+int axp_set_dcdc2(unsigned int mvolt)
+{
+	int ret;
+	uint8_t cfg;
+
+	if (mvolt >= 1220)
+		//cfg = 70 + axp858_mvolt_to_cfg(mvolt, 1220, 1540, 20);
+		cfg = 70 + axp858_mvolt_to_cfg(mvolt, 1220, 1300, 20);
+	else
+		cfg = axp858_mvolt_to_cfg(mvolt, 500, 1200, 10);
+
+	if (mvolt == 0)
+		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL1,
+					AXP858_OUTPUT_CTRL1_DCDC2_EN);
+
+	ret = pmic_bus_write(AXP858_DCDC2_CTRL, cfg);
+	if (ret)
+		return ret;
+
+	return pmic_bus_setbits(AXP858_OUTPUT_CTRL1,
+				AXP858_OUTPUT_CTRL1_DCDC2_EN);
+}
+
+int axp_set_dcdc3(unsigned int mvolt)
+{
+	int ret;
+	uint8_t cfg;
+
+	if (mvolt >= 1220)
+		//cfg = 70 + axp858_mvolt_to_cfg(mvolt, 1220, 1540, 20);
+		cfg = 70 + axp858_mvolt_to_cfg(mvolt, 1220, 1300, 20);
+	else
+		cfg = axp858_mvolt_to_cfg(mvolt, 500, 1200, 10);
+
+	if (mvolt == 0)
+		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL1,
+					AXP858_OUTPUT_CTRL1_DCDC3_EN);
+
+	ret = pmic_bus_write(AXP858_DCDC3_CTRL, cfg);
+	if (ret)
+		return ret;
+
+	return pmic_bus_setbits(AXP858_OUTPUT_CTRL1,
+				AXP858_OUTPUT_CTRL1_DCDC3_EN);
+}
+
+int axp_set_dcdc5(unsigned int mvolt)
+{
+	int ret;
+	uint8_t cfg;
+
+	if (mvolt >= 1140)
+		cfg = 32 + axp858_mvolt_to_cfg(mvolt, 1140, 1840, 20);
+	else
+		cfg = axp858_mvolt_to_cfg(mvolt, 800, 1120, 10);
+
+	if (mvolt == 0)
+		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL1,
+					AXP858_OUTPUT_CTRL1_DCDC5_EN);
+
+	ret = pmic_bus_write(AXP858_DCDC5_CTRL, cfg);
+	if (ret)
+		return ret;
+
+	return pmic_bus_setbits(AXP858_OUTPUT_CTRL1,
+				AXP858_OUTPUT_CTRL1_DCDC5_EN);
+}
+
+int axp_set_aldo(int aldo_num, unsigned int mvolt)
+{
+	int ret;
+	uint8_t cfg;
+
+	if (aldo_num < 1 || aldo_num > 3)
+		return -1;
+
+	if (mvolt == 0)
+		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL2,
+				AXP858_OUTPUT_CTRL2_ALDO1_EN << (aldo_num - 1));
+
+	cfg = axp858_mvolt_to_cfg(mvolt, 700, 3300, 100);
+	ret = pmic_bus_write(AXP858_ALDO1_CTRL + (aldo_num - 1), cfg);
+	if (ret)
+		return ret;
+
+	return pmic_bus_setbits(AXP858_OUTPUT_CTRL2,
+				AXP858_OUTPUT_CTRL2_ALDO1_EN << (aldo_num - 1));
+}
+
+/* TODO: re-work other AXP drivers to consolidate ALDO functions. */
+int axp_set_aldo1(unsigned int mvolt)
+{
+	return axp_set_aldo(1, mvolt);
+}
+
+int axp_set_aldo2(unsigned int mvolt)
+{
+	return axp_set_aldo(2, mvolt);
+}
+
+int axp_set_aldo3(unsigned int mvolt)
+{
+	return axp_set_aldo(3, mvolt);
+}
+
+// int axp_set_dldo(int dldo_num, unsigned int mvolt)
+// {
+// 	int ret;
+// 	uint8_t cfg;
+
+// 	if (dldo_num < 1 || dldo_num > 4)
+// 		return -EINVAL;
+
+// 	if (mvolt == 0)
+// 		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL2,
+// 				AXP858_OUTPUT_CTRL2_DLDO1_EN << (dldo_num - 1));
+
+// 	cfg = axp858_mvolt_to_cfg(mvolt, 700, 3300, 100);
+// 	if (dldo_num == 2 && mvolt > 3300)
+// 		cfg += 1 + axp858_mvolt_to_cfg(mvolt, 3400, 4200, 200);
+// 	ret = pmic_bus_write(AXP858_DLDO1_CTRL + (dldo_num - 1), cfg);
+// 	if (ret)
+// 		return ret;
+
+// 	return pmic_bus_setbits(AXP858_OUTPUT_CTRL2,
+// 				AXP858_OUTPUT_CTRL2_DLDO1_EN << (dldo_num - 1));
+// }
+
+// int axp_set_eldo(int eldo_num, unsigned int mvolt)
+// {
+// 	int ret;
+// 	uint8_t cfg;
+
+// 	if (eldo_num < 1 || eldo_num > 3)
+// 		return -EINVAL;
+
+// 	if (mvolt == 0)
+// 		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL2,
+// 				AXP858_OUTPUT_CTRL2_ELDO1_EN << (eldo_num - 1));
+
+// 	cfg = axp858_mvolt_to_cfg(mvolt, 700, 1900, 50);
+// 	ret = pmic_bus_write(AXP858_ELDO1_CTRL + (eldo_num - 1), cfg);
+// 	if (ret)
+// 		return ret;
+
+// 	return pmic_bus_setbits(AXP858_OUTPUT_CTRL2,
+// 				AXP858_OUTPUT_CTRL2_ELDO1_EN << (eldo_num - 1));
+// }
+
+// int axp_set_fldo(int fldo_num, unsigned int mvolt)
+// {
+// 	int ret;
+// 	uint8_t cfg;
+
+// 	if (fldo_num < 1 || fldo_num > 3)
+// 		return -EINVAL;
+
+// 	if (mvolt == 0)
+// 		return pmic_bus_clrbits(AXP858_OUTPUT_CTRL3,
+// 				AXP858_OUTPUT_CTRL3_FLDO1_EN << (fldo_num - 1));
+
+// 	if (fldo_num < 3) {
+// 		cfg = axp858_mvolt_to_cfg(mvolt, 700, 1450, 50);
+// 		ret = pmic_bus_write(AXP858_FLDO1_CTRL + (fldo_num - 1), cfg);
+// 	} else {
+// 		/*
+// 		 * Special case for FLDO3, which is DCDC5 / 2 or FLDOIN / 2
+// 		 * Since FLDOIN is unknown, test against DCDC5.
+// 		 */
+// 		if (mvolt * 2 == CONFIG_AXP_DCDC5_VOLT)
+// 			ret = pmic_bus_clrbits(AXP858_FLDO2_3_CTRL,
+// 					       AXP858_FLDO2_3_CTRL_FLDO3_VOL);
+// 		else
+// 			ret = pmic_bus_setbits(AXP858_FLDO2_3_CTRL,
+// 					       AXP858_FLDO2_3_CTRL_FLDO3_VOL);
+// 	}
+// 	if (ret)
+// 		return ret;
+
+// 	return pmic_bus_setbits(AXP858_OUTPUT_CTRL3,
+// 				AXP858_OUTPUT_CTRL3_FLDO1_EN << (fldo_num - 1));
+// }
+
+int axp_set_sw(int on)
+{
+	if (on)
+		return pmic_bus_setbits(AXP858_OUTPUT_CTRL3,
+					AXP858_OUTPUT_CTRL3_SW_EN);
+
+	return pmic_bus_clrbits(AXP858_OUTPUT_CTRL3,
+				AXP858_OUTPUT_CTRL3_SW_EN);
+}
+
+static int pmu_axp858_ap_reset_enable(void)
+{
+	uint8_t reg_value;
+
+	if (pmic_bus_read(AXP858_SHUTDOWN,  &reg_value))
+		return -1;
+
+	reg_value |= 1 << 4;
+	if (pmic_bus_write(AXP858_SHUTDOWN, reg_value))
+		return -1;
+
+	return 0;
+}
+
 int axp853_initialize(void)
 {
-	uint8_t axp853_chip_id;
+	uint8_t axp_chip_id;
 	int ret;
-
+	uint8_t reg_value;
+    PRINTF("PMIC: AXP853T/AXP858\n");
 	ret = pmic_bus_init();
 	if (ret)
 		return ret;
 
-	ret = pmic_bus_read(AXP803_CHIP_ID, &axp853_chip_id);
-	if (ret)
-		return ret;
+	ret = pmic_bus_read(AXP858_CHIP_ID, &axp_chip_id);
 
-	ret = pmic_bus_init();
-	if (ret)
-		return ret;
+	PRINTF("axp_chip_id=0x%02X (expected 0x54)\n", axp_chip_id);
+	if (!(axp_chip_id == 0x54))
+		return -1;
 
 	if (0)
 	{
@@ -682,10 +1010,6 @@ int axp853_initialize(void)
 			PRINTF("axp853 reg%02X=0x%02X\n", reg, v);
 		}
 	}
-
-	PRINTF("axp853_chip_id=0x%02X (expected 0x60)\n", axp853_chip_id);
-	if (!(axp853_chip_id == 0x60))
-		return -1;
 
 	return 0;
 }
