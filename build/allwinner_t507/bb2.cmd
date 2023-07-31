cabc12dd8c6e858a587617d5a9145b33690c9850
ua1arn <mgs2001@mail.ru>
2023-07-31 11:25:36 +0300

    safety

--- build/allwinner_t507/bb2.cmd
+++ build/allwinner_t507/bb2.cmd
@@ -1,6 +1,6 @@
 @rem bootable image should be linked to 0x00020000 base address
 @rem http://nskhuman.ru/allwinner/card/cardmode.php?ysclid=lkq00i1n2061987481
-@set TARGET=E
+@set TARGET=E_but_may_be_changed
 
 cfimager -raw -offset 0x2000 -f boot0_sdcard_sun50iw9p1.bin -d %TARGET%
 cfimager -raw -offset 0x1004000 -f bl31.bin -d %TARGET%
