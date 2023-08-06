4e6dbedcf3c904b2432f8798a91f84e8d7a6ebab
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-08-06 17:12:08 +0300

    t507: прерывания по передаче от SAI идут с правильной частотой (по диагностике в buffrts.c)

--- boards/arm_allwt507_cpustyle_helperboard_t507.h
+++ boards/arm_allwt507_cpustyle_helperboard_t507.h
@@ -124,8 +124,8 @@
 		//#define WITHCODEC1_I2S1_DUPLEX_MASTER	1		/* Обмен с аудиокодеком через I2S1 */
 		//#define WITHFPGAIF_I2S2_DUPLEX_MASTER	1		/* Обмен с FPGA через I2S2 */
 		//#define WITHFPGAIF_I2S0_DUPLEX_MASTER	1		/* Обмен с FPGA через I2S0 */
-		#define WITHFPGAIF_I2S0_DUPLEX_SLAVE	1		/* Обмен с FPGA через I2S0 */
-		//#define WITHFPGAPIPE_I2S0_DUPLEX_SLAVE	1		/* Интерфейс к FPGA, транзитом в аудио кодек через I2S0 */
+		//#define WITHFPGAIF_I2S0_DUPLEX_SLAVE	1		/* Обмен с FPGA через I2S0 */
+		#define WITHFPGAPIPE_I2S0_DUPLEX_SLAVE	1		/* Интерфейс к FPGA, транзитом в аудио кодек через I2S0 */
 	#endif /* WITHINTEGRATEDDSP */
 
 	////#define WITHMDMAHW		1	/* Использование G2D для формирования изображений */
