7689d6362232a696ea9219fe5e8dc0240cb7b21a
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-08-06 18:34:21 +0300

    save state

--- src/buffers.c
+++ src/buffers.c
@@ -2218,16 +2218,17 @@ uintptr_t RAMFUNC pipe_dmabuffer32tx(uintptr_t addr32tx, uintptr_t addr16tx)
 	IFDACvalue_t * const tx32 = (IFDACvalue_t *) addr32tx;
 	aubufv_t * const tx16 = (aubufv_t *) addr16tx;
 	unsigned i;
+	const FLOAT_t scale = 1;
 
 	ASSERT((DMABUFFSIZE32TX / DMABUFFSTEP32TX) == (DMABUFFSIZE16TX / DMABUFFSTEP16TX));
 	ASSERT(DMABUFFSTEP16TX >= 2);
 	ASSERT(sizeof * tx32 == sizeof * tx16);
 	for (i = 0; i < (DMABUFFSIZE32TX / DMABUFFSTEP32TX); ++ i)
 	{
-		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_LEFT] = adpt_output(& afcodectx, get_lout());
-		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_RIGHT] = adpt_output(& afcodectx, get_rout());
-//		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_LEFT] = tx16 [i * DMABUFFSTEP16TX + DMABUFF16TX_LEFT];
-//		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_RIGHT] = tx16 [i * DMABUFFSTEP16TX + DMABUFF16TX_RIGHT];
+		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_LEFT] = tx16 [i * DMABUFFSTEP16TX + DMABUFF16TX_LEFT];
+		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_RIGHT] = tx16 [i * DMABUFFSTEP16TX + DMABUFF16TX_RIGHT];
+		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_LEFT] = adpt_outputexact(& afcodectx, get_lout() * scale);
+		tx32 [i * DMABUFFSTEP32TX + DMABUFF32TX_CODEC1_RIGHT] = adpt_outputexact(& afcodectx, get_rout() * scale);
 	}
 	return addr32tx;
 }
