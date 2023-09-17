9fc4bd3befbdf0001ec1304f05f49b7ec4a4bacc
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-09-17 17:08:00 +0300

    приборка

--- build/allwinner_t507/bb2.cmd
+++ build/allwinner_t507/bb2.cmd
@@ -1,6 +1,6 @@
 @rem bootable image should be linked to 0x00020000 base address
 @rem http://nskhuman.ru/allwinner/card/cardmode.php?ysclid=lkq00i1n2061987481
-@set TARGET=F
+@set TARGET=_DISKLETTER
 
 bootutil tc1_awt507_app.bin 0x40000100 monitor.bin
 
