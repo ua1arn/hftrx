904f87b430352561b4e128cd7c4d4440d8c31a57
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-09-17 16:21:02 +0300

    save state (documents, custom config update)

--- boards/arm_allwt507_cpustyle_helperboard_t507.h
+++ boards/arm_allwt507_cpustyle_helperboard_t507.h
@@ -148,7 +148,7 @@
 
 	#define WITHUSBHW_DEVICE	USB20_OTG_DEVICE	/* на этом устройстве поддерживается функциональность DEVICE	*/
 	#define WITHUSBDEV_VBUSSENSE	1		/* используется предопределенный вывод OTG_VBUS */
-	#define WITHUSBDEV_HSDESC	1			/* Требуется формировать дескрипторы как для HIGH SPEED */
+	//#define WITHUSBDEV_HSDESC	1			/* Требуется формировать дескрипторы как для HIGH SPEED */
 	//#define WITHUSBDEV_HIGHSPEEDULPI	1	// ULPI
 	#define WITHUSBDEV_HIGHSPEEDPHYC	1	// UTMI -> USB0_DP & USB0_DM
 	//#define WITHUSBDEV_DMAENABLE 1
@@ -174,10 +174,10 @@
 	#if WITHINTEGRATEDDSP
 
 		//#define WITHUAC2		1	/* UAC2 support */
-		#define UACOUT_AUDIO48_SAMPLEBYTES	3	/* должны быть 2, 3 или 4 */
+		#define UACOUT_AUDIO48_SAMPLEBYTES	2	/* должны быть 2, 3 или 4 */
 		#define UACIN_AUDIO48_SAMPLEBYTES	3	/* должны быть 2, 3 или 4 */
-		#define UACIN_RTS96_SAMPLEBYTES		4	/* должны быть 2, 3 или 4 */
-		#define UACIN_RTS192_SAMPLEBYTES	4	/* должны быть 2, 3 или 4 */
+		#define UACIN_RTS96_SAMPLEBYTES		3	/* должны быть 2, 3 или 4 */
+		#define UACIN_RTS192_SAMPLEBYTES	3	/* должны быть 2, 3 или 4 */
 
 		#define WITHUSBUACINOUT	1	/* совмещённое усройство ввода/вывода (без спектра) */
 		#define WITHUSBUACOUT		1	/* использовать виртуальную звуковую плату на USB соединении */
