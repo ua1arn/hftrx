0cbada762493f26fa43abe027c73fb26b4f99380
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-09-23 09:52:26 +0300

    aw: fix TWI clock calculation

--- src/twi.c
+++ src/twi.c
@@ -1647,10 +1647,8 @@ static void write32(uintptr_t a, uint32_t v)
 	* (volatile uint32_t *) a = v;
 }
 
-// TODO: use allwnrt113_get_twi_freq()
-
 static void t113_i2c_set_rate(struct i2c_t113_pdata_t * pdat, uint64_t rate){
-	uint64_t pclk = 100000000;	//clk_get_rate(pdat->clk);
+	uint64_t pclk = allwnrt113_get_twi_freq();	//clk_get_rate(pdat->clk);
 	uint_fast64_t freq, delta, best = 0x7fffffffffffffffLL;
 	int tm = 5, tn = 0;
 	int m, n;
