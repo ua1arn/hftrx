c8b68d537afd15edb489db8415b76b73e66eb6de
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-02 13:10:51 +0300

    aw t507: add AudioCodec i/o registers

--- tools/genregdefs/aw_t507/cmsis_t507.cmd
+++ tools/genregdefs/aw_t507/cmsis_t507.cmd
@@ -1,6 +1,11 @@
 SET CONVERTER=..\Debug\genregdefs.exe
 SET OUTFILE=..\..\..\arch\aw_t507\device.h
-SET FILELIST= ccu.csv gic.csv gpio.csv gpu.csv twi.csv uart.csv usbehci.csv usbotg.csv usbphyc.csv usb_ehci_capability.csv usb_ohci_capability.csv g2d.csv spi.csv timer.csv c0_cpux_cfg.csv cpusubsysctrl.csv pwm.csv smhc.csv de3.csv tconlcd.csv dmac.csv ahub.csv syscfg.csv iommu.csv ths.csv ce.csv hdmi_tx.csv tcon_tv.csv
+SET FILELIST= ^
+	ccu.csv gic.csv gpio.csv gpu.csv twi.csv ^
+	uart.csv usbehci.csv usbotg.csv usbphyc.csv usb_ehci_capability.csv usb_ohci_capability.csv ^
+	spi.csv timer.csv c0_cpux_cfg.csv cpusubsysctrl.csv pwm.csv smhc.csv ^
+	de3.csv g2d.csv tconlcd.csv dmac.csv syscfg.csv iommu.csv ths.csv ce.csv hdmi_tx.csv tcon_tv.csv ^
+	audio_codec.csv ahub.csv 
 
 %CONVERTER% %FILELIST% > %OUTFILE%
 
