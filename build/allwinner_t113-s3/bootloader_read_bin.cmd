c85dc54e46feb678df2668bafbab73eb7079dc39
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-10 11:56:46 +0300

    aw t113:  fix DFU commands parameters

--- build/allwinner_t113-s3/bootloader_read_bin.cmd
+++ build/allwinner_t113-s3/bootloader_read_bin.cmd
@@ -1,6 +1,6 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
 del tc1_t113s3_boot_old.bin
-dfu-util-static --alt 1 --dfuse-address 0x40000000 --upload "tc1_t113s3_boot_old.bin"
+dfu-util-static --alt 1 --dfuse-address 0x30002000 --upload "fsbl_alw32_old.bin"
 @pause
 
 
--- build/allwinner_t113-s3/bootloader_write_bin.cmd
+++ build/allwinner_t113-s3/bootloader_write_bin.cmd
@@ -1,6 +1,6 @@
 REM Utility files can be downloaded from https://sourceforge.net/projects/dfu-util/files/latest/download
 @echo Press any key for write FIRST BOOTLOADER section of FLASH
 @pause
-dfu-util-static --alt 1 --dfuse-address 0x40000000 --download "tc1_t113s3_boot.bin"
+dfu-util-static --alt 1 --dfuse-address 0x30002000 --download "fsbl.alw32"
 @pause
 
