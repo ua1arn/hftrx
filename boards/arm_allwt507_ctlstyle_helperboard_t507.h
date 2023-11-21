d0800d55eec71df916da84a4affed10040f3fce8
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-21 22:08:30 +0300

    custom config update

--- boards/arm_allwt507_ctlstyle_helperboard_t507.h
+++ boards/arm_allwt507_ctlstyle_helperboard_t507.h
@@ -527,7 +527,7 @@
 	//#define WITHRTCLSI	1				/* тестирование без кварца 32.768 кГц */
 
 	//#define TSC1_TYPE TSC_TYPE_TSC2046	/* Resistive touch screen controller TI TSC2046 */
-	//#define TSC1_TYPE TSC_TYPE_STMPE811	/* touch screen controller */
+	#define TSC1_TYPE TSC_TYPE_STMPE811	/* touch screen controller */
 	//#define TSC_TYPE_STMPE811_USE_SPI	1
 	//#define WITH_STMPE811_INTERRUPTS	1
 	//#define TSC1_TYPE	TSC_TYPE_GT911		/* Capacitive touch screen with controller Goodix GT911 */
