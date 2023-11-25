214c499f288f71408387324436afc295be9bd910
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-25 04:30:47 +0300

    fpga: удалил старую копию (до pipe)

--- tools/convert_rbf_to_c.cmd
+++ tools/convert_rbf_to_c.cmd
@@ -1,33 +1,32 @@
-set SRCPATH=..\fpga_q131\output_files
-set SRCPATHLONG=..\fpga_q131_long\output_files
+set SRCPATH=..\fpga_q131_long\output_files
 rem set SRCPATH=..\fpga_q181std\output_files
 
 @rem STM32F/STM32H
 
-rbf2c.exe %SRCPATHLONG%\t1_v4.rbf ..\rbf\rbfimage_v4.h
-rbf2c.exe %SRCPATHLONG%\t1_v5_2ch.rbf ..\rbf\rbfimage_v5_2ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v6_2ch.rbf ..\rbf\rbfimage_v6_2ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v7_1ch.rbf ..\rbf\rbfimage_v7_1ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v7_2ch.rbf ..\rbf\rbfimage_v7_2ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v7a_2ch.rbf ..\rbf\rbfimage_v7a_2ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v7h_2ch.rbf ..\rbf\rbfimage_v7h_2ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v7_oleg4z.rbf ..\rbf\rbfimage_oleg4z.h
+rbf2c.exe %SRCPATH%\t1_v4.rbf ..\rbf\rbfimage_v4.h
+rbf2c.exe %SRCPATH%\t1_v5_2ch.rbf ..\rbf\rbfimage_v5_2ch.h
+rbf2c.exe %SRCPATH%\t1_v6_2ch.rbf ..\rbf\rbfimage_v6_2ch.h
+rbf2c.exe %SRCPATH%\t1_v7_1ch.rbf ..\rbf\rbfimage_v7_1ch.h
+rbf2c.exe %SRCPATH%\t1_v7_2ch.rbf ..\rbf\rbfimage_v7_2ch.h
+rbf2c.exe %SRCPATH%\t1_v7a_2ch.rbf ..\rbf\rbfimage_v7a_2ch.h
+rbf2c.exe %SRCPATH%\t1_v7h_2ch.rbf ..\rbf\rbfimage_v7h_2ch.h
+rbf2c.exe %SRCPATH%\t1_v7_oleg4z.rbf ..\rbf\rbfimage_oleg4z.h
 
 @rem Renesas
 
-rbf2c.exe %SRCPATHLONG%\t1_v8t_96k.rbf ..\rbf\rbfimage_v8t_96k.h
-rbf2c.exe %SRCPATHLONG%\t1_v8t_192k.rbf ..\rbf\rbfimage_v8t_192k.h
+rbf2c.exe %SRCPATH%\t1_v8t_96k.rbf ..\rbf\rbfimage_v8t_96k.h
+rbf2c.exe %SRCPATH%\t1_v8t_192k.rbf ..\rbf\rbfimage_v8t_192k.h
 
 @rem STM32MP157
 
-rbf2c.exe %SRCPATHLONG%\t1_v9a_2ch.rbf ..\rbf\rbfimage_v9a_2ch.h
-rbf2c.exe %SRCPATHLONG%\t1_v9c_2ch.rbf ..\rbf\rbfimage_v9c_2ch.h
+rbf2c.exe %SRCPATH%\t1_v9a_2ch.rbf ..\rbf\rbfimage_v9a_2ch.h
+rbf2c.exe %SRCPATH%\t1_v9c_2ch.rbf ..\rbf\rbfimage_v9c_2ch.h
 
 @rem Allwinner T113-s3, T507-H
 
-rbf2c.exe %SRCPATHLONG%\t1_v9d_2ch.rbf ..\rbf\rbfimage_v9d_2ch.h
+rbf2c.exe %SRCPATH%\t1_v9d_2ch.rbf ..\rbf\rbfimage_v9d_2ch.h
 
-rbf2c.exe %SRCPATHLONG%\t1_v9m_velo.rbf ..\rbf\rbfimage_v9m_velo.h
-bin2c.exe %SRCPATHLONG%\t1_v9m_velo.zip ..\rbf\rbfimage_v9m_velo_zip.h
+rbf2c.exe %SRCPATH%\t1_v9m_velo.rbf ..\rbf\rbfimage_v9m_velo.h
+bin2c.exe %SRCPATH%\t1_v9m_velo.zip ..\rbf\rbfimage_v9m_velo_zip.h
 
 pause
