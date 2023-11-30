3768d07416316a0c1fb7ab47e477eca73086793d
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-30 20:40:45 +0300

    boot sd utility update

--- build/allwinner_t507/bb.cmd
+++ build/allwinner_t507/bb.cmd
@@ -1,2 +1,3 @@
 @rem bootable image should be linked to 0x00020000 base address
 cfimager -raw -offset 0x2000 -f fsbl.alw32 -d _CHANGE_LETTER_E
+cfimager -raw -offset 0x40000 -f tc1_awt507_app.alw32 -d _CHANGE_LETTER_E
