5009974f48949d746159cf9ffd3fc64e83b38d56
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-01-21 08:22:24 +0300

    custom config update (default TFT backlight level)

--- boards/arm_allwt113s3_cpustyle_storch_v9w.h
+++ boards/arm_allwt113s3_cpustyle_storch_v9w.h
@@ -838,14 +838,14 @@
 		#define WITHLCDBACKLIGHT	1	// Имеется управление яркостью дисплея
 		#define WITHLCDBACKLIGHTMIN	1	// Нижний предел регулировки (показываемый на дисплее)
 		#define WITHLCDBACKLIGHTMAX	5	// Верхний предел регулировки (показываемый на дисплее)
-		#define WITHLCDBACKLIGHTDEF	4	// значение яркости по уиолчанию
+		#define WITHLCDBACKLIGHTDEF	5	// значение яркости по уиолчанию
 		//#define WITHKBDBACKLIGHT	1	// Имеется управление подсветкой клавиатуры
 	#elif LCDMODE_AT070TN90 || LCDMODE_AT070TNA2
 		#define WITHLCDBACKLIGHTOFF	1	// Имеется управление включением/выключением подсветки дисплея
 		#define WITHLCDBACKLIGHT	1	// Имеется управление яркостью дисплея
 		#define WITHLCDBACKLIGHTMIN	1	// Нижний предел регулировки (показываемый на дисплее)
 		#define WITHLCDBACKLIGHTMAX	5	// Верхний предел регулировки (показываемый на дисплее)
-		#define WITHLCDBACKLIGHTDEF	4	// значение яркости по уиолчанию
+		#define WITHLCDBACKLIGHTDEF	5	// значение яркости по уиолчанию
 		//#define WITHKBDBACKLIGHT	1	// Имеется управление подсветкой клавиатуры
 	#else
 		/* Заглушка для работы без дисплея */
