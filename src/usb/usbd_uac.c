1d3834b0846d77f33a6fabb5dddbf46767b04a7e
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-02-11 09:33:06 +0300

    usb audio - prepare for less inerrupts frequency

--- src/usb/usbd_uac.c
+++ src/usb/usbd_uac.c
@@ -846,10 +846,10 @@ static USBD_StatusTypeDef USBD_UAC_DataOut(USBD_HandleTypeDef *pdev, uint_fast8_
 		{
 			// use audio data
 			const uintptr_t addr = allocate_dmabufferuacout48();
-			memcpy((void *) addr, uacout48buff, UACOUT_AUDIO48_DATASIZE_DMAC);
+			memcpy((void *) addr, uacout48buff, UAC_GROUPING_DMAC * UACOUT_AUDIO48_DATASIZE_DMAC);
 			save_dmabufferuacout48(addr);
 			/* Prepare Out endpoint to receive next audio data packet */
-			USBD_LL_PrepareReceive(pdev, USB_ENDPOINT_OUT(epnum), uacout48buff, UACOUT_AUDIO48_DATASIZE_DMAC);
+			USBD_LL_PrepareReceive(pdev, USB_ENDPOINT_OUT(epnum), uacout48buff,UAC_GROUPING_DMAC * UACOUT_AUDIO48_DATASIZE_DMAC);
 		}
 		break;
 #endif /* WITHUSBUACOUT */
@@ -1006,7 +1006,7 @@ static USBD_StatusTypeDef USBD_UAC_Init(USBD_HandleTypeDef *pdev, uint_fast8_t c
 	/* UAC Open EP OUT */
 	USBD_LL_OpenEP(pdev, USBD_EP_AUDIO_OUT, USBD_EP_TYPE_ISOC, UACOUT_AUDIO48_DATASIZE_DMAC);
    /* UAC Prepare Out endpoint to receive 1st packet */
-	USBD_LL_PrepareReceive(pdev, USBD_EP_AUDIO_OUT, uacout48buff, UACOUT_AUDIO48_DATASIZE_DMAC);
+	USBD_LL_PrepareReceive(pdev, USBD_EP_AUDIO_OUT, uacout48buff, UAC_GROUPING_DMAC * UACOUT_AUDIO48_DATASIZE_DMAC);
 
 #endif /* WITHUSBUACOUT */
 	return USBD_OK;
