762d5d0b099af123f895ee54ba1a32607071b4b9
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-02-27 18:27:38 +0300

    add tools: buffers prototypes generation tool

---
+++ tools/genbuffif/genbuffif.cpp
@@ -0,0 +1,63 @@
+// genbuffif.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+void genonjectset(const char * obj, const char * comment)
+{
+	fprintf(stdout, "/* %s */\n", comment);
+	fprintf(stdout, "uintptr_t allocate_dmabuffer%s(void);\n", obj);
+	fprintf(stdout, "uintptr_t getfilled_dmabuffer%s(void);\n", obj);
+	fprintf(stdout, "int_fast32_t cachesize_dmabuffer%s(void);\n", obj);
+	fprintf(stdout, "void release_dmabuffer%s(uintptr_t addr);\n", obj);
+	fprintf(stdout, "void save_dmabuffer%s(uintptr_t addr);\n", obj);
+
+	fprintf(stdout, "\n");
+
+
+}
+
+struct params
+{
+	const char * obj;
+	const char * comment;
+};
+
+static const struct params params0 [] = 
+{
+	{	"32rx",	"FPGA to CPU", },
+	{	"32tx",	"CPU to FPGA", },
+	{	"16rx",	"CODEC to CPU", },
+	{	"16tx",	"CPU to CODEC", },
+
+	{	"rts192",	"FPGA to CPU", },
+
+	{	"uacin",	"usb audio48 to host", },
+	{	"uacinrts96"	"usb rts96 to host", },
+	{	"uacinrts192",	"usb rts192 to host", },
+	{	"uacinX",	"usb audio48/rts to host", },
+	{	"uacout",	"usb audio48 from host", },
+
+	{	"btout8k",	"BT audio to radio, samplerate 8000", },
+	{	"btout16k",	"BT audio to radio, samplerate 16000", },
+	{	"btout32k",	"BT audio to radio, samplerate 32000", },
+	{	"btout44p1k",	"BT audio to radio, samplerate 44100", },
+
+	{	"btin8k",	"BT audio from radio, samplerate 8000", },
+	{	"btin16k",	"BT audio from radio, samplerate 16000", },
+	{	"btin32k",	"BT audio from radio, samplerate 32000", },
+	{	"btin44p1k",	"BT audio from radio, samplerate 44100", },
+
+};
+
+int main(int argc, char* argv[])
+{
+
+	unsigned i;
+
+	for (i = 0; i < sizeof params0 / sizeof params0 [0]; ++ i)
+	{
+		genonjectset(params0 [i].obj, params0 [i].comment);
+	}
+	return 0;
+}
