a534c53a64f5066504e238843992e7ef8aab103f
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-04-09 12:42:41 +0300

    custom config update

--- boards/arm_allwt113s3_ctlstyle_storch_v9w_ua1cei.h
+++ boards/arm_allwt113s3_ctlstyle_storch_v9w_ua1cei.h
@@ -121,8 +121,8 @@
 	#define LCDMODE_AT070TN90 1	/* AT070TN90 panel (800*480) - 7" display */
 
 	//#define LCDMODE_V2 1	/* только главный экран 8 бит (две страницы), L8, без PIP */
-	//#define LCDMODE_V2A_2PAGE 1	/* только главный экран 16 бит (две страницы), без PIP */
-	#define LCDMODE_V5A	1	/* только главный экран с двумя видеобуферами 32 бит ARGB8888, без PIP */
+	#define LCDMODE_V2A_2PAGE 1	/* только главный экран 16 бит (две страницы), без PIP */
+	//#define LCDMODE_V5A	1	/* только главный экран с двумя видеобуферами 32 бит ARGB8888, без PIP */
 
 	//#define WITHFLATLINK 1	/* Работа с TFT панелью через преобразователь RGB->FlatLink SN75LVDS83B	*/
 	//#define WITHMIPIDSISHW 1	/* MIPI-DSI display support */
