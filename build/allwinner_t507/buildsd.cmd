0865d13c69db5a1a1e208a96eea439300b5c5cb2
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-05-22 01:48:42 +0300

    переъод на busybox dd для формирования образа SD карты

--- build/allwinner_t507/buildsd.cmd
+++ build/allwinner_t507/buildsd.cmd
@@ -1,5 +1,4 @@
-@echo zzz >sdimaget507.img
-wtitefsbl.exe sdimaget507.img 0x0000 bootsector0.bin -w
-wtitefsbl.exe sdimaget507.img 0x2000 fsbl.alw32 -w
-wtitefsbl.exe sdimaget507.img 0x40000 tc1_awt507_app.alw32 -w
+busybox dd oflag=seek_bytes of=sdimaget507.img bs=1b seek=0 if=bootsector0.bin
+busybox dd oflag=seek_bytes of=sdimaget507.img bs=1b seek=8k if=fsbl.alw32
+busybox dd oflag=seek_bytes of=sdimaget507.img bs=1b seek=256k if=tc1_awt507_app.alw32
 pause
