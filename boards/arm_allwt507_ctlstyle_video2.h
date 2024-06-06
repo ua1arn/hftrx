52689cf6c8077c37b477d177c0b382981ada291f
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-06-06 09:42:12 +0300

    custom config update

--- boards/arm_allwt507_ctlstyle_video2.h
+++ boards/arm_allwt507_ctlstyle_video2.h
@@ -111,7 +111,7 @@
 	//#define LCDMODE_V5A	1	/* только главный экран с двумя видеобуферами 32 бит ARGB8888, без PIP */
 
 	//#define WITHFLATLINK 1	/* Работа с TFT панелью через преобразователь RGB->FlatLink SN75LVDS83B	*/
-	#define WITHLCDDEMODE	1	/* DE MODE: MODE="1", VS and HS must pull high. */
+	//#define WITHLCDDEMODE	1	/* DE MODE: MODE="1", VS and HS must pull high. */
 	//#define WITHMIPIDSISHW 1	/* MIPI-DSI display support */
 	#define WITHMIPIDSISHW_LANES 2	/* mipi-dsi port lanes number */
 	#define BOARD_DSTYLE "g_x800_y480.h"
