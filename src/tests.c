ac6336519ea52491ad328dac034abef26395f8bf
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-07-19 06:34:37 +0300

    save state

--- src/tests.c
+++ src/tests.c
@@ -11014,7 +11014,6 @@ void hightests(void)
 		};
 		dcache_clean_all();
 		CCU->RISC_CFG_BGR_REG |= (UINT32_C(1) << 16) | (UINT32_C(1) << 0);
-		(void) CCU->RISC_CFG_BGR_REG;
 		CCU->RISC_RST_REG = (UINT32_C(0x16AA) << 16) | 0 * ((UINT32_C(1) << 0));	/* Assert rv64 reset */
 #if 0
 		/* setup RISC-V CPU & AXI clock */
@@ -11025,8 +11024,8 @@ void hightests(void)
 		CCU->RISC_CLK_REG |= (UINT32_C(1) << 31);	// not need
 #endif
 		CCU->RISC_GATING_REG = (UINT32_C(1) << 31) | 0x16AA;	/* key required for modifications (d1-h_user_manual_v1.0.pdf, page 152). */
-		RISC_CFG->RISC_STA_ADD0_REG = ptr_lo32((uintptr_t) rv64code);
-		RISC_CFG->RISC_STA_ADD1_REG = ptr_hi32((uintptr_t) rv64code);
+		RISC_CFG->RISC_STA_ADD0_REG = (uintptr_t) rv64code; //ptr_lo32((uintptr_t) rv64code);
+		RISC_CFG->RISC_STA_ADD1_REG = 0;//ptr_hi32((uintptr_t) rv64code);
 		CCU->RISC_RST_REG = (UINT32_C(0x16AA) << 16) | 1 * ((UINT32_C(1) << 0));	/* De-assert rv64 reset */
 
 		local_delay_ms(100);	/* see '#' on serial port UART0 */
