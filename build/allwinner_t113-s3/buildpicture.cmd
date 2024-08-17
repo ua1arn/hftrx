ac55fc7742591530420db7646e0590deef08d402
mgs2001@mail.ru <mgs2001@mail.ru>
2024-08-17 23:17:28 +0300

    tv test picture added

--- build/allwinner_t113-s3/buildpicture.cmd
+++ build/allwinner_t113-s3/buildpicture.cmd
@@ -1,4 +1,5 @@
 @rem see https://www.gyan.dev/ffmpeg/builds/ for binaries
-ffmpeg -i picture.png -c:v rawvideo -pix_fmt nv12 -qscale:v 0 -an -y picture.yuv
-ffmpeg -i red_on_white.png -c:v rawvideo -pix_fmt nv12 -qscale:v 0 -an -y picture.yuv
+ffmpeg -i tvtest1-c:v rawvideo -pix_fmt nv12 -qscale:v 0 -an -y picture.yuv
+@rem ffmpeg -i picture.png -c:v rawvideo -pix_fmt nv12 -qscale:v 0 -an -y picture.yuv
+@rem ffmpeg -i red_on_white.png -c:v rawvideo -pix_fmt nv12 -qscale:v 0 -an -y picture.yuv
 bin2c.exe picture.yuv picture.h
