b8406378139b64b7ebe17786465ad3fad0ac4e5f
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-08-26 13:05:46 +0300

    custom cobfig update: устранил зависание bootloader

--- boards/arm_allwt507_cpustyle_xhelperboard_t507.h
+++ boards/arm_allwt507_cpustyle_xhelperboard_t507.h
@@ -87,7 +87,7 @@ void user_uart5_ontxchar(void * ctx);
 	#define WITHSDHCHW	1		/* Hardware SD HOST CONTROLLER */
 	#define WITHSDHC0HW	1		/* TF CARD */
 
-	//#define WITHSDRAMHW	1		/* В процессоре есть внешняя память */
+	#define WITHSDRAMHW	1		/* В процессоре есть внешняя память */
 	#define BOARD_CONFIG_DRAM_TYPE SUNXI_DRAM_TYPE_LPDDR4
 	#define BOARD_CONFIG_DRAM_CLK 792
 	#define CONFIG_SUNXI_DRAM_H616_LPDDR4 1
