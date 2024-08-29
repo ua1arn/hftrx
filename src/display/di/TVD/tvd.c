4550d95be90840748b87ca4434dc1be678914f94
mgs2001@mail.ru <mgs2001@mail.ru>
2024-08-29 23:15:08 +0300

    save test

--- src/display/di/TVD/tvd.c
+++ src/display/di/TVD/tvd.c
@@ -124,11 +124,295 @@ static uint32_t TVD_Status(void)                        //состояние: 0 
 
 static const unsigned tbx [] =
 {
+		50,
+		51,
+		52,
+		53,
+		54,
+		55,
+		56,
+		57,
+		58,
+		59,
+		60,
+		61,
+		62,
+		63,
+		64,
+		65,
+		66,
+		67,
+		68,
+		69,
+		70,
+		71,
+		72,
+		73,
+		74,
+		75,
+		76,
+		77,
+		78,
+		79,
+		80,
+		81,
+		82,
+		83,
+		84,
+		85,
+		86,
+		87,
+		88,
+		89,
+		90,
+		91,
+		92,
+		93,
+		94,
+		95,
+		96,
+		97,
+		98,
+		99,
+		100,
+		101,
+		102,
+		103,
+		104,
+		105,
+		106,
+		107,
+		108,
+		109,
+		110,
+		111,
+		112,
+		113,
+		114,
+		115,
+		116,
+		117,
+		118,
+		119,
+		120,
+		121,
+		122,
+		123,
+		124,
+		125,
+		126,
+		127,
+		128,
+		129,
+		198,
+		199,
+		200,
+		201,
+		202,
+		203,
+		204,
+		205,
+		206,
+		207,
+		208,
+		209,
+		210,
+		211,
+		212,
+		213,
+		214,
+		215,
+		216,
+		217,
+		218,
+		219,
+		220,
+		221,
+		222,
+		223,
+		224,
+		0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16,
+		17,
+		18,
+		19,
+		20,
+		21,
+		22,
+		23,
+		24,
+		25,
+		26,
+		27,
+		28,
+		29,
+		30,
+		31,
+		32,
+		33,
+		34,
+		35,
+		36,
+		37,
+		38,
+		39,
+		40,
+		41,
+		42,
+		43,
+		44,
+		45,
+		46,
+		47,
+		48,
+		49,
+		225,
+		226,
+		227,
+		228,
+		229,
+		230,
+		231,
+		232,
+		233,
+		234,
+		235,
+		236,
+		237,
+		238,
+		239,
+		240,
+		241,
+		242,
+		243,
+		244,
+		245,
+		246,
+		247,
+		248,
+		249,
+		250,
+		251,
+		252,
+		253,
+		254,
+		255,
+		256,
+		257,
+		258,
+		259,
+		260,
+		261,
+		262,
+		263,
+		264,
+		265,
+		266,
+		267,
+		268,
+		269,
+		270,
+		271,
+		272,
+		273,
+		274,
+		275,
+		276,
+		277,
+		278,
+		279,
+		280,
+		281,
+		282,
+		283,
+		284,
+		285,
+		286,
 		287,
-	#include "src/testdata/pal_set.bin"
+		130,
+		131,
+		132,
+		133,
+		134,
+		135,
+		136,
+		137,
+		138,
+		139,
+		140,
+		141,
+		142,
+		143,
+		144,
+		145,
+		146,
+		147,
+		148,
+		149,
+		150,
+		151,
+		152,
+		153,
+		154,
+		155,
+		156,
+		157,
+		158,
+		159,
+		160,
+		161,
+		162,
+		163,
+		164,
+		165,
+		166,
+		167,
+		168,
+		169,
+		170,
+		171,
+		172,
+		173,
+		174,
+		175,
+		176,
+		177,
+		178,
+		179,
+		180,
+		181,
+		182,
+		183,
+		184,
+		185,
+		186,
+		187,
+		188,
+		189,
+		190,
+		191,
+		192,
+		193,
+		194,
+		195,
+		196,
+		197,
 };
-
-static unsigned tbd [ARRAY_SIZE(tbx)];
 
 static unsigned encode288(unsigned v)
 {
@@ -136,13 +420,12 @@ static unsigned encode288(unsigned v)
 	return tbx [v];
 }
 
-static unsigned decode288(unsigned v)
+static unsigned pass288(unsigned v)
 {
-	ASSERT(v < ARRAY_SIZE(tbd));
-	return tbd [v];
+	return v;
 }
 
-static void vdecode(uintptr_t dst, uintptr_t src, unsigned (* recode288fn)(unsigned v))
+static void vdecode(uintptr_t dst, uintptr_t src, unsigned (* infn)(unsigned v), unsigned (* outfn)(unsigned v))
 {
 	uintptr_t vramsrc0 = src;
 	uintptr_t vramsrc1 = src + TVD_SIZE;
@@ -155,10 +438,11 @@ static void vdecode(uintptr_t dst, uintptr_t src, unsigned (* recode288fn)(unsig
 	unsigned row = 0;
 	for (row = 0; row < TVD_HEIGHT; row += 2)
 	{
-		const unsigned drow = 2 * recode288fn(row / 2);
+		const unsigned srow = 2 * infn(row / 2);
+		const unsigned drow = 2 * outfn(row / 2);
 		// копирование области двух строк
-		memcpy((void *) (vramdst0 + drow * TVD_WIDTH), (void *) (vramsrc0 + row * TVD_WIDTH), TVD_WIDTH * 2);		// для двух строк
-		memcpy((void *) (vramdst1 + drow * TVD_WIDTH / 2), (void *) (vramsrc1 + row * TVD_WIDTH / 2), TVD_WIDTH);	// для двух строк
+		memcpy((void *) (vramdst0 + drow * TVD_WIDTH), (void *) (vramsrc0 + srow * TVD_WIDTH), TVD_WIDTH * 2);		// для двух строк
+		memcpy((void *) (vramdst1 + drow * TVD_WIDTH / 2), (void *) (vramsrc1 + srow * TVD_WIDTH / 2), TVD_WIDTH);	// для двух строк
 	}
 	//memcpy(dst, src, TVD_SIZE * 3 / 2);
 }
@@ -185,14 +469,14 @@ void TVD_Handler(void)
 		if ((gpioX_getinputs(GPIOG) & EMASK) == 0)
 		{
 			uintptr_t vramout = allocate_dmabuffercolmain1fb();
-			vdecode(vramout, old, encode288);
+			vdecode(vramout, old, pass288, encode288);
 			save_dmabuffercolmain1fb(vramout);
 			release_dmabuffercolmain1fb(old);
 		}
 		else if ((gpioX_getinputs(GPIOG) & DMASK) == 0)
 		{
 			uintptr_t vramout = allocate_dmabuffercolmain1fb();
-			vdecode(vramout, old, decode288);
+			vdecode(vramout, old, encode288, pass288);
 			save_dmabuffercolmain1fb(vramout);
 			release_dmabuffercolmain1fb(old);
 		}
@@ -227,14 +511,6 @@ void cap_test(void)
 	arm_hardware_piog_inputs(EMASK);
 	arm_hardware_piog_inputs(DMASK);
 	PRINTF("ARRAY_SIZE(tbx)=%d\n", ARRAY_SIZE(tbx));
-	{
-		unsigned i;
-		ASSERT(288 == ARRAY_SIZE(tbx));
-		for (i = 0; i < ARRAY_SIZE(tbx); ++ i)
-		{
-			tbd [tbx[i]] = i;
-		}
-	}
 //	DI_INIT();
 //	di_dev_query_state_with_clear();                                  //очистка флага прерывания
 //	arm_hardware_set_handler_system(DI_IRQn, DI_Handler);
