12723a44df45b27f302581427f0ccadf0ec22af5
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-10-25 13:37:26 +0300

    save work state

--- src/display/ltdc.c
+++ src/display/ltdc.c
@@ -7869,172 +7869,6 @@ static void h3_de2_vsu_init(int rtmixid, const videomode_t * vdmodeDESIGN, const
 
 #if CPUSTYLE_T507 && WITHHDMITVHW
 
-static void xt507_de_initialize(const videomode_t * vdmode)
-{
-
-	// https://github.com/bigtreetech/CB1-Kernel/blob/244c0fd1a2a8e7f2748b2a9ae3a84b8670465351/u-boot/drivers/video/sunxi/sunxi_de2.c#L128
-
-	/* переключить память к DE & VI */
-    // https://github.com/bigtreetech/CB1-Kernel/blob/244c0fd1a2a8e7f2748b2a9ae3a84b8670465351/u-boot/drivers/video/sunxi/sunxi_de2.c#L39
-	SYS_CFG->MEMMAP_REG &= ~ (UINT32_C(1) << 24);
-
-	/* Configure DE clock (no FACTOR_N on T507/H616 CPU) */
-	//	CLK_SRC_SEL.
-	//	Clock Source Select
-	//	0: PLL_DE
-	//	1: PLL_PERI0(2X)
-	unsigned divider = 4;
-    CCU->DE_CLK_REG = (CCU->DE_CLK_REG & ~ (UINT32_C(1) << 24) & ~ (UINT32_C(0x0F) << 0)) |
-		0x01 * (UINT32_C(1) << 24) |	// CLK_SRC_SEL 0: PLL_DE 1: PLL_PERI0(2X)
-		(divider - 1) * (UINT32_C(1) << 0) |	// FACTOR_M 300 MHz
-		0;
-    CCU->DE_CLK_REG |= (UINT32_C(1) << 31);	// SCLK_GATING
-    local_delay_us(10);
-
-	//PRINTF("allwnr_t507_get_de_freq()=%" PRIuFAST32 " MHz\n", allwnr_t507_get_de_freq() / 1000 / 1000);
-	//PRINTF("allwnr_t507_get_mbus_freq()=%" PRIuFAST32 " MHz\n", allwnr_t507_get_mbus_freq() / 1000 / 1000);
-
-    CCU->DE_BGR_REG = (UINT32_C(1) << 0);		// Open the clock gate
-    CCU->DE_BGR_REG |= (UINT32_C(1) << 16);		// De-assert reset
-    local_delay_us(10);
-
- 	/* Global DE settings */
-
-	// https://github.com/BPI-SINOVOIP/BPI-M2U-bsp/blob/2adcf0fe39e54b9bcacbd5bcd3ecb6077e081122/linux-sunxi/drivers/video/sunxi/disp2/disp/de/lowlevel_v3x/de_clock.c#L91
-	// https://github.com/rvboards/linux_kernel_for_d1/blob/5703a18aa3ca12829027b0b20cd197e9741c4c0f/drivers/video/fbdev/sunxi/disp2/disp/de/lowlevel_v33x/de330/de_top.c#L245
-
-    {
-    	const unsigned disp = RTMIXIDLCD - 1;
-
-    	// CORE0..CORE3 bits valid
-
-     	DE_TOP->DE_SCLK_GATE |= UINT32_C(1) << disp;	// COREx_SCLK_GATE
-     	DE_TOP->DE_HCLK_GATE |= UINT32_C(1) << disp;	// COREx_HCLK_GATE
-
-    }
-#if 1 /* TCONTV_PTR */
-    {
-    	const unsigned disp = RTMIXIDLCD - 1;
-
-    	// CORE0..CORE3 bits valid
-
-     	DE_TOP->DE_SCLK_GATE |= UINT32_C(1) << disp;	// COREx_SCLK_GATE
-     	DE_TOP->DE_HCLK_GATE |= UINT32_C(1) << disp;	// COREx_HCLK_GATE
-
-    }
-#endif
- 	// Only one bit writable
- 	//DE_TOP->DE_AHB_RESET &= ~ (UINT32_C(1) << 0);	// CORE0_AHB_RESET
-	DE_TOP->DE_AHB_RESET |= (UINT32_C(1) << 0);		// CORE0_AHB_RESET
-
-	// DE_PORT2CHN_MUX [0]=9x00A98210
-	// bits 3:0 - BLD_EN_COLOR_CTL bit 8 (pipe0)
-	// bits 7:4 - BLD_EN_COLOR_CTL bit 9 (pipe1)
-	// ...
-	// каждлая четверка битов в DE_PORT2CHN_MUX говорит, какому из битов-источников в
-	// bld->BLD_EN_COLOR_CTL соответствует оверлей. Номера оверлеев начиная с 0 - VI, с 8 - UI
-
-	//de_rtmx_set_chn_mux(disp);
-
-	// T507, H616
-	//	RTMIX0: VI1, UI1
-	//	RTMIX1: VI2, UI2
-	// каждлая четверка битов в DE_PORT2CHN_MUX говорит, какому из битов-источников в
-	// bld->BLD_EN_COLOR_CTL соответствует оверлей. Номера оверлеев начиная с 0 - VI, с 8 - UI
-	{
-		DE_TOP->DE_PORT2CHN_MUX [0] =
-			0x00 * (UINT32_C(1) << (4 * 0)) | 	// VI1
-			0x08 * (UINT32_C(1) << (4 * 1)) | 	// UI1
-			0;
-		DE_TOP->DE_PORT2CHN_MUX [1] =
-			0x01 * (UINT32_C(1) << (4 * 0)) | 	// VI2
-			0x09 * (UINT32_C(1) << (4 * 1)) | 	// UI2
-			0;
-
-		DE_TOP->DE_CHN2CORE_MUX =
-				0x00 * (UINT32_C(1) << (2 * 0x00)) | 	// VI1 - CIRE0
-				0x00 * (UINT32_C(1) << (2 * 0x08)) | 	// UI1 - CIRE0
-				0x01 * (UINT32_C(1) << (2 * 0x01)) | 	// VI2 - CIRE1
-				0x01 * (UINT32_C(1) << (2 * 0x09)) | 	// UI2 - CIRE1
-				0;
-
-//		PRINTF("DE_PORT2CHN_MUX[0]=%08" PRIX32 "\n", DE_TOP->DE_PORT2CHN_MUX [0]);
-//		PRINTF("DE_PORT2CHN_MUX[1]=%08" PRIX32 "\n", DE_TOP->DE_PORT2CHN_MUX [1]);
-//		PRINTF("DE_CHN2CORE_MUX=%08" PRIX32 "\n", DE_TOP->DE_CHN2CORE_MUX);
-	}
-
-	{
-		const int rtmixid = RTMIXIDLCD;
-
-		DE_GLB_TypeDef * const glb = de3_getglb(rtmixid);
-		if (glb != NULL)
-		{
-			glb->GLB_CTL =
-					(UINT32_C(1) << 12) |	// OUT_DATA_WB 0:RT-WB fetch data after DEP port
-					(UINT32_C(1) << 0) |		// EN RT enable/disable
-					0;
-
-			glb->GLB_CLK |= (UINT32_C(1) << 0);
-
-			//* (volatile uint32_t *) (DE_TOP_BASE + 0x00C) = 1;	// это не делитель
-			//* (volatile uint32_t *) (DE_TOP_BASE + 0x010) |= 0xFFu;	// вешает. После сброса 0x000000E4
-			//* (volatile uint32_t *) (DE_TOP_BASE + 0x010) |= 0xFF000000u;
-
-			ASSERT(glb->GLB_CTL & (UINT32_C(1) << 0));
-		}
-	}
-#if 1 /* TCONTV_PTR */
-	{
-		const int rtmixid = RTMIXIDLCD;
-
-		DE_GLB_TypeDef * const glb = de3_getglb(rtmixid);
-		if (glb != NULL)
-		{
-			glb->GLB_CTL =
-					(UINT32_C(1) << 12) |	// OUT_DATA_WB 0:RT-WB fetch data after DEP port
-					(UINT32_C(1) << 0) |		// EN RT enable/disable
-					0;
-
-			glb->GLB_CLK |= (UINT32_C(1) << 0);
-
-			//* (volatile uint32_t *) (DE_TOP_BASE + 0x00C) = 1;	// это не делитель
-			//* (volatile uint32_t *) (DE_TOP_BASE + 0x010) |= 0xFFu;	// вешает. После сброса 0x000000E4
-			//* (volatile uint32_t *) (DE_TOP_BASE + 0x010) |= 0xFF000000u;
-
-			ASSERT(glb->GLB_CTL & (UINT32_C(1) << 0));
-		}
-	}
-#endif
-}
-
-
-// T113: PLL_VIDEO1
-// T507: PLL_VIDEO1
-static void xt507_tcon_PLL_configuration(void)
-{
-
-	// не меняем параметры по умолчанию (частота может поменяться для LVDS)
-	CCU->PLL_VIDEO1_CTRL_REG |= (UINT32_C(1) << 31) | (UINT32_C(1) << 30);
-
-	/* Lock enable */
-	CCU->PLL_VIDEO1_CTRL_REG |= (UINT32_C(1) << 29);
-
-	/* Wait pll stable */
-	while (! (CCU->PLL_VIDEO1_CTRL_REG & (UINT32_C(1) << 28)))
-		;
-
-	// не меняем параметры по умолчанию (частота может поменяться для LVDS)
-	CCU->PLL_VIDEO0_CTRL_REG |= (UINT32_C(1) << 31) | (UINT32_C(1) << 30);
-
-	/* Lock enable */
-	CCU->PLL_VIDEO0_CTRL_REG |= (UINT32_C(1) << 29);
-
-	/* Wait pll stable */
-	while (! (CCU->PLL_VIDEO0_CTRL_REG & (UINT32_C(1) << 28)))
-		;
-
-}
-
 
 static void xt507_tcontv_sequence_parameters(const videomode_t * vdmode)
 {
@@ -8152,6 +7986,7 @@ static void xt507_hdmi_tcon_initialize(const videomode_t * vdmode)
 {
 	xt507_tcon_hdmi_initsteps(vdmode);
 }
+
 static void xt507_tcontv_CCU_configuration(void)
 {
 	const uint_fast32_t needfreq = 27000000; //display_getdotclock(vdmode);
@@ -8203,7 +8038,7 @@ static void xt507_tcontv_CCU_configuration(void)
     //TCONLCD_PTR->LCD_IO_TRI_REG = UINT32_C(0xFFFFFFFF);
 }
 
-static void xt507_tcontv_initialize(const videomode_t * vdmode)
+static void t507_hdmi_tcon_init(const videomode_t * vdmode)
 {
 	// step0 - CCU configuration
 	//t113_tconlcd_CCU_configuration(vdmode, prei, divider, lvdsfreq);
@@ -8247,54 +8082,6 @@ static void xt507_tcontv_PLL_configuration(void)
 }
 
 
-
-static void xt507_hdmi_CCU_configuration(void)
-{
-
-    CCU->LVDS_BGR_REG |= (UINT32_C(1) << 16); // LVDS0_RST: De-assert reset (оба LVDS набора выходов разрешаются только одним битом)
-//    PRINTF("CCU->LVDS_BGR_REG=%08X\n", (unsigned) CCU->LVDS_BGR_REG);
-//    CCU->LVDS_BGR_REG |= (UINT32_C(1) << 16); // LVDS0_RST: De-assert reset (bits 19..16 writable)
-
-    //PRCM->VDD_SYS_PWROFF_GATING_REG |= (UINT32_C(1) << 4); // ANA_VDDON_GATING
-    local_delay_ms(10);
-    //PRINTF("PRCM->VDD_SYS_PWROFF_GATING_REG=%08X\n", (unsigned) PRCM->VDD_SYS_PWROFF_GATING_REG);
-
-    // CCU_32K select as CEC clock as default
-    // https://github.com/intel/mOS/blob/f67dfb38e6805f01ab96387597b24d4e3c285562/drivers/clk/sunxi-ng/ccu-sun50i-h616.c#L1135
-	/*
-	 * First clock parent (osc32K) is unusable for CEC. But since there
-	 * is no good way to force parent switch (both run with same frequency),
-	 * just set second clock parent here.
-	 */
-	CCU->HDMI_CEC_CLK_REG = 0x01 * (UINT32_C(1) << 24);
-
-    CCU->HDMI_CEC_CLK_REG |= (UINT32_C(1) << 31);	// SCLK_GATING
-    CCU->HDMI_CEC_CLK_REG |= (UINT32_C(1) << 30);	// PLL_PERI_GATING
-    //PRINTF("CCU->HDMI_CEC_CLK_REG=%08X\n", (unsigned) CCU->HDMI_CEC_CLK_REG);
-
-    CCU->HDMI0_CLK_REG |= (UINT32_C(1) << 31);
-    CCU->HDMI0_SLOW_CLK_REG |= (UINT32_C(1) << 31);
-
-    CCU->HDMI_BGR_REG |= (UINT32_C(1) << 0);	// HDMI0_GATING
-    CCU->HDMI_BGR_REG |= (UINT32_C(1) << 17) | (UINT32_C(1) << 16);	// HDMI0_SUB_RST HDMI0_MAIN_RST
-    //PRINTF("CCU->HDMI_BGR_REG=%08X\n", (unsigned) CCU->HDMI_BGR_REG);
-
-    if (0)
-    {
-    	// HDCP: High-bandwidth Digital Content Protection
-        CCU->HDMI_HDCP_CLK_REG |= (UINT32_C(1) << 31);	// SCLK_GATING
-        CCU->HDMI_HDCP_BGR_REG |= (UINT32_C(1) << 0);	// HDMI_HDCP_GATING
-        CCU->HDMI_HDCP_BGR_REG |= (UINT32_C(1) << 16);	// HDMI_HDCP_RST
-    }
-
-    //printhex32(HDMI_PHY_BASE, HDMI_PHY, 256);
-	DISP_IF_TOP->MODULE_GATING |= (UINT32_C(1) << 28);	// TV0_HDMI_GATE ???? may be not need
-	DISP_IF_TOP->MODULE_GATING |= (UINT32_C(1) << 20);	// TV0_GATE ???? may be not need
-	//PRINTF("DISP_IF_TOP->MODULE_GATING=%08X\n", (unsigned) DISP_IF_TOP->MODULE_GATING);
-
-    //PRINTF("HDMI_PHY->CEC_VERSION=%08X\n", (unsigned) HDMI_PHY->CEC_VERSION);
-    //PRINTF("HDMI_PHY->VERSION=%08X\n", (unsigned) HDMI_PHY->VERSION);
-}
 
 #endif /* CPUSTYLE_T507 */
 
@@ -8359,26 +8146,12 @@ void hardware_ltdc_initialize(const videomode_t * vdmode)
 		h3_hdmi_phy_init();
 		h3_hdmi_init(vdmode_HDMI);
 		h3_hdmi_tcon_init(vdmode_HDMI);
-#endif /* WITHHDMITVHW */
-
-
 #if CPUSTYLE_T507
-	//xt507_de_initialize(vdmode);
-	//xt507_tcon_PLL_configuration();	// VIDEO & VIDEO1
-	xt507_hdmi_tcon_initialize(vdmode);
-	xt507_tcontv_PLL_configuration();	// перенастройка для получения точных 216 и 27 МГц
-	xt507_tcontv_initialize(vdmode);
-	xt507_hdmi_CCU_configuration();
-
-	t507_hdmi_phy_initialize();
-//	xt507_hdmi_phy_initialize();
-//	xt507_hdmi_initialize();
-//	h3_hdmi_phy_init();
-//	h3_hdmi_init(vdmode);
-
-	t507_hdmi_edid_test();
-	t507_hdmi_edid_test();
+	t507_hdmi_tcon_init(vdmode_HDMI);
 #endif
+#endif /* WITHHDMITVHW */
+		t507_hdmi_edid_test();
+		t507_hdmi_edid_test();
 		{
 			const int rtmixid = RTMIXIDLCD;
 
