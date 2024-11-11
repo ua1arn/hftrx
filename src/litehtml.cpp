85b854654e8057cbddeb6dccc092795b20d0aca4
mgs2001@mail.ru <mgs2001@mail.ru>
2024-11-11 15:56:23 +0300

    save state

--- src/litehtml.cpp
+++ src/litehtml.cpp
@@ -14,7 +14,7 @@
 
 using namespace litehtml;
 
-class hftrxcontainer: public litehtml::document_container
+class hftrxgd: public litehtml::document_container
 {
 	// call back interface to draw text, images and other elements
 public:
@@ -55,8 +55,10 @@ public:
 //		return litehtml::string();
 //	}
 //	void split_text(const char *text, const std::function<void(const char*)> &on_word, const std::function<void(const char*)> &on_space);
-	hftrxcontainer() = default;
-	virtual ~hftrxcontainer() = default;
+	int m_dx;
+	int m_dy;
+	hftrxgd() = default;
+	virtual ~hftrxgd() = default;
 
 };
 
@@ -65,7 +67,7 @@ static COLORPIP_T getCOLPIP(const litehtml::web_color& color)
 	return TFTALPHA(color.alpha, TFTRGB(color.red, color.green, color.blue));
 }
 
-litehtml::uint_ptr hftrxcontainer::create_font(const char *faceName, int size, int weight, litehtml::font_style italic, unsigned int decoration, litehtml::font_metrics *fm)
+litehtml::uint_ptr hftrxgd::create_font(const char *faceName, int size, int weight, litehtml::font_style italic, unsigned int decoration, litehtml::font_metrics *fm)
 {
 	PRINTF("create_font: faceName='%s', size=%d\n", faceName, size);
 	if (fm)
@@ -78,18 +80,18 @@ litehtml::uint_ptr hftrxcontainer::create_font(const char *faceName, int size, i
 	}
 	return 1;
 }
-void hftrxcontainer::delete_font(litehtml::uint_ptr hFont)
+void hftrxgd::delete_font(litehtml::uint_ptr hFont)
 {
 	(void) hFont;
 }
 
-int hftrxcontainer::text_width(const char *text, litehtml::uint_ptr hFont)
+int hftrxgd::text_width(const char *text, litehtml::uint_ptr hFont)
 {
 	(void) hFont;
 	return SMALLCHARW * strlen(text);
 }
 
-void hftrxcontainer::draw_text(litehtml::uint_ptr hdc, const char *text, litehtml::uint_ptr hFont, litehtml::web_color color, const litehtml::position &pos)
+void hftrxgd::draw_text(litehtml::uint_ptr hdc, const char *text, litehtml::uint_ptr hFont, litehtml::web_color color, const litehtml::position &pos)
 {
 	//PRINTF("draw_text: text='%s'\n", text);
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
@@ -100,19 +102,19 @@ void hftrxcontainer::draw_text(litehtml::uint_ptr hdc, const char *text, litehtm
 	colpip_string_tbg(buffer, dx, dy, pos.left(), pos.top(), text, getCOLPIP(color));
 }
 
-int hftrxcontainer::pt_to_px(int pt) const
+int hftrxgd::pt_to_px(int pt) const
 {
 	return pt;
 }
-int hftrxcontainer::get_default_font_size() const
+int hftrxgd::get_default_font_size() const
 {
 	return 12;
 }
-const char* hftrxcontainer::get_default_font_name() const
+const char* hftrxgd::get_default_font_name() const
 {
 	return "Times New Roman";
 }
-void hftrxcontainer::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_marker &marker)
+void hftrxgd::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_marker &marker)
 {
 	TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
@@ -133,19 +135,19 @@ void hftrxcontainer::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::li
 //	colpip_fillrect(buffer, dx, dy, draw_x, draw_y, draw_width, draw_height, getCOLPIP(marker.color));
 }
 
-void hftrxcontainer::load_image(const char *src, const char *baseurl, bool redraw_on_ready)
+void hftrxgd::load_image(const char *src, const char *baseurl, bool redraw_on_ready)
 {
 	TP();
 }
-void hftrxcontainer::get_image_size(const char *src, const char *baseurl, litehtml::size &sz)
+void hftrxgd::get_image_size(const char *src, const char *baseurl, litehtml::size &sz)
 {
 	TP();
 }
-void hftrxcontainer::draw_image(litehtml::uint_ptr hdc, const background_layer &layer, const std::string &url, const std::string &base_url)
+void hftrxgd::draw_image(litehtml::uint_ptr hdc, const background_layer &layer, const std::string &url, const std::string &base_url)
 {
 	TP();
 }
-void hftrxcontainer::draw_solid_fill(litehtml::uint_ptr hdc, const background_layer &layer, const web_color &color)
+void hftrxgd::draw_solid_fill(litehtml::uint_ptr hdc, const background_layer &layer, const web_color &color)
 {
 	//PRINTF("draw_solid_fill: bottom_left_x=%d\n", layer.border_radius.bottom_left_x);
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
@@ -155,7 +157,7 @@ void hftrxcontainer::draw_solid_fill(litehtml::uint_ptr hdc, const background_la
 	colpip_fillrect(buffer, dx, dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, getCOLPIP(color));
 }
 
-void hftrxcontainer::draw_linear_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::linear_gradient &gradient)
+void hftrxgd::draw_linear_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::linear_gradient &gradient)
 {
 	//TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
@@ -166,7 +168,7 @@ void hftrxcontainer::draw_linear_gradient(litehtml::uint_ptr hdc, const backgrou
 	colpip_fillrect(buffer, dx, dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
 
 }
-void hftrxcontainer::draw_radial_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::radial_gradient &gradient)
+void hftrxgd::draw_radial_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::radial_gradient &gradient)
 {
 	//TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
@@ -178,7 +180,7 @@ void hftrxcontainer::draw_radial_gradient(litehtml::uint_ptr hdc, const backgrou
 
 }
 
-void hftrxcontainer::draw_conic_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::conic_gradient &gradient)
+void hftrxgd::draw_conic_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::conic_gradient &gradient)
 {
 	//TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
@@ -190,22 +192,24 @@ void hftrxcontainer::draw_conic_gradient(litehtml::uint_ptr hdc, const backgroun
 
 }
 
-void hftrxcontainer::draw_borders(litehtml::uint_ptr hdc, const litehtml::borders &borders, const litehtml::position &draw_pos, bool root)
+void hftrxgd::draw_borders(litehtml::uint_ptr hdc, const litehtml::borders &borders, const litehtml::position &draw_pos, bool root)
 {
 	//PRINTF("draw_borders: bottom_left_x=%d\n", borders.radius.bottom_left_x);
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
 	const uint_fast16_t dx = DIM_X;
 	const uint_fast16_t dy = DIM_Y;
-	COLORPIP_T color = COLORPIP_RED;
-
-	colpip_rect(buffer, dx, dy, draw_pos.left(), draw_pos.top(), draw_pos.right() - 1, draw_pos.bottom() - 1, color, 0);
+	COLORPIP_T top_color = getCOLPIP(borders.top.color);
+	COLORPIP_T botom_color = getCOLPIP(borders.bottom.color);
+	COLORPIP_T left_color = getCOLPIP(borders.left.color);
+	COLORPIP_T right_color = getCOLPIP(borders.right.color);
+	colpip_rect(buffer, dx, dy, draw_pos.left(), draw_pos.top(), draw_pos.right() - 1, draw_pos.bottom() - 1, top_color, 0);
 }
 
-void hftrxcontainer::set_caption(const char *caption)
+void hftrxgd::set_caption(const char *caption)
 {
 	//PRINTF("set_caption: caption='%s'\n", caption);
 }
-void hftrxcontainer::set_base_url(const char *base_url)
+void hftrxgd::set_base_url(const char *base_url)
 {
 	//PRINTF("set_base_url: base_url='%s'\n", base_url);
 }
@@ -213,47 +217,47 @@ void link(const std::shared_ptr<litehtml::document> &doc, const litehtml::elemen
 {
 	//TP();
 }
-void hftrxcontainer::on_anchor_click(const char *url, const litehtml::element::ptr &el)
+void hftrxgd::on_anchor_click(const char *url, const litehtml::element::ptr &el)
 {
 	TP();
 }
-void hftrxcontainer::on_mouse_event(const litehtml::element::ptr &el, litehtml::mouse_event event)
+void hftrxgd::on_mouse_event(const litehtml::element::ptr &el, litehtml::mouse_event event)
 {
 	TP();
 }
-void hftrxcontainer::set_cursor(const char *cursor)
+void hftrxgd::set_cursor(const char *cursor)
 {
 	TP();
 }
-void hftrxcontainer::transform_text(litehtml::string &text, litehtml::text_transform tt)
+void hftrxgd::transform_text(litehtml::string &text, litehtml::text_transform tt)
 {
 	TP();
 }
-void hftrxcontainer::import_css(litehtml::string &text, const litehtml::string &url, litehtml::string &baseurl)
+void hftrxgd::import_css(litehtml::string &text, const litehtml::string &url, litehtml::string &baseurl)
 {
 	//TP();
 }
 
-void hftrxcontainer::set_clip(const litehtml::position &pos, const litehtml::border_radiuses &bdr_radius)
+void hftrxgd::set_clip(const litehtml::position &pos, const litehtml::border_radiuses &bdr_radius)
 {
 	TP();
 }
-void hftrxcontainer::del_clip()
+void hftrxgd::del_clip()
 {
 	TP();
 }
-void hftrxcontainer::get_client_rect(litehtml::position &client) const
+void hftrxgd::get_client_rect(litehtml::position &client) const
 {
 	client = litehtml::position(0, 0, DIM_X, DIM_Y);
 }
 
-litehtml::element::ptr hftrxcontainer::create_element(const char *tag_name, const litehtml::string_map &attributes, const std::shared_ptr<litehtml::document> &doc)
+litehtml::element::ptr hftrxgd::create_element(const char *tag_name, const litehtml::string_map &attributes, const std::shared_ptr<litehtml::document> &doc)
 {
 	//PRINTF("create_element: tag_name='%s'\n", tag_name);
 	return 0;
 }
 
-void hftrxcontainer::get_media_features(litehtml::media_features &media) const
+void hftrxgd::get_media_features(litehtml::media_features &media) const
 {
 	position client;
 	get_client_rect(client);
@@ -269,20 +273,20 @@ void hftrxcontainer::get_media_features(litehtml::media_features &media) const
 	media.device_height = client.height; //1000; //GetDeviceCaps(m_tmp_hdc, VERTRES);
 }
 
-void hftrxcontainer::get_language(litehtml::string &language, litehtml::string &culture) const
+void hftrxgd::get_language(litehtml::string &language, litehtml::string &culture) const
 {
 	language = "en";
 	culture = "";
 }
 
-//void hftrxcontainer::split_text(const char *text, const std::function<void(const char*)> &on_word, const std::function<void(const char*)> &on_space)
+//void hftrxgd::split_text(const char *text, const std::function<void(const char*)> &on_word, const std::function<void(const char*)> &on_space)
 //{
 //	//PRINTF("split_text: text='%s'\n", text);
 //	//on_word("zzzzzz");
 //	on_word(text);
 //}
 
-void hftrxcontainer::link(const std::shared_ptr<litehtml::document>& doc, const litehtml::element::ptr& el)
+void hftrxgd::link(const std::shared_ptr<litehtml::document>& doc, const litehtml::element::ptr& el)
 {
 	//TP();
 }
@@ -297,13 +301,16 @@ void litehtmltest(void)
 {
 	uint_ptr hdc = 0;
 	const position wndclip(0, 0, DIM_X, DIM_Y);
-	hftrxcontainer cont;
+	hftrxgd cont;
 	// see doc/document_createFromString.txt
 	auto doc = document::createFromString(htmlString, & cont);
 
 	doc->render(wndclip.width, render_all);
 	doc->draw(hdc, 0, 0, & wndclip);
 	colmain_nextfb();
+	TP();
 	local_delay_ms(2500);
+	for (;;)
+		;
 }
 #endif
