e2e89f2a8057c986bd5918b88426859f1e28a305
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-11-12 12:38:43 +0300

    style

--- src/litehtml.cpp
+++ src/litehtml.cpp
@@ -57,7 +57,7 @@ public:
 //	void split_text(const char *text, const std::function<void(const char*)> &on_word, const std::function<void(const char*)> &on_space);
 	int m_dx;
 	int m_dy;
-	hftrxgd() = default;
+	hftrxgd(int dx, int dy) : m_dx(dx), m_dy(dy) { }
 	virtual ~hftrxgd() = default;
 
 };
@@ -95,11 +95,9 @@ void hftrxgd::draw_text(litehtml::uint_ptr hdc, const char *text, litehtml::uint
 {
 	//PRINTF("draw_text: text='%s'\n", text);
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
 
-	//colpip_fillrect(buffer, dx, dy, pos.left(), pos.top(), pos.width, pos.height, getCOLPIP(color));
-	colpip_string_tbg(buffer, dx, dy, pos.left(), pos.top(), text, getCOLPIP(color));
+	//colpip_fillrect(buffer, m_dx, m_dy, pos.left(), pos.top(), pos.width, pos.height, getCOLPIP(color));
+	colpip_string_tbg(buffer, m_dx, m_dy, pos.left(), pos.top(), text, getCOLPIP(color));
 }
 
 int hftrxgd::pt_to_px(int pt) const
@@ -118,10 +116,8 @@ void hftrxgd::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_mark
 {
 	TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
 
-    colpip_fillrect(buffer, dx, dy, marker.pos.left(), marker.pos.top(), marker.pos.width, marker.pos.height, getCOLPIP(marker.color));
+    colpip_fillrect(buffer, m_dx, m_dy, marker.pos.left(), marker.pos.top(), marker.pos.width, marker.pos.height, getCOLPIP(marker.color));
 
 //	int top_margin = marker.pos.height / 3;
 //	if (top_margin < 4)
@@ -132,7 +128,7 @@ void hftrxgd::draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_mark
 //	int draw_width = marker.pos.height - top_margin * 2;
 //	int draw_height = marker.pos.height - top_margin * 2;
 //
-//	colpip_fillrect(buffer, dx, dy, draw_x, draw_y, draw_width, draw_height, getCOLPIP(marker.color));
+//	colpip_fillrect(buffer, m_dx, m_dy, draw_x, draw_y, draw_width, draw_height, getCOLPIP(marker.color));
 }
 
 void hftrxgd::load_image(const char *src, const char *baseurl, bool redraw_on_ready)
@@ -151,32 +147,28 @@ void hftrxgd::draw_solid_fill(litehtml::uint_ptr hdc, const background_layer &la
 {
 	//PRINTF("draw_solid_fill: bottom_left_x=%d\n", layer.border_radius.bottom_left_x);
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
+	const uint_fast16_t m_dx = DIM_X;
+	const uint_fast16_t m_dy = DIM_Y;
 
-	colpip_fillrect(buffer, dx, dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, getCOLPIP(color));
+	colpip_fillrect(buffer, m_dx, m_dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, getCOLPIP(color));
 }
 
 void hftrxgd::draw_linear_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::linear_gradient &gradient)
 {
 	//TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
 	COLORPIP_T color = COLORPIP_RED;
 
-	colpip_fillrect(buffer, dx, dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
+	colpip_fillrect(buffer, m_dx, m_dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
 
 }
 void hftrxgd::draw_radial_gradient(litehtml::uint_ptr hdc, const background_layer &layer, const background_layer::radial_gradient &gradient)
 {
 	//TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
 	COLORPIP_T color = COLORPIP_GREEN;
 
-	colpip_fillrect(buffer, dx, dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
+	colpip_fillrect(buffer, m_dx, m_dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
 
 }
 
@@ -184,11 +176,9 @@ void hftrxgd::draw_conic_gradient(litehtml::uint_ptr hdc, const background_layer
 {
 	//TP();
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
 	COLORPIP_T color = COLORPIP_BLUE;
 
-	colpip_fillrect(buffer, dx, dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
+	colpip_fillrect(buffer, m_dx, m_dy, layer.border_box.left(), layer.border_box.top(), layer.border_box.width, layer.border_box.height, color);
 
 }
 
@@ -196,13 +186,11 @@ void hftrxgd::draw_borders(litehtml::uint_ptr hdc, const litehtml::borders &bord
 {
 	//PRINTF("draw_borders: bottom_left_x=%d\n", borders.radius.bottom_left_x);
 	PACKEDCOLORPIP_T * const buffer = colmain_fb_draw();
-	const uint_fast16_t dx = DIM_X;
-	const uint_fast16_t dy = DIM_Y;
 	COLORPIP_T top_color = getCOLPIP(borders.top.color);
 	COLORPIP_T botom_color = getCOLPIP(borders.bottom.color);
 	COLORPIP_T left_color = getCOLPIP(borders.left.color);
 	COLORPIP_T right_color = getCOLPIP(borders.right.color);
-	colpip_rect(buffer, dx, dy, draw_pos.left(), draw_pos.top(), draw_pos.right() - 1, draw_pos.bottom() - 1, top_color, 0);
+	colpip_rect(buffer, m_dx, m_dy, draw_pos.left(), draw_pos.top(), draw_pos.right() - 1, draw_pos.bottom() - 1, top_color, 0);
 }
 
 void hftrxgd::set_caption(const char *caption)
@@ -293,7 +281,7 @@ void hftrxgd::link(const std::shared_ptr<litehtml::document>& doc, const litehtm
 
 static const char htmlString [] =
 {
-	#include "testdata/nut.html.h"
+	#include "testdata/layout_800x480.html.h"
 	0,
 };
 
@@ -301,10 +289,10 @@ void litehtmltest(void)
 {
 	uint_ptr hdc = 0;
 	const position wndclip(0, 0, DIM_X, DIM_Y);
-	hftrxgd cont;
+	hftrxgd cont(DIM_X, DIM_Y);
 	// see doc/document_createFromString.txt
 	auto doc = document::createFromString(htmlString, & cont);
-
+	//element el_freq = doc->
 	doc->render(wndclip.width, render_all);
 	doc->draw(hdc, 0, 0, & wndclip);
 	colmain_nextfb();
