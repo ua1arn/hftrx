90f78a2c0d258b592a2dd556263344b3b5e44696
mgs2001@mail.ru <mgs2001@mail.ru>
2024-11-14 03:17:46 +0300

    style: html страница дизайна перенесена в код

--- src/litehtml.cpp
+++ src/litehtml.cpp
@@ -16,7 +16,8 @@
 #include <litehtml/el_td.h>
 #include <litehtml/render_item.h>
 
-using namespace litehtml;
+namespace litehtml
+{
 
 class hftrxgd: public litehtml::document_container
 {
@@ -301,8 +302,8 @@ void freqel::draw_background(uint_ptr hdc, int x, int y, const position *clip,
 	string text;
 	get_text(text);
 	PRINTF("draw: x=%d, y=%d, text='%s', x=%d, y=%d, w=%d, h=%d\n", x, y, text.c_str(), pos.x, pos.y, pos.width, pos.height);
-	colpip_fillrect(buffer, dx, dy, x + pos.x, y + pos.y, pos.width, pos.height, color);
-	//litehtml::el_td::draw_background(hdc, x, y, clip, ri);
+	//colpip_fillrect(buffer, dx, dy, x + pos.x, y + pos.y, pos.width, pos.height, color);
+	litehtml::el_td::draw_background(hdc, x, y, clip, ri);
 }
 
 void freqel::get_content_size(size& sz, int max_width)
@@ -348,7 +349,8 @@ litehtml::element::ptr hftrxgd::create_element(const char *tag_name, const liteh
 	try
 	{
 		std::string id = attributes.at("id");
-		PRINTF("create_element: tag_name='%s', id='%s'\n", tag_name, id.c_str());
+		std::string classname;// = attributes.at("class");
+		PRINTF("create_element: tag_name='%s', id='%s', class='%s'\n", tag_name, id.c_str(), classname.c_str());
 
 		if (id == "FREQ_A")
 		{
@@ -403,26 +405,444 @@ void hftrxgd::link(const std::shared_ptr<litehtml::document>& doc, const litehtm
 	//TP();
 }
 
-static const char htmlString [] =
+}	// namespace
+
+static const char hftrx_css [] = R"##(
+
+
+html {
+	display: block;
+}
+
+head {
+	display: none
+}
+
+meta {
+	display: none
+}
+
+title {
+	display: none
+}
+
+link {
+	display: none
+}
+
+style {
+	display: none
+}
+
+script {
+	display: none
+}
+
+body {
+	display:block; 
+	margin:0px; 
+}
+
+p {
+	display:block; 
+	margin-top:1em; 
+	margin-bottom:1em;
+}
+
+b, strong {
+	display:inline; 
+	font-weight:bold;
+}
+
+i, em, cite {
+	display:inline; 
+	font-style:italic;
+}
+
+ins, u {
+	text-decoration:underline
+}
+
+del, s, strike {
+	text-decoration:line-through
+}
+
+center 
 {
-	#include "testdata/layout_800x480.html.h"
-	0,
-};
+	text-align:center;
+	display:block;
+}
 
-static hftrxgd cont(DIM_X, DIM_Y);
-static const position wndclip(0, 0, DIM_X, DIM_Y);
+a:link
+{
+	text-decoration: underline;
+	color: #00f;
+	cursor: pointer;
+}
 
-//static hftrxgd cont2(DIM_X * 2, DIM_Y * 2);
-//static const position wndclip2(0, 0, DIM_X * 2, DIM_Y *2 );
+h1, h2, h3, h4, h5, h6, div {
+	display:block;
+}
+
+h1 {
+	font-weight:bold; 
+	margin-top:0.67em; 
+	margin-bottom:0.67em; 
+	font-size: 2em;
+}
+
+h2 {
+	font-weight:bold; 
+	margin-top:0.83em; 
+	margin-bottom:0.83em; 
+	font-size: 1.5em;
+}
+
+h3 {
+	font-weight:bold; 
+	margin-top:1em; 
+	margin-bottom:1em; 
+	font-size:1.17em;
+}
+
+h4 {
+	font-weight:bold; 
+	margin-top:1.33em; 
+	margin-bottom:1.33em
+}
+
+h5 {
+	font-weight:bold; 
+	margin-top:1.67em; 
+	margin-bottom:1.67em;
+	font-size:.83em;
+}
+
+h6 {
+	font-weight:bold; 
+	margin-top:2.33em; 
+	margin-bottom:2.33em;
+	font-size:.67em;
+} 
+
+br {
+	display:inline-block;
+}
+
+br[clear="all"]
+{
+	clear:both;
+}
+
+br[clear="left"]
+{
+	clear:left;
+}
+
+br[clear="right"]
+{
+	clear:right;
+}
+
+span {
+	display:inline
+}
+
+img {
+	display: inline-block;
+}
+
+img[align="right"]
+{
+	float: right;
+}
+
+img[align="left"]
+{
+	float: left;
+}
+
+hr {
+	display: block;
+	margin-top: 0.5em;
+	margin-bottom: 0.5em;
+	margin-left: auto;
+	margin-right: auto;
+	border-style: inset;
+	border-width: 1px
+}
+
+
+/***************** TABLES ********************/
+
+table {
+	display: table;
+	border-collapse: separate;
+	border-spacing: 2px;
+	border-top-color:gray;
+	border-left-color:gray;
+	border-bottom-color:black;
+	border-right-color:black;
+	font-size: medium;
+	font-weight: normal;
+	font-style: normal;
+}
+
+tbody, tfoot, thead {
+	display:table-row-group;
+	vertical-align:middle;
+}
+
+tr {
+	display: table-row;
+	vertical-align: inherit;
+	border-color: inherit;
+}
+
+td, th {
+	display: table-cell;
+	vertical-align: inherit;
+	border-width:1px;
+	padding:1px;
+}
+
+th {
+	font-weight: bold;
+}
+
+table[border] {
+	border-style:solid;
+}
+
+table[border^="0"] {
+	border-style:none;
+}
+
+table[border] td, table[border] th {
+	border-style:solid;
+	border-top-color:black;
+	border-left-color:black;
+	border-bottom-color:gray;
+	border-right-color:gray;
+}
+
+table[border^="0"] td, table[border^="0"] th {
+	border-style:none;
+}
+
+table[align=left] {
+   float: left;
+}
+
+table[align=right] {
+   float: right;
+}
+
+table[align=center] {
+   margin-left: auto;
+   margin-right: auto;
+}
+
+caption {
+	display: table-caption;
+}
+
+td[nowrap], th[nowrap] {
+	white-space:nowrap;
+}
+
+tt, code, kbd, samp {
+	font-family: monospace
+}
+
+pre, xmp, plaintext, listing {
+	display: block;
+	font-family: monospace;
+	white-space: pre;
+	margin: 1em 0
+}
+
+/***************** LISTS ********************/
+
+ul, menu, dir {
+	display: block;
+	list-style-type: disc;
+	margin-top: 1em;
+	margin-bottom: 1em;
+	margin-left: 0;
+	margin-right: 0;
+	padding-left: 40px
+}
+
+ol {
+	display: block;
+	list-style-type: decimal;
+	margin-top: 1em;
+	margin-bottom: 1em;
+	margin-left: 0;
+	margin-right: 0;
+	padding-left: 40px
+}
+
+li {
+	display: list-item;
+}
+
+ul ul, ol ul {
+	list-style-type: circle;
+}
+
+ol ol ul, ol ul ul, ul ol ul, ul ul ul {
+	list-style-type: square;
+}
+
+dd {
+	display: block;
+	margin-left: 40px;
+}
+
+dl {
+	display: block;
+	margin-top: 1em;
+	margin-bottom: 1em;
+	margin-left: 0;
+	margin-right: 0;
+}
+
+dt {
+	display: block;
+}
+
+ol ul, ul ol, ul ul, ol ol {
+	margin-top: 0;
+	margin-bottom: 0
+}
+
+blockquote {
+	display: block;
+	margin-top: 1em;
+	margin-bottom: 1em;
+	margin-left: 40px;
+	margin-right: 40px;
+}
+
+/*********** FORM ELEMENTS ************/
+
+form {
+	display: block;
+	margin-top: 0em;
+}
+
+option {
+	display: none;
+}
+
+input, textarea, keygen, select, button, isindex {
+	margin: 0em;
+	color: initial;
+	line-height: normal;
+	text-transform: none;
+	text-indent: 0;
+	text-shadow: none;
+	display: inline-block;
+}
+input[type="hidden"] {
+	display: none;
+}
+
+
+article, aside, footer, header, hgroup, nav, section 
+{
+	display: block;
+}
+
+sub {
+	vertical-align: sub;
+	font-size: smaller;
+}
+
+sup {
+	vertical-align: super;
+	font-size: smaller;
+}
+
+figure {
+	display: block;
+	margin-top: 1em;
+	margin-bottom: 1em;
+	margin-left: 40px;
+	margin-right: 40px;
+}
+
+figcaption {
+	display: block;
+}
+
+)##";
+
+static const char hftrx_layout_800x480 [] = R"##(
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<!-- Created by , GNU Texinfo 7.1 -->
+  <head>
+    <meta charset="utf-8">
+    <title>HF TRX</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.min.css">
+
+   <style>
+     td {
+         border-radius:5px;
+         background-color:red;
+      }
+   </style>
+
+  </head> 
+  <body style="background-color:green;">
+
+
+test<br>
+test2<br>
+test3<br>
+
+<table border="1" cellspacing="1" cellpadding="1" height="300">
+   <tr>
+      <td>Cell 1</td>
+      <td>Cell 2</td>
+      <td>Cell 3</td>
+   </tr>
+   <tr>
+      <td>Cell 4</td>
+      <td id="FREQ_A">Cell 5</td>
+      <td>Cell 6</td>
+   </tr>
+   <tr>
+      <td>Cell 7</td>
+      <td>Cell 8</td>
+      <td>Cell 9</td>
+   </tr>
+</table>
+
+  </body>
+</html>
+
+)##";
 
 void litehtmltest(void)
 {
+	using namespace litehtml;
+	static hftrxgd cont(DIM_X, DIM_Y);
+	static const litehtml::position wndclip(0, 0, DIM_X, DIM_Y);
+
+	//static hftrxgd cont2(DIM_X * 2, DIM_Y * 2);
+	//static const position wndclip2(0, 0, DIM_X * 2, DIM_Y *2 );
 	uint_ptr hdc = 0;
 	// see doc/document_createFromString.txt
-	auto doc = document::createFromString(htmlString, & cont);
+	auto doc = litehtml::document::createFromString(hftrx_layout_800x480, & cont, hftrx_css);
 	//auto doc2 = document::createFromString(htmlString, & cont2);
 	//element el_freq = doc->
-	doc->render(wndclip.width, render_all);
+	doc->render(wndclip.width, litehtml::render_all);
 	//doc2->render(wndclip2.width, render_all);
 
 	doc->draw(hdc, 0, 0, & wndclip);
