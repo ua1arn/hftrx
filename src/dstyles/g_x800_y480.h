55404c5864c4fae5c89493345201dd4d5fc2b0cd
mgs2001@mail.ru <mgs2001@mail.ru>
2024-11-25 15:44:44 +0300

    display layout update

--- src/dstyles/g_x800_y480.h
+++ src/dstyles/g_x800_y480.h
@@ -77,15 +77,15 @@
 		{	0,	0,	0, 0, display2_clearbg, 	REDRM_MODE, PGALL | REDRSUBSET_SLEEP, },
 		{	0,	0,	0, 0, display2_keyboard_screen0,	REDRM_KEYB, PGALL | REDRSUBSET_SLEEP, }, // Обработка клавиатуры и валкодеров при нахождении в режиме основного экрана
 
-		{	0,	0, 	7, 5, display_siglevel7, 	REDRM_BARS, PGALL, },	// signal level dBm
-		//{	0,	0, 	5, 5, display2_smeors5, 	REDRM_BARS, PGALL, },	// уровень сигнала в баллах S или dBm
-		{	10,	0,	4, 5, display2_rxbwval4,	REDRM_MODE, PGALL, },	// RX BW value
-		{	15,	0,	2, 5, display_txrxstate2, REDRM_MODE, PGALL, },
-		{	18, 0,	3, 5, display2_atu3,		REDRM_MODE, PGALL, },	// TUNER state (optional)
-		{	22, 0,	3, 5, display2_byp3,		REDRM_MODE, PGALL, },	// TUNER BYPASS state (optional)
-		{	26,	0,	5, 5, display2_ant5,		REDRM_MODE, PGALL, },
-		{	32,	0,	4, 5, display2_att4,		REDRM_MODE, PGALL, },
-		{	37,	0,	3, 5, display2_preovf3,	REDRM_BARS, PGALL, },
+		{	0,	0, 	7, 4, display_siglevel7, 	REDRM_BARS, PGALL, },	// signal level dBm
+		//{	0,	0, 	5, 4, display2_smeors5, 	REDRM_BARS, PGALL, },	// уровень сигнала в баллах S или dBm
+		{	10,	0,	4, 4, display2_rxbwval4,	REDRM_MODE, PGALL, },	// RX BW value
+		{	15,	0,	2, 4, display_txrxstate2, REDRM_MODE, PGALL, },
+		{	18, 0,	3, 4, display2_atu3,		REDRM_MODE, PGALL, },	// TUNER state (optional)
+		{	22, 0,	3, 4, display2_byp3,		REDRM_MODE, PGALL, },	// TUNER BYPASS state (optional)
+		{	26,	0,	5, 4, display2_ant5,		REDRM_MODE, PGALL, },
+		{	32,	0,	4, 4, display2_att4,		REDRM_MODE, PGALL, },
+		{	37,	0,	3, 4, display2_preovf3,	REDRM_BARS, PGALL, },
 	#if WITHBARS
 		{   0, 	4,  0, 0, display2_smeter15_init,REDRM_INIS, PGINI, },	//  Инициализация стрелочного прибора
 		{   0, 	4,	15, 20, display2_smeter15, 	REDRM_BARS, PGALL, },	// Изображение стрелочного прибора
@@ -99,39 +99,39 @@
 		{	15,	6,	0, 0, display2_freqX_a_init,	REDRM_INIS, PGINI, },	// MAIN FREQ Частота (большие цифры)
 		{	15,	6,	21, 13, display2_freqX_a,	REDRM_FREQ, PGALL, },	// MAIN FREQ Частота Герцы маленьким шрифтом.
 
-		{	41, 0,	9, 5, display2_fnlabel9,	REDRM_MODE, PGALL, },	// FUNC item label
-		{	41,	4,	9, 5, display2_fnvalue9,	REDRM_MODE, PGALL, },	// FUNC item value
+		{	41, 0,	9, 4, display2_fnlabel9,	REDRM_MODE, PGALL, },	// FUNC item label
+		{	41,	4,	9, 4, display2_fnvalue9,	REDRM_MODE, PGALL, },	// FUNC item value
 
-		{	37, 10,	3, 5, display2_mode3_a,	REDRM_MODE,	PGALL, },	// SSB/CW/AM/FM/...
-		{	41, 10,	3, 5, display2_rxbw3,		REDRM_MODE, PGALL, },	// 3.1 / 0,5 / WID / NAR
-		{	46, 10,	3, 5, display2_agc3,		REDRM_MODE, PGALL, },	// AGC mode
+		{	37, 10,	3, 4, display2_mode3_a,	REDRM_MODE,	PGALL, },	// SSB/CW/AM/FM/...
+		{	41, 10,	3, 4, display2_rxbw3,		REDRM_MODE, PGALL, },	// 3.1 / 0,5 / WID / NAR
+		{	46, 10,	3, 4, display2_agc3,		REDRM_MODE, PGALL, },	// AGC mode
 
-		{	37, 15,	3, 5, display2_nr3,		REDRM_MODE, PGALL, },	// NR : was: AGC
-		{	41, 15,	3, 5, display2_datamode3,	REDRM_MODE, PGALL, },	// DATA mode indicator
-		{	45, 15,	5, 5, display2_notch5,	REDRM_MODE, PGALL, },	// NOTCH on/off
+		{	37, 15,	3, 4, display2_nr3,		REDRM_MODE, PGALL, },	// NR : was: AGC
+		{	41, 15,	3, 4, display2_datamode3,	REDRM_MODE, PGALL, },	// DATA mode indicator
+		{	45, 15,	5, 4, display2_notch5,	REDRM_MODE, PGALL, },	// NOTCH on/off
 
 		{	15, 20,	3, 0, display2_mainsub3,	REDRM_MODE, PGALL, },	// main/sub RX: A/A, A/B, B/A, etc
-		{	20,	20,	3, 5, display2_vfomode3,	REDRM_MODE, PGALL, },	// SPL
-		{	24,	20,	12, 5, display2_freqX_b,	REDRM_FRQB, PGALL, },	// SUB FREQ
-		{	37, 20,	3, 5, display2_mode3_b,	REDRM_MODE,	PGALL, },	// SSB/CW/AM/FM/...
-		{	41, 20,	3, 5, display2_voxtune3,	REDRM_MODE, PGALL, },	// VOX
-		{	45,	20,	4, 5, display2_lockstate4, REDRM_MODE, PGALL, },	// LOCK
+		{	20,	20,	3, 4, display2_vfomode3,	REDRM_MODE, PGALL, },	// SPL
+		{	24,	20,	12, 4, display2_freqX_b,	REDRM_FRQB, PGALL, },	// SUB FREQ
+		{	37, 20,	3, 4, display2_mode3_b,	REDRM_MODE,	PGALL, },	// SSB/CW/AM/FM/...
+		{	41, 20,	3, 4, display2_voxtune3,	REDRM_MODE, PGALL, },	// VOX
+		{	45,	20,	4, 4, display2_lockstate4, REDRM_MODE, PGALL, },	// LOCK
 
 		// размещены под S-метром (15 ячеек)
-		{	1, 	25,	5, 5, display2_voltlevelV5, REDRM_VOLT, PGALL, },	// voltmeter with "V"
-		{	7, 	25,	6, 5, display2_currlevelA6, REDRM_VOLT, PGALL, },	// PA drain current d.dd with "A"
+		{	1, 	25,	5, 4, display2_voltlevelV5, REDRM_VOLT, PGALL, },	// voltmeter with "V"
+		{	7, 	25,	6, 4, display2_currlevelA6, REDRM_VOLT, PGALL, },	// PA drain current d.dd with "A"
 
 		//{	14, 25,
 		//{	19, 25,
 		//{	23, 25,
 		//{	27, 25,
-		{	33, 25,	3, 5, display2_rec3,		REDRM_BARS, PGALL, },	// Отображение режима записи аудио фрагмента
-		{	37, 25,	3, 5, display2_spk3,		REDRM_MODE, PGALL, },	// отображение признака включения динамика
-		{	41, 25, 3, 5, display2_bkin3,		REDRM_MODE, PGALL, },	// BREAK-IN
-		{	45,	25,	5, 5, display2_wpm5, 		REDRM_BARS, PGALL, },	// 22WPM
+		{	33, 25,	3, 4, display2_rec3,		REDRM_BARS, PGALL, },	// Отображение режима записи аудио фрагмента
+		{	37, 25,	3, 4, display2_spk3,		REDRM_MODE, PGALL, },	// отображение признака включения динамика
+		{	41, 25, 3, 4, display2_bkin3,		REDRM_MODE, PGALL, },	// BREAK-IN
+		{	45,	25,	5, 4, display2_wpm5, 		REDRM_BARS, PGALL, },	// 22WPM
 
-		//{	24, 30,	10, 5, display_freqmeter10, REDRM_BARS, PGALL, },	// измеренная частота опоры
-		{	37, 30,	8, 5, display2_freqdelta8, REDRM_BARS, PGALL, },	// выход ЧМ демодулятора
+		//{	24, 30,	10, 4, display_freqmeter10, REDRM_BARS, PGALL, },	// измеренная частота опоры
+		{	37, 30,	8, 4, display2_freqdelta8, REDRM_BARS, PGALL, },	// выход ЧМ демодулятора
 
 	#if WITHSPECTRUMWF
 		{	0,	DLES,	0, 0, display2_wfl_init,	REDRM_INIS,	PGINI, },	// формирование палитры водопада
@@ -139,11 +139,11 @@
 		{	0,	DLES,	BDTH_ALLRX, BDCV_ALLRX, display2_gcombo,	REDRM_BARS, PG0, },// подготовка изображения спектра
 	#endif /* WITHSPECTRUMWF */
 
-		{	0,	DLE1,	12, 5, display2_datetime12,	REDRM_BARS, PGALL,	},	// DATE&TIME Jan-01 13:40
-		{	13,	DLE1,	9, 5, display2_span9,		REDRM_MODE, PGALL, },	/* Получить информацию об ошибке настройки в режиме SAM */
-		{	23, DLE1,	4, 5, display2_thermo4,	REDRM_VOLT, PGALL, },	// thermo sensor
-		{	28, DLE1,	3, 5, display2_usbsts3,		REDRM_BARS, PG0, },	// USB host status
-		{	32, DLE1,	7, 5, display2_classa7,		REDRM_BARS, PGALL, },	// Class-A power amplifier
+		{	0,	DLE1,	12, 4, display2_datetime12,	REDRM_BARS, PGALL,	},	// DATE&TIME Jan-01 13:40
+		{	13,	DLE1,	9, 4, display2_span9,		REDRM_MODE, PGALL, },	/* Получить информацию об ошибке настройки в режиме SAM */
+		{	23, DLE1,	4, 4, display2_thermo4,	REDRM_VOLT, PGALL, },	// thermo sensor
+		{	28, DLE1,	3, 4, display2_usbsts3,		REDRM_BARS, PG0, },	// USB host status
+		{	32, DLE1,	7, 4, display2_classa7,		REDRM_BARS, PGALL, },	// Class-A power amplifier
 		//{	28, DLE1,	10, 5, display_freqmeter10, REDRM_BARS, PGALL, },	// измеренная частота опоры
 
 	#if WITHMENU
@@ -157,8 +157,8 @@
 	#endif /* WITHMENU */
 
 		// sleep mode display
-		{	5,	25,	12, 5, display2_datetime12,	REDRM_BARS, PGSLP, },	// DATE & TIME // DATE&TIME Jan-01 13:40
-		{	20, 25,	5, 5, display2_voltlevelV5, REDRM_VOLT, PGSLP, },	// voltmeter with "V"
+		{	5,	25,	12, 4, display2_datetime12,	REDRM_BARS, PGSLP, },	// DATE & TIME // DATE&TIME Jan-01 13:40
+		{	20, 25,	5, 4, display2_voltlevelV5, REDRM_VOLT, PGSLP, },	// voltmeter with "V"
 	#if WITHMENU
 		{	15, 25,	0, 0, display2_popup,		REDRM_MODE, PG0, },	// Всплывающие меню. В конце массива для перекрытия всего что под ним
 	#endif /* WITHMENU */
@@ -205,42 +205,42 @@ R"##(
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <!--link rel="stylesheet" type="text/css" href="hftrx.css"-->
 <style>
- #id2 { position:absolute; left:0px; top:0px; width:112px; height:25px; }
- #id3 { position:absolute; left:160px; top:0px; width:64px; height:25px; }
- #id4 { position:absolute; left:240px; top:0px; width:32px; height:25px; }
- #id5 { position:absolute; left:288px; top:0px; width:48px; height:25px; }
- #id6 { position:absolute; left:352px; top:0px; width:48px; height:25px; }
- #id7 { position:absolute; left:416px; top:0px; width:80px; height:25px; }
- #id8 { position:absolute; left:512px; top:0px; width:64px; height:25px; }
- #id9 { position:absolute; left:592px; top:0px; width:48px; height:25px; }
+ #id2 { position:absolute; left:0px; top:0px; width:112px; height:20px; }
+ #id3 { position:absolute; left:160px; top:0px; width:64px; height:20px; }
+ #id4 { position:absolute; left:240px; top:0px; width:32px; height:20px; }
+ #id5 { position:absolute; left:288px; top:0px; width:48px; height:20px; }
+ #id6 { position:absolute; left:352px; top:0px; width:48px; height:20px; }
+ #id7 { position:absolute; left:416px; top:0px; width:80px; height:20px; }
+ #id8 { position:absolute; left:512px; top:0px; width:64px; height:20px; }
+ #id9 { position:absolute; left:592px; top:0px; width:48px; height:20px; }
  #id11 { position:absolute; left:0px; top:20px; width:240px; height:100px; }
  #id13 { position:absolute; left:240px; top:30px; width:336px; height:65px; }
- #id14 { position:absolute; left:656px; top:0px; width:144px; height:25px; }
- #id15 { position:absolute; left:656px; top:20px; width:144px; height:25px; }
- #id16 { position:absolute; left:592px; top:50px; width:48px; height:25px; }
- #id17 { position:absolute; left:656px; top:50px; width:48px; height:25px; }
- #id18 { position:absolute; left:736px; top:50px; width:48px; height:25px; }
- #id19 { position:absolute; left:592px; top:75px; width:48px; height:25px; }
- #id20 { position:absolute; left:656px; top:75px; width:48px; height:25px; }
- #id21 { position:absolute; left:720px; top:75px; width:80px; height:25px; }
- #id23 { position:absolute; left:320px; top:100px; width:48px; height:25px; }
- #id24 { position:absolute; left:384px; top:100px; width:192px; height:25px; }
- #id25 { position:absolute; left:592px; top:100px; width:48px; height:25px; }
- #id26 { position:absolute; left:656px; top:100px; width:48px; height:25px; }
- #id27 { position:absolute; left:720px; top:100px; width:64px; height:25px; }
- #id28 { position:absolute; left:16px; top:125px; width:80px; height:25px; }
- #id29 { position:absolute; left:112px; top:125px; width:96px; height:25px; }
- #id30 { position:absolute; left:528px; top:125px; width:48px; height:25px; }
- #id31 { position:absolute; left:592px; top:125px; width:48px; height:25px; }
- #id32 { position:absolute; left:656px; top:125px; width:48px; height:25px; }
- #id33 { position:absolute; left:720px; top:125px; width:80px; height:25px; }
- #id34 { position:absolute; left:592px; top:150px; width:128px; height:25px; }
+ #id14 { position:absolute; left:656px; top:0px; width:144px; height:20px; }
+ #id15 { position:absolute; left:656px; top:20px; width:144px; height:20px; }
+ #id16 { position:absolute; left:592px; top:50px; width:48px; height:20px; }
+ #id17 { position:absolute; left:656px; top:50px; width:48px; height:20px; }
+ #id18 { position:absolute; left:736px; top:50px; width:48px; height:20px; }
+ #id19 { position:absolute; left:592px; top:75px; width:48px; height:20px; }
+ #id20 { position:absolute; left:656px; top:75px; width:48px; height:20px; }
+ #id21 { position:absolute; left:720px; top:75px; width:80px; height:20px; }
+ #id23 { position:absolute; left:320px; top:100px; width:48px; height:20px; }
+ #id24 { position:absolute; left:384px; top:100px; width:192px; height:20px; }
+ #id25 { position:absolute; left:592px; top:100px; width:48px; height:20px; }
+ #id26 { position:absolute; left:656px; top:100px; width:48px; height:20px; }
+ #id27 { position:absolute; left:720px; top:100px; width:64px; height:20px; }
+ #id28 { position:absolute; left:16px; top:125px; width:80px; height:20px; }
+ #id29 { position:absolute; left:112px; top:125px; width:96px; height:20px; }
+ #id30 { position:absolute; left:528px; top:125px; width:48px; height:20px; }
+ #id31 { position:absolute; left:592px; top:125px; width:48px; height:20px; }
+ #id32 { position:absolute; left:656px; top:125px; width:48px; height:20px; }
+ #id33 { position:absolute; left:720px; top:125px; width:80px; height:20px; }
+ #id34 { position:absolute; left:592px; top:150px; width:128px; height:20px; }
  #id37 { position:absolute; left:0px; top:175px; width:800px; height:275px; }
- #id38 { position:absolute; left:0px; top:455px; width:192px; height:25px; }
- #id39 { position:absolute; left:208px; top:455px; width:144px; height:25px; }
- #id40 { position:absolute; left:368px; top:455px; width:64px; height:25px; }
- #id41 { position:absolute; left:448px; top:455px; width:48px; height:25px; }
- #id42 { position:absolute; left:512px; top:455px; width:112px; height:25px; }
+ #id38 { position:absolute; left:0px; top:455px; width:192px; height:20px; }
+ #id39 { position:absolute; left:208px; top:455px; width:144px; height:20px; }
+ #id40 { position:absolute; left:368px; top:455px; width:64px; height:20px; }
+ #id41 { position:absolute; left:448px; top:455px; width:48px; height:20px; }
+ #id42 { position:absolute; left:512px; top:455px; width:112px; height:20px; }
 </style>
 </head>
 <body style="background-color:orange;">
