4a4037707397150dfba04e859b8cd2b9ac5d5337
mgs2001@mail.ru <mgs2001@mail.ru>
2024-11-25 13:10:02 +0300

    Fix rv64 config build

---
+++ arch/aw_f133/arm_acle.h
@@ -0,0 +1,13 @@
+/*
+ * arm_acle.h
+ *
+ *  Created on: Nov 19, 2024
+ *      Author: gena
+ */
+
+#ifndef ARCH_AW_F133_ARM_ACLE_H_
+#define ARCH_AW_F133_ARM_ACLE_H_
+
+
+
+#endif /* ARCH_AW_F133_ARM_ACLE_H_ */
