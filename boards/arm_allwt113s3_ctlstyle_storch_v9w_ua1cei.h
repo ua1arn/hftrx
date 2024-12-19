6daf34a84c2088f190510afd4782bea3d10233f3
Genadi V. Zawidowski <mgs2001@mail.ru>
2024-12-19 16:38:17 +0300

    custom config update

--- boards/arm_allwt113s3_ctlstyle_storch_v9w_ua1cei.h
+++ boards/arm_allwt113s3_ctlstyle_storch_v9w_ua1cei.h
@@ -313,7 +313,7 @@
 
 	#define WITHRTS96 1		/* Получение от FPGA квадратур, возможно передача по USB и отображение спектра/водопада. */
 
-	#define WITHGRADIENT_FIXED 1	/* использование массива цветов как базы для создания палитры водопада. */
+	//#define WITHGRADIENT_FIXED 1	/* использование массива цветов как базы для создания палитры водопада. */
 	//#define COLORSTYLE_GREEN	1
 	#define COLORSTYLE_BLUE	1
 	//#define COLORSTYLE_RED	1
