c7d14ab70bf50fcc31f9d0a7b99596674472f298
mgs2001@mail.ru <mgs2001@mail.ru>
2024-12-20 01:35:23 +0300

    grnregdef: add GIC registers

--- arch/vm14/device.h
+++ arch/vm14/device.h
@@ -97,8 +97,11 @@ typedef enum IRQn
 #define CMCTR_BASE ((uintptr_t) 0x38094000)           /*!< CMCTR Контроллер управления синхронизацией Base */
 #define PMCTR_BASE ((uintptr_t) 0x38095000)           /*!< PMCTR Контроллер управления энергопотреблением Base */
 #define SMCTR_BASE ((uintptr_t) 0x38096000)           /*!< SMCTR общиме системные настройки микросхемы Base */
+#define GIC_BASE ((uintptr_t) 0x39000000)             /*!< GIC  Base */
 #define GIC_INTERFACE_BASE ((uintptr_t) 0x39000100)   /*!< GIC_INTERFACE GIC CPU IF Base */
 #define GIC_DISTRIBUTOR_BASE ((uintptr_t) 0x39001000) /*!< GIC_DISTRIBUTOR  Base */
+#define GICVSELF_BASE ((uintptr_t) 0x39004000)        /*!< GICV  Base */
+#define GICV_BASE ((uintptr_t) 0x39005000)            /*!< GICV  Base */
 
 #if __aarch64__
     #include <core64_ca.h>
@@ -144,6 +147,30 @@ typedef __PACKED_STRUCT CMCTR_Type
     __IO uint32_t SEL_VPLL;                           /*!< Offset 0x110 Регистр контроля VPLL */
     __IO uint32_t SEL_UPLL;                           /*!< Offset 0x114 Регистр контроля UPLL */
 } CMCTR_TypeDef; /* size of structure = 0x118 */
+/*
+ * @brief GICV
+ */
+/*!< GICV  */
+typedef __PACKED_STRUCT GICV_Type
+{
+    __IO uint32_t GICH_HCR;                           /*!< Offset 0x000 RW 0x00000000 Hypervisor Control Register */
+    __IO uint32_t GICH_VTR;                           /*!< Offset 0x004 RO 0x90000003 VGIC Type Register, GICH_VTR on page 3-13 */
+    __IO uint32_t GICH_VMCR;                          /*!< Offset 0x008 RW 0x004C0000 Virtual Machine Control Register */
+         uint32_t reserved_0x00C;
+    __IO uint32_t GICH_MISR;                          /*!< Offset 0x010 RO 0x00000000 Maintenance Interrupt Status Register */
+         uint32_t reserved_0x014 [0x0003];
+    __IO uint32_t GICH_EISR0;                         /*!< Offset 0x020 RO 0x00000000 End of Interrupt Status Register */
+         uint32_t reserved_0x024 [0x0003];
+    __IO uint32_t GICH_ELSR0;                         /*!< Offset 0x030 RO 0x0000000F Empty List register Status Register */
+         uint32_t reserved_0x034 [0x002F];
+    __IO uint32_t GICH_APR0;                          /*!< Offset 0x0F0 RW 0x00000000 Active Priority Register */
+         uint32_t reserved_0x0F4 [0x0003];
+    __IO uint32_t GICH_LR0;                           /*!< Offset 0x100 RW 0x00000000 List Register 0 */
+    __IO uint32_t GICH_LR1;                           /*!< Offset 0x104 RW 0x00000000 List Register 1 */
+    __IO uint32_t GICH_LR2;                           /*!< Offset 0x108 RW 0x00000000 List Register 2 */
+    __IO uint32_t GICH_LR3;                           /*!< Offset 0x10C RW 0x00000000 List Register 3 */
+         uint32_t reserved_0x110 [0x003C];
+} GICV_TypeDef; /* size of structure = 0x200 */
 /*
  * @brief GPIO
  */
@@ -641,6 +668,8 @@ typedef __PACKED_STRUCT UART_Type
 #define CMCTR ((CMCTR_TypeDef *) CMCTR_BASE)          /*!< CMCTR Контроллер управления синхронизацией register set access pointer */
 #define PMCTR ((PMCTR_TypeDef *) PMCTR_BASE)          /*!< PMCTR Контроллер управления энергопотреблением register set access pointer */
 #define SMCTR ((SMCTR_TypeDef *) SMCTR_BASE)          /*!< SMCTR общиме системные настройки микросхемы register set access pointer */
+#define GICVSELF ((GICV_TypeDef *) GICVSELF_BASE)     /*!< GICVSELF  register set access pointer */
+#define GICV ((GICV_TypeDef *) GICV_BASE)             /*!< GICV  register set access pointer */
 
 
 #endif /* HEADER_00003039_INCLUDED */
--- tools/genregdefs/xc7z/cmsis_xc7z.cmd
+++ tools/genregdefs/xc7z/cmsis_xc7z.cmd
@@ -1,6 +1,7 @@
 SET CONVERTER=..\Debug\genregdefs.exe 
 SET OUTFILE=cmsis_xc7z.h
 SET LISTOFFILES= ^
+	gic.csv ^
 	dmacthreads.csv dmacchannels.csv dmac.csv
 
 %CONVERTER% %LISTOFFILES% > %OUTFILE%
