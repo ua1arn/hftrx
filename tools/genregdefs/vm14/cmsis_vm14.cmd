5a93e604bd9b4328925862a9896896e8e24abbb8
mgs2001@mail.ru <mgs2001@mail.ru>
2024-12-29 07:57:16 +0300

    Remove Salute VM15 CPU dupport

--- tools/genregdefs/vm14/cmsis_vm14.cmd
+++
@@ -1,9 +0,0 @@
-SET CONVERTER=..\Debug\genregdefs.exe 
-SET OUTFILE=..\..\..\arch\vm14\device.h
-SET FILELIST=pmctr.csv smctr.csv cmctr.csv gic.csv uart.csv gpio.csv sdma.csv nandmport.csv normport.csv mfbsp.csv i2c.csv i2s.csv swic.csv swic_mfpsp_dma.csv
-
-%CONVERTER% %FILELIST% > %OUTFILE%
-
-%CONVERTER% --svd %FILELIST% > elvees_vm14.svd
-
-@pause
