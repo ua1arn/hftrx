ee1b5fe0e4c0fb5b4a25be22d1477d3d987f7e0e
mgs2001@mail.ru <mgs2001@mail.ru>
2024-12-30 10:10:42 +0300

    Пожжержка компилятора (библиотеки) вынесена в отдельны файл

---
+++ src/gccsupport.c
@@ -0,0 +1,257 @@
+/* $Id$ */
+//
+// Проект HF Dream Receiver (КВ приёмник мечты)
+// автор Гена Завидовский mgs2001@mail.ru
+// UA1ARN
+//
+
+#include "hardware.h"	/* зависящие от процессора функции работы с портами */
+#include "formats.h"	/* зависящие от процессора функции работы с портами */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+#if ! LINUX_SUBSYSTEM && ! (CPUSTYLE_ATMEGA || CPUSTYLE_ATXMEGA)
+
+#if (CPUSTYLE_RISCV || defined(__aarch64__))
+
+/**
+  \brief   Initializes data and bss sections
+  \details This default implementations initialized all data and additional bss
+           sections relying on .copy.table and .zero.table specified properly
+           in the used linker script.
+
+ */
+__NO_RETURN void __riscv_start(void)
+{
+  extern void _start(void) __NO_RETURN;
+
+  typedef struct {
+    volatile uint32_t const* src;
+    volatile uint32_t* dest;
+    ptrdiff_t  wlen;
+  } __copy_table_t;
+
+  typedef struct {
+	volatile uint32_t* dest;
+    ptrdiff_t  wlen;
+  } __zero_table_t;
+
+  extern const __copy_table_t __copy_table_start64__;
+  extern const __copy_table_t __copy_table_end64__;
+  extern const __zero_table_t __zero_table_start64__;
+  extern const __zero_table_t __zero_table_end64__;
+
+  for (__copy_table_t const* pTable = &__copy_table_start64__; pTable < &__copy_table_end64__; ++pTable) {
+    for(ptrdiff_t i=0u; i<pTable->wlen; ++i) {
+      pTable->dest[i] = pTable->src[i];
+    }
+  }
+
+  for (__zero_table_t const* pTable = &__zero_table_start64__; pTable < &__zero_table_end64__; ++pTable) {
+    for(ptrdiff_t i=0u; i<pTable->wlen; ++i) {
+      pTable->dest[i] = 0u;
+    }
+  }
+
+  _start();
+}
+#endif /* CPUSTYLE_RISCV || defined(__aarch64__) */
+
+// Используется в случае наличия ключа ld -nostartfiles
+// Так же смотреть вокруг software_init_hook
+// see https://github.com/gmarkall/newlib/blob/de33102df7309e77441dfc76aa8cdef6d26da209/newlib/libc/sys/arm/crt0.S#L1
+
+extern int main(void);
+extern void __libc_init_array(void);
+
+void __NO_RETURN _start(void)
+{
+	__libc_init_array();	// invoke constructors
+    /* Branch to main function */
+    main();
+     /* Infinite loop */
+	for (;;)
+		;
+}
+
+// call after __preinit_array_xxx and before __init_array_xxx passing
+void _init(void)
+{
+}
+
+void * __dso_handle;
+
+void _fini(void)
+{
+	for (;;)
+		;
+}
+
+void _exit(int code)
+{
+	for (;;)
+		;
+}
+
+// See
+// https://github.com/littlekernel/newlib/blob/master/libgloss/arm/crt0.S
+
+// User mode only:           This routine makes default target specific Stack
+
+void _stack_init(void)
+{
+
+}
+
+#include <sys/stat.h>
+#include <sys/unistd.h>
+#include <string.h>
+#include <errno.h>
+
+static int SER_GetChar(void)
+{
+
+	return (-1);
+}
+
+int __attribute__((used)) (_open)(const char * path, int flags, ...)
+{
+	return (-1);
+}
+
+int __attribute__((used)) (_close)(int fd) {
+	return (-1);
+}
+
+int __attribute__((used)) (_unlink)(const char * path) {
+	return (-1);
+}
+
+int __attribute__((used)) (_lseek)(int fd, int ptr, int dir)
+{
+	return (0);
+}
+
+int __attribute__((used)) (_stat)(const char *__restrict path, struct stat *__restrict st)
+{
+	memset(st, 0, sizeof(*st));
+	st->st_mode = S_IFCHR;
+	return (0);
+}
+
+int __attribute__((used)) (_fstat)(int fd, struct stat *st )
+{
+	memset(st, 0, sizeof(*st));
+	st->st_mode = S_IFCHR;
+	return (0);
+}
+
+int __attribute__((used)) (_isatty)(int fd)
+{
+	return (1);
+}
+
+int __attribute__((used)) (_kill)(int id)
+{
+	return (-1);
+}
+
+int __attribute__((used)) (_getpid)(int id)
+{
+	return (-1);
+}
+
+int __attribute__((used)) (_read)(int fd, char * ptr, int len) {
+	char c;
+	int i;
+
+	for (i = 0; i < len; i++) {
+		c = SER_GetChar();
+		if (c == 0x0D)
+			break;
+		*ptr++ = c;
+		while (dbg_putchar(c) == 0)
+			;
+	}
+	return (len - i);
+}
+
+int __attribute__((used)) (_write)(int fd, char * ptr, int len)
+{
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		const char c = * ptr ++;
+		while (dbg_writechar(c) == 0)
+			;
+	}
+	return (i);
+}
+
+struct _reent * __getreent(void)
+{
+    static struct _reent r [4];
+    PRINTF("__getreent: CPU%u\n", arm_hardware_cpuid());
+    return r + arm_hardware_cpuid();
+}
+
+#if defined(__aarch64__)
+
+// Fix dc zva, xx opcode usage
+void * memset(void * dst, int v, size_t n)
+{
+	volatile uint8_t * d = (volatile uint8_t *) dst;
+	while (n --)
+		* d ++ = v;
+	return dst;
+}
+
+void * memcpy(void * dst, const void * src, size_t n)
+{
+	volatile uint8_t * d = (volatile uint8_t *) dst;
+	const volatile uint8_t * s = (const volatile uint8_t *) src;
+	while (n --)
+		* d ++ = * s ++;
+	return dst;
+}
+
+void * memmove(void * dst, const void * src, size_t n)
+{
+	volatile uint8_t * d = (volatile uint8_t *) dst;
+	const volatile uint8_t * s = (const volatile uint8_t *) src;
+	if (s >= d && s < (d + n))
+	{
+		// If overlaps
+		s += n;
+		d += n;
+		while (n --)
+			* -- d = * -- s;
+	}
+	else
+	{
+		while (n --)
+			* d ++ = * s ++;
+	}
+	return dst;
+}
+
+size_t strlen(const char * s1)
+{
+	volatile const char * s = s1;
+	size_t n = 0;
+	while (* s ++ != '\0')
+		++ n;
+	return n;
+}
+#endif /* defined(__aarch64__) */
+
+#endif /* ! LINUX_SUBSYSTEM && ! (CPUSTYLE_ATMEGA || CPUSTYLE_ATXMEGA) */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
