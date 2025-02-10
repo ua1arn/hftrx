f82a29764cd44eb580ee0c19ad3c230616a1db6d
RA4ASN <ra4asn@rambler.ru>
2025-02-10 18:16:08 +0300

    RK3566: OpenGL ES 3.2 acceleration

--- src/linux/sdl2.c
+++ src/linux/sdl2.c
@@ -9,7 +9,7 @@
 
 #include "linux_subsystem.h"
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <GLES3/gl32.h>
 
 #if WITHSDL2VIDEO
 
@@ -24,7 +24,13 @@ int sdl2_render_init(void)
         return 0;
     }
 
-    window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DIM_X, DIM_Y, SDL_WINDOW_RESIZABLE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DIM_X, DIM_Y, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
@@ -32,6 +38,12 @@ int sdl2_render_init(void)
         sdl2_render_close();
         return 0;
     }
+
+    // Get OpenGL version information
+    const char* glVersion = (const char*)glGetString(GL_VERSION);
+    const char* glRenderer = (const char*)glGetString(GL_RENDERER);
+    printf("OpenGL Version: %s\n", glVersion);
+    printf("OpenGL Renderer: %s\n", glRenderer);
 
     SDL_RenderSetScale(renderer, 1.28f, 1.25f); // 800x480 -> 1024x600
     ASSERT(SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"));
@@ -53,7 +65,6 @@ void sdl2_render_close(void)
 void sdl2_render_update(uintptr_t frame)
 {
 	ASSERT(! SDL_UpdateTexture(texture, NULL, (PACKEDCOLORPIP_T *) frame, DIM_X * sizeof(PACKEDCOLORPIP_T)));
-	ASSERT(SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"));
 	SDL_Rect destRect = { 0, 0, DIM_X, DIM_Y };
 	SDL_RenderCopy(renderer, texture, NULL, & destRect);
 	SDL_RenderPresent(renderer);
