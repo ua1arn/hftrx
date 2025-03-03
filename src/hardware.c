346b8a9fe884e33311bad0469a4575b1a9c4f6bc
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-03-03 09:07:49 +0300

    style

--- src/hardware.c
+++ src/hardware.c
@@ -4160,15 +4160,10 @@ static void aarch64_mp_cpuN_start(uintptr_t startfunc, unsigned targetcore)
 static void aarch32_mp_cpuN_start(uintptr_t startfunc, unsigned targetcore)
 {
 	const uint32_t CORE_RESET_MASK = UINT32_C(1) << targetcore;	// CPU0_CORE_RESET
-	//volatile uint32_t * const rvaddr = ((volatile uint32_t *) (R_CPUCFG_BASE + 0x1c4 + targetcore * 4));
-	//ASSERT(startfunc != 0);
-	ASSERT(targetcore != 0);
 
 	C0_CPUX_CFG->C0_RST_CTRL &= ~ CORE_RESET_MASK;
 	R_CPUCFG->SOFTENTRY [targetcore] = startfunc;
-	//* rvaddr = startfunc;
-	//ASSERT(* rvaddr == startfunc);
-	dcache_clean_all();	// startup code should be copied in to sysram for example.
+	dcache_clean_all();				// startup code should be copied in to sysram for example.
 	C0_CPUX_CFG->C0_RST_CTRL |= CORE_RESET_MASK;
 }
 
