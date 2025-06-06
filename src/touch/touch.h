56abc1bf81e20d02110ffeade937ba7bf21df9a2
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-06-07 00:57:43 +0300

    fix c++ compile

--- src/touch/touch.h
+++ src/touch/touch.h
@@ -3,10 +3,18 @@
 
 #include "hardware.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void board_tsc_initialize(void);
 uint_fast8_t board_tsc_getxy(uint_fast16_t * x, uint_fast16_t * y);	/* touch screen interface */
 uint_fast8_t board_tsc_getraw(uint_fast16_t * x, uint_fast16_t * y);	/* touch screen interface */
 uint_fast16_t board_tsc_normalize_x(uint_fast16_t x, uint_fast16_t y, const void * params);	/* получение координаты нажатия в пределах 0..DIM_X-1 */
 uint_fast16_t board_tsc_normalize_y(uint_fast16_t x, uint_fast16_t y, const void * params);	/* получение координаты нажатия в пределах 0..DIM_Y-1 */
+
+#ifdef __cplusplus
+ }
+#endif
 
 #endif /* TOUCH_H_INCLUDED */
