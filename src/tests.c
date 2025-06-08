82a49d7a766831d58dd191e83186476d14a7790e
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-06-08 23:03:09 +0300

    lvgl: lv_canvas test

--- src/tests.c
+++ src/tests.c
@@ -10046,41 +10046,92 @@ static void lidar_parse(unsigned char c)
 //#include "../demos/vector_graphic/lv_demo_vector_graphic.h"
 #include "src/lvgl_gui/styles.h"
 
+#define CANVAS_WIDTH  DIM_X		//200
+#define CANVAS_HEIGHT  DIM_Y	//150
+
 /**
  * Draw a line to the canvas
  */
 static void lv_example_canvas_7(void)
 {
-	/*Create a buffer for the canvas*/
-	LV_DRAW_BUF_DEFINE_STATIC(draw_buf, DIM_X, DIM_Y, LV_COLOR_FORMAT_ARGB8888);
-	LV_DRAW_BUF_INIT_STATIC(draw_buf);
-	/*Create a canvas and initialize its palette*/
-	lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
-	lv_canvas_set_draw_buf(canvas, & draw_buf);
-	lv_canvas_fill_bg(canvas, lv_color_hex3(0xccc), LV_OPA_COVER);
-	lv_obj_center(canvas);
+    lv_draw_rect_dsc_t rect_dsc;
+    lv_draw_rect_dsc_init(&rect_dsc);
+    rect_dsc.radius = 10;
+    rect_dsc.bg_opa = LV_OPA_COVER;
+    rect_dsc.bg_grad.dir = LV_GRAD_DIR_VER;
+    rect_dsc.bg_grad.stops[0].color = lv_palette_main(LV_PALETTE_RED);
+    rect_dsc.bg_grad.stops[0].opa = LV_OPA_100;
+    rect_dsc.bg_grad.stops[1].color = lv_palette_main(LV_PALETTE_BLUE);
+    rect_dsc.bg_grad.stops[1].opa = LV_OPA_50;
+    rect_dsc.border_width = 2;
+    rect_dsc.border_opa = LV_OPA_90;
+    rect_dsc.border_color = lv_color_white();
+    rect_dsc.shadow_width = 5;
+    rect_dsc.shadow_offset_x = 5;
+    rect_dsc.shadow_offset_y = 5;
 
-	lv_layer_t layer;
-	lv_canvas_init_layer(canvas, &layer);
+    lv_draw_label_dsc_t label_dsc;
+    lv_draw_label_dsc_init(&label_dsc);
+    label_dsc.color = lv_palette_main(LV_PALETTE_ORANGE);
+    label_dsc.text = "Some text on text canvas";
+    /*Create a buffer for the canvas*/
+    LV_DRAW_BUF_DEFINE_STATIC(draw_buf_16bpp, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_RGB565);
+    LV_DRAW_BUF_INIT_STATIC(draw_buf_16bpp);
 
-	lv_draw_line_dsc_t dsc;
-	lv_draw_line_dsc_init(&dsc);
+    {
+        lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
 
-	dsc.color = lv_palette_main(LV_PALETTE_RED);
-	dsc.width = 4;
-	dsc.round_end = 1;
-	dsc.round_start = 1;
-	dsc.p1.x = 15;
-	dsc.p1.y = 15;
-	dsc.p2.x = 35;
-	dsc.p2.y = 10;
-	lv_draw_line(& layer, &dsc);
-	TP();
+        lv_canvas_set_draw_buf(canvas, &draw_buf_16bpp);
+        lv_obj_center(canvas);
+        lv_canvas_fill_bg(canvas, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_OPA_COVER);
 
-	lv_canvas_finish_layer(canvas, &layer);
-	TP();
-    for (;;)
-    	lv_timer_handler();
+        lv_layer_t layer;
+        lv_canvas_init_layer(canvas, &layer);
+
+        lv_area_t coords_rect = {30, 20, 100, 70};
+        lv_draw_rect(&layer, &rect_dsc, &coords_rect);
+
+        lv_area_t coords_text = {40, 80, 100, 120};
+        lv_draw_label(&layer, &label_dsc, &coords_text);
+
+        lv_canvas_finish_layer(canvas, &layer);
+
+        //lv_obj_delete(canvas);
+		lv_obj_set_flag(canvas, LV_OBJ_FLAG_HIDDEN, 1);
+     }
+    /*Test the rotation. It requires another buffer where the original image is stored.
+     *So use previous canvas as image and rotate it to the new canvas*/
+    LV_DRAW_BUF_DEFINE_STATIC(draw_buf_32bpp, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_ARGB8888);
+    LV_DRAW_BUF_INIT_STATIC(draw_buf_32bpp);
+    {
+        /*Create a canvas and initialize its palette*/
+        lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
+
+        lv_canvas_set_draw_buf(canvas, &draw_buf_32bpp);
+        lv_canvas_fill_bg(canvas, lv_color_hex3(0xccc), LV_OPA_COVER);
+        lv_obj_center(canvas);
+
+        lv_canvas_fill_bg(canvas, lv_palette_lighten(LV_PALETTE_GREY, 1), LV_OPA_COVER);
+
+        lv_layer_t layer;
+        lv_canvas_init_layer(canvas, &layer);
+        lv_image_dsc_t img;
+        lv_draw_buf_to_image(&draw_buf_16bpp, &img);	// копируктся ранее нарисованный буфер
+        lv_draw_image_dsc_t img_dsc;
+        lv_draw_image_dsc_init(&img_dsc);
+        img_dsc.rotation = 120;
+        img_dsc.src = &img;
+        img_dsc.pivot.x = CANVAS_WIDTH / 2;
+        img_dsc.pivot.y = CANVAS_HEIGHT / 2;
+
+        lv_area_t coords_img = {0, 0, CANVAS_WIDTH - 1, CANVAS_HEIGHT - 1};
+        lv_draw_image(&layer, &img_dsc, &coords_img);
+
+        lv_canvas_finish_layer(canvas, &layer);
+        //lv_obj_delete(canvas);
+		lv_obj_set_flag(canvas, LV_OBJ_FLAG_HIDDEN, 1);
+
+    }
 }
 #endif /* WITHLVGL */
 
@@ -10104,6 +10155,8 @@ void hightests(void)
 #if 0 && WITHLVGL
 	{
 		lv_example_canvas_7();
+//		for (;;)
+//			lv_timer_handler();
 	}
 #endif
 #if 0
