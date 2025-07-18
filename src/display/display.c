05136db2e12c7e55a77fbf223dc87c5e3c4ef34c
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-07-18 22:00:24 +0300

    Fix lvgl builds

--- src/display/display.c
+++ src/display/display.c
@@ -994,7 +994,7 @@ display_text(const gxdrawb_t * db, uint_fast8_t xcell, uint_fast8_t ycell, const
 		lv_area_t coords;
 	    lv_draw_label_dsc_init(& l);
 		lv_draw_rect_dsc_init(& d);
-		lv_area_set(& coords, xpix, ypix, xpix + GRID2X(xspan) - 1, ypix + DISPLAY_AT_H - 1);
+		lv_area_set(& coords, xpix, ypix, xpix + GRID2X(xspan) - 1, ypix + GRID2Y(yspan) - 1);
 	    d.bg_color = display_lvlcolor(ltdc_bg);
 	    l.color = display_lvlcolor(ltdc_fg);
 	    l.align = LV_TEXT_ALIGN_RIGHT;
