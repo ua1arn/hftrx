2df6815c40b92c0acf6069248ee7404e3a4f2682
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-07-29 23:46:52 +0300

    style

--- inc/spi.h
+++ inc/spi.h
@@ -17,6 +17,31 @@
 extern "C" {
 #endif /* __cplusplus */
 
+
+#define SPIBASEconcat(base, index) base ## index
+
+#if CPUSTYLE_R7S721
+	typedef struct st_rspi SPI_t;
+	#define SPIBASENAME(port) SPIBASEconcat(& SCIF, port)
+#elif CPUSTYLE_ALLWINNER
+	typedef SPI_TypeDef SPI_t;
+	#define SPIBASENAME(port) SPIBASEconcat(SPI, port)
+#elif CPUSTYLE_STM32F
+	typedef SPI_TypeDef SPI_t;
+	#define SPIBASENAME(port) SPIBASEconcat(SPI, port)
+#elif CPUSTYLE_STM32MP1
+	typedef SPI_TypeDef SPI_t;
+	#define SPIBASENAME(port) SPIBASEconcat(USART, port)
+#elif CPUSTYLE_ROCKCHIP
+	typedef SPI_TypeDef SPI_t;
+	#define SPIBASENAME(port) SPIBASEconcat(SPI, port)
+#elif CPUSTYLE_XC7Z
+	typedef XSPIPS_Registers SPI_t;
+	#define SPIBASENAME(port) SPIBASEconcat(SPI, port)
+#else
+	typedef void SPI_t;
+#endif
+
 void spi_initialize(void);	// отдельно инициализация SPI
 
 /* Управление SPI. Так как некоторые периферийные устройства не могут работать с 8-битовыми блоками
