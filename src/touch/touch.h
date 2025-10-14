369422dd6c24ca18afc230c900b245e71ed389f1
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-10-14 11:18:01 +0300

    comments

--- src/touch/touch.h
+++ src/touch/touch.h
@@ -26,7 +26,7 @@ uint_fast8_t board_tsc_getxy(uint_fast16_t * x, uint_fast16_t * y);	/* touch scr
 uint_fast8_t board_tsc_getraw(uint_fast16_t * xr, uint_fast16_t * yr, uint_fast16_t * zr);	/* touch screen interface */
 uint_fast16_t board_tsc_normalize_x(uint_fast16_t x, uint_fast16_t y, const void * params);	/* получение координаты нажатия в пределах 0..DIM_X-1 */
 uint_fast16_t board_tsc_normalize_y(uint_fast16_t x, uint_fast16_t y, const void * params);	/* получение координаты нажатия в пределах 0..DIM_Y-1 */
-tPoint * board_tsc_getcalpoints(void);
+tPoint * board_tsc_getcalpoints(void);	/* поддержка калибровки */
 
 #ifdef __cplusplus
  }
