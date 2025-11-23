0bb0587b235f911a565c56b0bebfae2b725da946
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-11-23 17:21:45 +0300

    code style

--- src/display/display.c
+++ src/display/display.c
@@ -983,95 +983,10 @@ void gxdrawb_initlvgl(gxdrawb_t * db, void * layerv)
 #endif /* WITHLVGL //&& ! LINUX_SUBSYSTEM */
 
 // Используется при выводе на графический индикатор с кординатами и размерами в пикселях
-void
-pix_display_text(const gxdrawb_t * db, uint_fast16_t xpix, uint_fast16_t ypix, uint_fast16_t w, uint_fast16_t h, const gxstyle_t * dbstyle, const char * s)
-{
-	size_t len;
-	char c;
-	const COLORPIP_T fg = dbstyle->textcolor;
-
-	savestring = s;
-	savewhere = __func__;
-	if (dbstyle->bgradius)
-	{
-		w -= GXSTYLE_BACKOFF;
-		h -= GXSTYLE_BACKOFF;
-	}
-#if WITHLVGL
-	lv_layer_t * const layer = (lv_layer_t *) db->layerv;
-	if (layer)
-	{
-		//PRINTF("x/y=%d/%d '%s'\n", xpix, ypix, s);
-		lv_draw_rect_dsc_t d;
-	    lv_draw_label_dsc_t l;
-		lv_area_t coords;
-	    lv_draw_label_dsc_init(& l);
-		lv_draw_rect_dsc_init(& d);
-		lv_area_set(& coords, xpix, ypix, xpix + GRID2X(xspan) - 1, ypix + GRID2Y(yspan) - 1);
-	    d.bg_color = display_lvlcolor(dbstyle->bgcolor);
-	    l.color = display_lvlcolor(fg);
-	    l.align = LV_TEXT_ALIGN_RIGHT;
-	    l.flag = 0*LV_TEXT_FLAG_EXPAND | LV_TEXT_FLAG_FIT;
-	    l.text = s;
-	    l.font = & Epson_LTDC_small;
-	    //PRINTF("display_string: x/y=%d/%d '%s'\n", (int) xpix, (int) xpix, s);
-		lv_draw_rect(layer, & d, & coords);
-        lv_draw_label(layer, & l, & coords);
-
-        return;
-	}
-#endif
-	ASSERT3(w >= (dbstyle->bgradius * 2), __FILE__, __LINE__, s);
-	ASSERT3(h >= (dbstyle->bgradius * 2), __FILE__, __LINE__, s);
-	const uint_fast16_t avlw = w - (dbstyle->bgradius * 2);
-	const uint_fast16_t avlh = h - (dbstyle->bgradius * 2);
-	colmain_rounded_rect(db, xpix, ypix, xpix + w - 1, ypix + h - 1, dbstyle->bgradius, dbstyle->bgcolor, 1);
-	xpix += dbstyle->bgradius;
-	ypix += dbstyle->bgradius;
-	switch (dbstyle->textvalign)
-	{
-	default:
-	case GXSTYLE_VALIGN_CENTER:
-		if (avlh > smallfont_height())
-			ypix += (avlh - dbstyle->font_height()) / 2;
-		break;
-	case GXSTYLE_VALIGN_TOP:
-		break;
-
-	case GXSTYLE_VALIGN_BOTTOM:
-		if (avlh > smallfont_height())
-			ypix += (avlh - dbstyle->font_height());
-		break;
-	}
-
-	const uint_fast16_t textw = ulmin16(avlw, gxstyle_strwidth(dbstyle, s));
-	const uint_fast16_t xpix0 = xpix;
-	//ASSERT3(avlw >= textw, __FILE__, __LINE__, s);
-	switch (dbstyle->texthalign)
-	{
-	default:
-	case GXSTYLE_HALIGN_RIGHT:
-		xpix = textw < w ? xpix + avlw - textw : xpix;
-		while ((c = * s ++) != '\0' && xpix - xpix0 + dbstyle->font_width(c) <= avlw)
-			xpix = dbstyle->font_draw_char(db, xpix, ypix, c, fg);
-		break;
-	case GXSTYLE_HALIGN_LEFT:
-		while ((c = * s ++) != '\0' && xpix - xpix0 + dbstyle->font_width(c) <= avlw)
-			xpix = dbstyle->font_draw_char(db, xpix, ypix, c, fg);
-		break;
-	case GXSTYLE_HALIGN_CENTER:
-		// todo: to be implemented
-		break;
-	}
-
-}
-
 // Многострочное отображение
 void pix_display_texts(const gxdrawb_t * db, uint_fast16_t xpixB, uint_fast16_t ypix, uint_fast16_t w, uint_fast16_t h, const gxstyle_t * dbstyle, const char * const * slines, unsigned nlines)
 {
 	size_t len;
-	char c;
-	const COLORPIP_T fg = dbstyle->textcolor;
 
 	savewhere = __func__;
 	if (dbstyle->bgradius)
@@ -1112,8 +1027,11 @@ void pix_display_texts(const gxdrawb_t * db, uint_fast16_t xpixB, uint_fast16_t 
 	const uint_fast16_t vstep = avlh / nlines;
 	for (; nlines --; ypix += vstep)
 	{
+		const COLORPIP_T fg = dbstyle->textcolor;
 		uint_fast16_t xpix = xpixB + dbstyle->bgradius;
 		const char * s = * slines ++;
+		char c;
+
 		savestring = s;
 		switch (dbstyle->textvalign)
 		{
@@ -1152,6 +1070,13 @@ void pix_display_texts(const gxdrawb_t * db, uint_fast16_t xpixB, uint_fast16_t 
 		}
 	}
 
+}
+
+// Используется при выводе на графический индикатор с кординатами и размерами в пикселях
+void
+pix_display_text(const gxdrawb_t * db, uint_fast16_t xpix, uint_fast16_t ypix, uint_fast16_t w, uint_fast16_t h, const gxstyle_t * dbstyle, const char * s)
+{
+	pix_display_texts(db, xpix, ypix, w, h, dbstyle, & s, 1);	// одга строка для multi-line
 }
 
 // Используется при выводе на графический индикатор с кординатами и размерами по сетке
