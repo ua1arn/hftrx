aff22c6ae3f97db2b314cd1ae63b952826e47b77
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-11-24 01:57:44 +0300

    dstyle update: более компактное расположение

--- src/dstyles/g_x800_y480.h
+++ src/dstyles/g_x800_y480.h
@@ -6,7 +6,7 @@
 	#define SWRMAX	(SWRMIN * 40 / 10)	// 4.0 - значение на полной шкале (на этом дизайне нет, просто для того чтобы компилировлось)
 
 	enum {
-		DLES = 30,		// spectrum window upper line
+		DLES = 29,		// spectrum window upper line
         DLE1 = 96 - GRID2Y(1) - 1,		// 96-5
 		DLE_unused
 	};
@@ -89,38 +89,34 @@
 		{	0,	4,	15, 20, display2_af_spectre15,		& dzi_compat, PG0, },
 	#endif /* WITHAFSPECTRE */
 
-		{	15,	8,	0, 0, display2_freqX_a_init,	& dzi_compat, PGINI, },	// MAIN FREQ Частота (большие цифры)
-		{	15,	8,	21, 11, display2_freqX_a,	& dzi_compat, PGALL, },	// MAIN FREQ Частота Герцы маленьким шрифтом.
+		{	15,	6,	0, 0, display2_freqX_a_init,	& dzi_compat, PGINI, },	// MAIN FREQ Частота (большие цифры)
+		{	15,	6,	21, 11, display2_freqX_a,	& dzi_compat, PGALL, },	// MAIN FREQ Частота Герцы маленьким шрифтом.
 
-		{	40, 0,	10, 10, display2_fnblock9,	& dzi_compat, PGALL, },	// FUNC menu item label & value
+		{	40, 0,	10, 8, display2_fnblock9,	& dzi_compat, PGALL, },	// FUNC menu item label & value
 
-		{	36, 10,	4, 5, display2_mode3_a,		& dzi_compat,	PGALL, },	// SSB/CW/AM/FM/...
-		{	40, 10,	4, 5, display2_rxbw3,		& dzi_compat, PGALL, },	// 3.1 / 0,5 / WID / NAR
-		{	44, 10,	6, 5, display2_agc3,		& dzi_compat, PGALL, },	// AGC mode
+		{	36, 8,	4, 5, display2_mode3_a,		& dzi_compat,	PGALL, },	// SSB/CW/AM/FM/...
+		{	40, 8,	4, 5, display2_rxbw3,		& dzi_compat, PGALL, },	// 3.1 / 0,5 / WID / NAR
+		{	44, 8,	6, 5, display2_agc3,		& dzi_compat, PGALL, },	// AGC mode
 
-		{	36, 15,	4, 5, display2_nr3,			& dzi_compat, PGALL, },	// NR : was: AGC
-		{	40, 15,	4, 5, display2_datamode3,	& dzi_compat, PGALL, },	// DATA mode indicator
-		{	44, 15,	6, 5, display2_notch5,		& dzi_compat, PGALL, },	// NOTCH on/off
+		{	36, 13,	4, 5, display2_nr3,			& dzi_compat, PGALL, },	// NR : was: AGC
+		{	40, 13,	4, 5, display2_datamode3,	& dzi_compat, PGALL, },	// DATA mode indicator
+		{	44, 13,	6, 5, display2_notch5,		& dzi_compat, PGALL, },	// NOTCH on/off
 
-		{	15, 20,	4, 5, display2_mainsub3,	& dzi_compat, PGALL, },	// main/sub RX: A/A, A/B, B/A, etc
-		{	20,	20,	4, 5, display2_vfomode3,	& dzi_compat, PGALL, },	// SPL
-		{	24,	20,	12, 5, display2_freqX_b,	& dzi_compat, PGALL, },	// SUB FREQ
-		{	36, 20,	4, 5, display2_mode3_b,		& dzi_compat,	PGALL, },	// SSB/CW/AM/FM/...
-		{	40, 20,	4, 5, display2_voxtune3,	& dzi_compat, PGALL, },	// VOX
-		{	44,	20,	6, 5, display2_lockstate4, 	& dzi_compat, PGALL, },	// LOCK
+		{	15, 18,	4, 5, display2_mainsub3,	& dzi_compat, PGALL, },	// main/sub RX: A/A, A/B, B/A, etc
+		{	20,	18,	4, 5, display2_vfomode3,	& dzi_compat, PGALL, },	// SPL
+		{	24,	18,	12, 5, display2_freqX_b,	& dzi_compat, PGALL, },	// SUB FREQ
+		{	36, 18,	4, 5, display2_mode3_b,		& dzi_compat,	PGALL, },	// SSB/CW/AM/FM/...
+		{	40, 18,	4, 5, display2_voxtune3,	& dzi_compat, PGALL, },	// VOX
+		{	44,	18,	6, 5, display2_lockstate4, 	& dzi_compat, PGALL, },	// LOCK
 
 		// размещены под S-метром (15 ячеек)
-		{	0, 	25,	6, 5, display2_voltlevelV, & dzi_compat, PGALL, },	// voltmeter with "V"
-		{	6, 	25,	8, 5, display2_currlevelA, & dzi_compat, PGALL, },	// PA drain current d.dd with "A"
+		{	0, 	23,	6, 5, display2_voltlevelV, & dzi_compat, PGALL, },	// voltmeter with "V"
+		{	6, 	23,	8, 5, display2_currlevelA, & dzi_compat, PGALL, },	// PA drain current d.dd with "A"
 
-		//{	14, 25,
-		//{	19, 25,
-		//{	23, 25,
-		//{	27, 25,
-		{	32, 25,	4, 5, display2_rec3,		& dzi_compat, PGALL, },	// Отображение режима записи аудио фрагмента
-		{	36, 25,	4, 5, display2_spk3,		& dzi_compat, PGALL, },	// отображение признака включения динамика
-		{	40, 25, 4, 5, display2_bkin3,		& dzi_compat, PGALL, },	// BREAK-IN
-		{	44,	25,	6, 5, display2_wpm5, 		& dzi_compat, PGALL, },	// 22WPM
+		{	32, 23,	4, 5, display2_rec3,		& dzi_compat, PGALL, },	// Отображение режима записи аудио фрагмента
+		{	36, 23,	4, 5, display2_spk3,		& dzi_compat, PGALL, },	// отображение признака включения динамика
+		{	40, 23, 4, 5, display2_bkin3,		& dzi_compat, PGALL, },	// BREAK-IN
+		{	44,	23,	6, 5, display2_wpm5, 		& dzi_compat, PGALL, },	// 22WPM
 
 		//{	24, 30,	10, 5, display_freqmeter10, & dzi_compat, PGALL, },	// измеренная частота опоры
 		//{	36, 30,	8, 5, display2_freqdelta8, & dzi_compat, PGALL, },	// выход ЧМ демодулятора
