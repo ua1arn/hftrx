c144a4ffb34fda5b235e5cbff23b86188bd99447
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-12-10 23:40:20 +0300

    fix FPGA fir loading

--- boards/arm_allwt507_cpustyle_veloci_v0.h
+++ boards/arm_allwt507_cpustyle_veloci_v0.h
@@ -968,12 +968,9 @@
 	#define HARDWARE_SPI_FREQ (allwnr_t507_get_spi1_freq())
 
 	#define HARDWARE_SPI1_INITIALIZE() do { \
-		arm_hardware_pioh_altfn2m(SPI_SCLK_BIT, GPIO_CFG_AF4); 	/* PH6 SPI1_CLK */ \
-		arm_hardware_pioh_altfn2m(SPI_MOSI_BIT, GPIO_CFG_AF4); 	/* PH7 SPI1_MOSI */ \
-		arm_hardware_pioh_altfn2m(SPI_MISO_BIT, GPIO_CFG_AF4); 	/* PH8 SPI1_MISO */ \
-		gpioX_prog(GPIOH, SPI_SCLK_BIT, GPIO_CFG_AF4, 1, 0x00); 	/* PH6 SPI1_CLK */ \
-		gpioX_prog(GPIOH, SPI_MOSI_BIT, GPIO_CFG_AF4, 1, 0x00); 	/* PH7 SPI1_MOSI */ \
-		gpioX_prog(GPIOH, SPI_MISO_BIT, GPIO_CFG_AF4, 1, 0x00); 	/* PH8 SPI1_MISO */ \
+		arm_hardware_pioh_altfn20(SPI_SCLK_BIT, GPIO_CFG_AF4); 	/* PH6 SPI1_CLK */ \
+		arm_hardware_pioh_altfn20(SPI_MOSI_BIT, GPIO_CFG_AF4); 	/* PH7 SPI1_MOSI */ \
+		arm_hardware_pioh_altfn20(SPI_MISO_BIT, GPIO_CFG_AF4); 	/* PH8 SPI1_MISO */ \
 	} while (0)
 	#define WITHSPI1HW	1	// Use SPI1
 
