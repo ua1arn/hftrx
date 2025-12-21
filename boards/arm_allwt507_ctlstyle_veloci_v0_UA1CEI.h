da9441a08319019226ddb2e1adf12599f85cf385
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-12-21 17:46:50 +0300

    custopm config update

--- boards/arm_allwt507_ctlstyle_veloci_v0_UA1CEI.h
+++ boards/arm_allwt507_ctlstyle_veloci_v0_UA1CEI.h
@@ -238,11 +238,11 @@
 
 	// Выбор используемой цветовой палитры
 	//#define COLORSTYLE_GREEN	1
-	//#define COLORSTYLE_BLUE	1
-	#define COLORSTYLE_BLUE2 1
+	#define COLORSTYLE_BLUE	1
+	//#define COLORSTYLE_BLUE2 1
 	//#define COLORSTYLE_WHITE	1
 	//#define COLORSTYLE_RED	1	// Цвета а-ля FT-1000
-	//#define WITHGRADIENT_FIXED 1	/* использование массива цветов как базы для создания палитры водопада. */
+	#define WITHGRADIENT_FIXED 1	/* использование массива цветов как базы для создания палитры водопада. */
 
 	#define ENCRES_DEFAULT ENCRES_600
 	#define ENCRES_SUB_DEFAULT ENCRES_600
