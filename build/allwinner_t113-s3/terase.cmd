4882903e50fc5b1cf137e59a671f70dbd8498188
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-12-24 20:41:11 +0300

    allwinner spi nor fsbl backup add

--- build/allwinner_t113-s3/terase.cmd
+++ build/allwinner_t113-s3/terase.cmd
@@ -1,5 +1,7 @@
 @echo Press any key for erase APPLICATION and BOOTLOADER
 @pause
 xfel_fixed_t113-s3 spinor erase 0x00000000 1024
+xfel_fixed_t113-s3 spinor erase 0x00010000 1024
+xfel_fixed_t113-s3 spinor erase 0x00020000 1024
 xfel_fixed_t113-s3 spinor erase 0x00040000 1024
 @pause
--- build/allwinner_t113-s3/tflush.cmd
+++ build/allwinner_t113-s3/tflush.cmd
@@ -1,5 +1,7 @@
 @echo Press any key for write APPLICATION and BOOTLOADER
 @pause
 xfel_fixed_t113-s3 spinor write 0x00000000 fsbl.bt0
+xfel_fixed_t113-s3 spinor write 0x00010000 fsbl.bt0
+xfel_fixed_t113-s3 spinor write 0x00020000 fsbl.bt0
 xfel_fixed_t113-s3 spinor write 0x00040000 tc1_t113s3_app.alw32
 @pause
