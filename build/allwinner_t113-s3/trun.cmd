044c376cfab22fb2d858e46a0d1e0ee2dbed323c
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-01-14 02:25:08 +0300

    ddr init sleep parameter update to 1 S

--- build/allwinner_t113-s3/trun.cmd
+++ build/allwinner_t113-s3/trun.cmd
@@ -1,6 +1,6 @@
 xfel_fixed_t113-s3 write 0x00028000 fsbl_frz_s4.alw32
 xfel_fixed_t113-s3 exec 0x0002ce24
-busybox sleep 0.5s
+busybox sleep 1s
 xfel_fixed_t113-s3 write 0x40000000 tc1_t113s3_app.alw32
 xfel_fixed_t113-s3 exec 0x000291a4
 pause
