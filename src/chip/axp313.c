161fd171b617d08b8a5dafbe3db36d7b69373f3d
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-02-03 18:36:10 +0300

    Add universal AXP init support

--- src/chip/axp313.c
+++ src/chip/axp313.c
@@ -11,8 +11,24 @@
 
 #include "gpio.h"
 #include "formats.h"
+#include <string.h>
 
 //#include "axp313.h"
+
+#define NA 0xff
+
+struct axp_regulator_plat {
+	const char	*name;
+	uint8_t		enable_reg;
+	uint8_t		enable_mask;
+	uint8_t		volt_reg;
+	uint8_t		volt_mask;
+	uint16_t		min_mV;
+	uint16_t		max_mV;
+	uint8_t		step_mV;
+	uint8_t		split;
+	const uint16_t	*table;
+};
 
 #include <errno.h>
 
@@ -463,6 +479,75 @@ int board_orangepi_zero3_axp313_initialize(void)
 //
 //}
 
+// https://github.com/u-boot/u-boot/blob/1de103fc29761fa729dffaa15d0cfb2766be05e4/drivers/power/regulator/axp_regulator.c#L198
+
+/*
+ * Only two level step tuning is implemented for DCDC6, 8, 9
+ * so the voltage below is not support in this driver
+ * DCDC6:   20  (v1.8 - 2.4v), 40 (2.44v - 2.76v)
+ * DCDC8,9: 100 (1.9v - 3.4v)
+ */
+static const struct axp_regulator_plat axp318_regulators[] = {
+	{  "dcdc1", 0x10, BIT(0), 0x12, 0x1f, 1000, 3400, 100, NA },
+	{  "dcdc2", 0x10, BIT(1), 0x13, 0x7f,  500, 1540,  10, 70 },
+	{  "dcdc3", 0x10, BIT(2), 0x14, 0x7f,  500, 1540,  10, 70 },
+	{  "dcdc4", 0x10, BIT(3), 0x15, 0x7f,  500, 1540,  10, 70 },
+	{  "dcdc5", 0x10, BIT(4), 0x16, 0x7f,  500, 1540,  10, 70 },
+	{  "dcdc6", 0x10, BIT(5), 0x17, 0x7f,  500, 1540,  10, 70 },
+	{  "dcdc7", 0x10, BIT(6), 0x18, 0x7f,  500, 1840,  10, 70 },
+	{  "dcdc8", 0x10, BIT(7), 0x19, 0x7f,  500, 1840,  10, 70 },
+	{  "dcdc9", 0x11, BIT(0), 0x1a, 0x7f,  500, 1840,  10, 70 },
+	{  "aldo1", 0x20, BIT(0), 0x24, 0x1f,  500, 3400, 100, NA },
+	{  "aldo2", 0x20, BIT(1), 0x25, 0x1f,  500, 3400, 100, NA },
+	{  "aldo3", 0x20, BIT(2), 0x26, 0x1f,  500, 3400, 100, NA },
+	{  "aldo4", 0x20, BIT(3), 0x27, 0x1f,  500, 3400, 100, NA },
+	{  "aldo5", 0x20, BIT(4), 0x28, 0x1f,  500, 3400, 100, NA },
+	{  "aldo6", 0x20, BIT(5), 0x29, 0x1f,  500, 3400, 100, NA },
+	{  "bldo1", 0x20, BIT(6), 0x2a, 0x1f,  500, 3400, 100, NA },
+	{  "bldo2", 0x20, BIT(7), 0x2b, 0x1f,  500, 3400, 100, NA },
+	{  "bldo3", 0x21, BIT(0), 0x2c, 0x1f,  500, 3400, 100, NA },
+	{  "bldo4", 0x21, BIT(1), 0x2d, 0x1f,  500, 3400, 100, NA },
+	{  "bldo5", 0x21, BIT(2), 0x2e, 0x1f,  500, 3400, 100, NA },
+	{  "cldo1", 0x21, BIT(3), 0x2f, 0x1f,  500, 3400, 100, NA },
+	{  "cldo2", 0x21, BIT(4), 0x30, 0x1f,  500, 3400, 100, NA },
+	{  "cldo3", 0x21, BIT(5), 0x31, 0x1f,  500, 3400, 100, NA },
+	{  "cldo4", 0x21, BIT(6), 0x32, 0x1f,  500, 3400, 100, NA },
+	{  "cldo5", 0x21, BIT(7), 0x33, 0x1f,  500, 3400, 100, NA },
+	{  "dldo1", 0x22, BIT(0), 0x34, 0x1f,  500, 3400, 100, NA },
+	{  "dldo2", 0x22, BIT(1), 0x35, 0x1f,  500, 3400, 100, NA },
+	{  "dldo3", 0x22, BIT(2), 0x36, 0x1f,  500, 3400, 100, NA },
+	{  "dldo4", 0x22, BIT(3), 0x37, 0x1f,  500, 3400, 100, NA },
+	{  "dldo5", 0x22, BIT(4), 0x38, 0x1f,  500, 3400, 100, NA },
+	{  "dldo6", 0x22, BIT(5), 0x39, 0x1f,  500, 3400, 100, NA },
+	{  "eldo1", 0x22, BIT(6), 0x3a, 0x1f,  500, 1500,  25, NA },
+	{  "eldo2", 0x22, BIT(7), 0x3b, 0x1f,  500, 1500,  25, NA },
+	{  "eldo3", 0x23, BIT(0), 0x3c, 0x1f,  500, 1500,  25, NA },
+	{  "eldo4", 0x23, BIT(1), 0x3d, 0x1f,  500, 1500,  25, NA },
+	{  "eldo5", 0x23, BIT(2), 0x3e, 0x1f,  500, 1500,  25, NA },
+	{  "eldo6", 0x23, BIT(3), 0x3f, 0x1f,  500, 1500,  25, NA },
+	{ "swout1", 0x11, BIT(3),   NA,   NA,   NA,   NA,  NA, NA },
+	{ "swout2", 0x11, BIT(4),   NA,   NA,   NA,   NA,  NA, NA },
+	{ }
+};
+
+static int axpXXX_set(const struct axp_regulator_plat * regs, const char * name, unsigned voltage)
+{
+	for (;; ++ regs)
+	{
+		if (regs->name == NULL)
+			return - 1;
+		if (strcmp(name, regs->name))
+			continue;
+		PRINTF("Found %s for set %u mV\n", name, voltage);
+	}
+	PRINTF("axpXXX_set: %s not found\n", name);
+	return - 1;
+}
+static int axp318w_set(const char * name, unsigned voltage)
+{
+	return axpXXX_set(axp318_regulators, name, voltage);
+}
+
 int board_radaxa_cubie_axp318w_initialize(void)
 {
 
@@ -474,13 +559,6 @@ int board_radaxa_cubie_axp318w_initialize(void)
 	if (ret)
 	{
 		PRINTF("pmic_bus_init() failure.\n");
-			dbg_flush();
-		return ret;
-	}
-	ret = pmic_bus_read(AXP313_CHIP_VERSION, &axp318_chip_id);
-	if (ret)
-	{
-		PRINTF("pmic_bus_read() failure.\n");
 			dbg_flush();
 		return ret;
 	}
@@ -497,13 +575,20 @@ int board_radaxa_cubie_axp318w_initialize(void)
 		PRINTF("axp318 registers (count = 0x%02X):\n", reg);
 		printhex(0, registers, reg);
 	}
+//	ret = pmic_bus_read(AXP313_CHIP_VERSION, &axp318_chip_id);
+//	if (ret)
+//	{
+//		PRINTF("pmic_bus_read() failure.\n");
+//			dbg_flush();
+//		return ret;
+//	}
 
-	PRINTF("axp318_chip_id=0x%02X (expected 0x%02X)\n", axp318_chip_id, 0x4B);
-
-	dbg_flush();
-	if (!(axp318_chip_id == 0x4B))
-		return -1;
-    PRINTF("axp318_chip_id=OK\n");
+//	PRINTF("axp318_chip_id=0x%02X (expected 0x%02X)\n", axp318_chip_id, 0x4B);
+//
+//	dbg_flush();
+//	if (!(axp318_chip_id == 0x4B))
+//		return -1;
+//    PRINTF("axp318_chip_id=OK\n");
 
     // Radaxa Cubie A7Z voltages:
 	//	SWOUT1 VCC33-LDO 3.3V with ON/OFF
@@ -529,28 +614,28 @@ int board_radaxa_cubie_axp318w_initialize(void)
 	//	DLDO6 VCC-UFS 2500 mV
 	//	ELDO6 VDD-CPUS, VDD-USB 800 mV
 	//	RTCLDO-PMU VCC-RTC
-#if 0
-    axp318w_set_dcdc2(800); // VDD-SYS, VDD-DRAM, VDD-VE, ... 800 mV
-    axp318w_set_dcdc3(900); // VDD-CPUB 0.8-1V
-    axp318w_set_dcdc4(900); // VDD-GPU 0.8V-0.96V
-    axp318w_set_dcdc5(900); // VDD-CPUL 0.8V-1V
-    axp318w_set_dcdc6(800); // VCC-DRAML, VDD-VDDQ [see options] 0.8/0.6/0.5V
-    axp318w_set_dcdc7(1100); // VDD-DRAM, VCC-VDDQ [seee options] 1.1/1.05V
-    axp318w_set_dcdc8(1200); // VCC-UFS-IO,, VCC12-UFS, VCC18-UFS 0.8/1.2/1.8V
-    axp318w_set_dcdc9(1200); // ELDO-INPUT 1.25/1.1V
-    axp318w_set_aldo1(3300); // VCC-PL, VCC22-18-USB, VCC33-USB, VCC-3V3_TYPEC 3300 mV
-    axp318w_set_bldo1(1800); // VCC-PE, VCC-PK, VCC-MCSI 1800 mV
-    axp318w_set_bldo2(1800); // VCC-PD, VCC-LVDS0, VCC-PJ, VCC18-LCS 1800 mV
-    axp318w_set_bldo4(1800); // VCC18-CODEC 1800 mV
-    axp318w_set_bldo5(1800); // VCC-PG 1800 mV
-    axp318w_set_cldo1(1800); // VCC-PM, ... 1800 mV
-    axp318w_set_cldo2(1800); // VCC18-HDMI 1800 mV
-    axp318w_set_cldo3(1800); // ... 1800 mV
-    axp318w_set_cldo5(1800); // ... 1800 mV
-    axp318w_set_dldo1(1800); // VCC-EFUSE 1800 mV
-    axp318w_set_dldo6(2500); // VCC-UFS 2500 mV
-    axp318w_set_eldo6(800); // VDD-CPUS, VDD-USB 800 mV
-#endif
+
+    axp318w_set("dcdc2", 800); // VDD-SYS, VDD-DRAM, VDD-VE, ... 800 mV
+    axp318w_set("dcdc3", 900); // VDD-CPUB 0.8-1V
+    axp318w_set("dcdc4", 900); // VDD-GPU 0.8V-0.96V
+    axp318w_set("dcdc5", 900); // VDD-CPUL 0.8V-1V
+    axp318w_set("dcdc6", 800); // VCC-DRAML, VDD-VDDQ [see options] 0.8/0.6/0.5V
+    axp318w_set("dcdc7", 1100); // VDD-DRAM, VCC-VDDQ [seee options] 1.1/1.05V
+    axp318w_set("dcdc8", 1200); // VCC-UFS-IO,, VCC12-UFS, VCC18-UFS 0.8/1.2/1.8V
+    axp318w_set("dcdc9", 1200); // ELDO-INPUT 1.25/1.1V
+    axp318w_set("aldo1", 3300); // VCC-PL, VCC22-18-USB, VCC33-USB, VCC-3V3_TYPEC 3300 mV
+    axp318w_set("bldo1", 1800); // VCC-PE, VCC-PK, VCC-MCSI 1800 mV
+    axp318w_set("bldo2", 1800); // VCC-PD, VCC-LVDS0, VCC-PJ, VCC18-LCS 1800 mV
+    axp318w_set("bldo4", 1800); // VCC18-CODEC 1800 mV
+    axp318w_set("bldo5", 1800); // VCC-PG 1800 mV
+    axp318w_set("cldo1", 1800); // VCC-PM, ... 1800 mV
+    axp318w_set("cldo2", 1800); // VCC18-HDMI 1800 mV
+    axp318w_set("cldo3", 1800); // ... 1800 mV
+    axp318w_set("cldo5", 1800); // ... 1800 mV
+    axp318w_set("dldo1", 1800); // VCC-EFUSE 1800 mV
+    axp318w_set("dldo6", 2500); // VCC-UFS 2500 mV
+    axp318w_set("eldo6", 800); // VDD-CPUS, VDD-USB 800 mV
+
 	PRINTF("axp318 INIT END\n");
 	dbg_flush();
 	return 0;
