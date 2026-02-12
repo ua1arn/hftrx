951d1615acd1ce85b69d77cf97ef81d6e5072204
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-02-12 16:34:15 +0300

    a733: LPDDR5 работает

--- src/sdram/aw_a733/dram_glue.c
+++ src/sdram/aw_a733/dram_glue.c
@@ -9,14 +9,15 @@
 
 #include "hardware.h"
 
-#if WITHSDRAMHW && (CPUSTYLE_A733)
+#if WITHSDRAMHW && (CONFIG_SUNXI_DRAM_A733_LPDDR5 || CONFIG_SUNXI_DRAM_A733_LPDDR4)
 
 #include "formats.h"
 #include "clocks.h"
 #include <string.h>
 
+#if CONFIG_SUNXI_DRAM_A733_LPDDR5
 uint32_t dram_para [128] = {
-		2400,	   // dram_clk
+		CONFIG_DRAM_CLK, //2400,	   // dram_clk
 		9,		   // dram_type
 		0x0e0e0e0e,// dram_dx_odt
 		0x0f0f0f0f,// dram_dx_dri
@@ -49,7 +50,45 @@ uint32_t dram_para [128] = {
 		0x10061,   // dram_tpr13
 		0		   // dram_tpr14
 };
+#endif /* CONFIG_SUNXI_DRAM_A733_LPDDR5 */
 
+
+#if CONFIG_SUNXI_DRAM_A733_LPDDR4
+uint32_t dram_para [128] = {
+		CONFIG_DRAM_CLK, //800,	   // dram_clk
+		8,		   // dram_type
+		0x0e0e0e0e,// dram_dx_odt
+		0x0f0f0f0f,// dram_dx_dri
+		0xec030e0f,// dram_ca_dri
+		0,		   // dram_para0
+		0xa10a,	   // dram_para1
+		0x1001,	   // dram_para2
+		0,		   // dram_mr0
+		0,		   // dram_mr1
+		0,		   // dram_mr2
+		0x6,	   // dram_mr3
+		0,		   // dram_mr4
+		0,		   // dram_mr5
+		0,		   // dram_mr6
+		0x12,	   // dram_mr11
+		0x44,	   // dram_mr12
+		0,		   // dram_mr13
+		0x34,	   // dram_mr14
+		0,		   // dram_mr16
+		0x06,	   // dram_mr17
+		0,		   // dram_mr22
+		0x4040,	   // dram_tpr0
+		0,		   // dram_tpr1
+		0x170b070, // dram_tpr2
+		0x3800,	   // dram_tpr3
+		0x3514,	   // dram_tpr6
+		0x325f0000,// dram_tpr10
+		0,		   // dram_tpr11
+		0,		   // dram_tpr12
+		0x10061,   // dram_tpr13
+		0		   // dram_tpr14
+};
+#endif /* CONFIG_SUNXI_DRAM_A733_LPDDR4 */
 
 void sunxi_smc_en_with_glitch_workaround(void) {
 	return;
@@ -91,12 +130,11 @@ void arm_hardware_sdram_initialize(void)
 
 	//sunxi_dram_init(NULL);
 
-	PRINTF("arm_hardware_sdram_initialize!\n");
-	int ec;
-	ec = init_DRAM(0, dram_para);
-	//PRINTF("arm_hardware_sdram_initialize: result=%u MB\n", memsizeMB);
-	PRINTF("arm_hardware_sdram_initialize: result=%d\n", ec);
-	PRINTF("arm_hardware_sdram_initialize!\n");
+	PRINTF("arm_hardware_sdram_initialize start\n");
+	int memsizeMB;
+	memsizeMB = init_DRAM(0, dram_para);
+	PRINTF("arm_hardware_sdram_initialize: result=%d MB\n", memsizeMB);
+	PRINTF("arm_hardware_sdram_initialize done\n");
 }
 
 #endif
