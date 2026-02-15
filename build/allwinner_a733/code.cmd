6d94c859e0d36a7c0377da993ecfb0746b243643
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-02-15 15:18:20 +0300

    Командный файл для получение списка пердопределенный макросов gcc

--- build/allwinner_a733/code.cmd
+++ build/allwinner_a733/code.cmd
@@ -1,3 +1,8 @@
 arm-none-eabi-gcc -Ofast -mcpu=cortex-a55 -mfloat-abi=hard -mfpu=neon-vfpv4 --specs=nosys.specs -I "../../CMSIS_6/CMSIS/Core/Include" -I "../../CMSIS_6/CMSIS/Core/Include/A-PROFILE"  -D"NDEBUG"=1 -D"CPUSTYLE_A733"=1 -c code.c
 arm-none-eabi-objdump -d code.o >code.txt
 
+arm-none-eabi-gcc -o defs_a55_32bit.txt -Ofast -mcpu=cortex-a55 -mfloat-abi=hard -mfpu=neon-vfpv4 --specs=nosys.specs -dM -E - < NUL
+aarch64-none-elf-gcc -o defs_a55_64bit.txt -Ofast -mcpu=cortex-a55+fp+simd --specs=nosys.specs -dM -E - < NUL
+
+arm-none-eabi-gcc -o defs_a53_32bit.txt -Ofast -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-vfpv4 --specs=nosys.specs -dM -E - < NUL
+aarch64-none-elf-gcc -o defs_a53_64bit.txt -Ofast -mcpu=cortex-a53+fp+simd --specs=nosys.specs -dM -E - < NUL
