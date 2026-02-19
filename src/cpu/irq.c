9b3d83934410769a007d3f70b06be8e0f02bfc78
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-02-19 18:11:19 +0300

    comments

--- src/cpu/irq.c
+++ src/cpu/irq.c
@@ -1777,6 +1777,48 @@ enum aarch64_frame_offsets
 #define EXC_EXC_ELR_OFFSET (24 / 8)	/* __asm_offsetof(struct _exception_frame, exc_elr) */
 #define EXC_EXC_SPSR_OFFSET (32 / 8)/* __asm_offsetof(struct _exception_frame, exc_spsr) */
 
+typedef struct exception_frame_tag
+{
+	uint64_t exc_type;
+	uint64_t exc_esr;
+	uint64_t exc_sp;
+	uint64_t exc_elr;
+	uint64_t exc_spsr;
+	uint64_t x0;
+	uint64_t vfpstate [2];	// FPCR, FPEXC32_EL2 (check order)
+	uint64_t vfpregs [64];	// 32 128-bit registers
+	uint64_t x1;
+	uint64_t x2;
+	uint64_t x3;
+	uint64_t x4;
+	uint64_t x5;
+	uint64_t x6;
+	uint64_t x7;
+	uint64_t x8;
+	uint64_t x9;
+	uint64_t x10;
+	uint64_t x11;
+	uint64_t x12;
+	uint64_t x13;
+	uint64_t x14;
+	uint64_t x15;
+	uint64_t x16;
+	uint64_t x17;
+	uint64_t x18;
+	uint64_t x19;
+	uint64_t x20;
+	uint64_t x21;
+	uint64_t x22;
+	uint64_t x23;
+	uint64_t x24;
+	uint64_t x25;
+	uint64_t x26;
+	uint64_t x27;
+	uint64_t x28;
+	uint64_t x29;
+	uint64_t x30;
+} exception_frame_t;
+
 #else
 
 #endif
