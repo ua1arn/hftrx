15c38e95337a00538ede814eb3647d9b000367c9
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-02-26 13:01:25 +0300

    save state

--- arch/aw_a733/allwnr_a733.h
+++ arch/aw_a733/allwnr_a733.h
@@ -32,6 +32,7 @@
 #define __GIC_PRESENT                 1U      /*!< Set to 1 if GIC is present                  */
 #define __TIM_PRESENT                 1U      /*!< Set to 1 if TIM is present                  */
 #define __L2C_PRESENT                 0U      /*!< Set to 1 if L2C is present                  */
+#define __DSP_PRESENT         		  1U       /* Set to 1 if DSP extension are present */
 
 #if __aarch64__
 
