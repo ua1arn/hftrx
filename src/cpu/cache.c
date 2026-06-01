7e7f048f1b23664a226fd88c0c66051d9e8a8bd1
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-06-01 21:25:05 +0300

    fix cache inv

--- src/cpu/cache.c
+++ src/cpu/cache.c
@@ -408,8 +408,15 @@ void L2_InvalidateDCache_by_Addr(void *__restrict addr, int32_t dsize)
 // применяется после начальной инициализации среды выполнния
 void dcache_clean_all(void)
 {
+#if __aarch64__
 	extern uint32_t Reset_Handler, __stack;
 	dcache_clean_invalidate((uintptr_t) & Reset_Handler, (uintptr_t) & __stack - (uintptr_t) & Reset_Handler);
+#else
+    L1C_CleanInvalidateDCacheAll();
+#if (__L2C_PRESENT == 1)
+    L2C_CleanInvAllByWay();
+#endif
+#endif
 }
 
 // Сейчас в эту память будем читать по DMA
