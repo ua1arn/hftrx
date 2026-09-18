33247f40c3212ff2a017598df018b1eb5417bd50
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-09-18 09:48:27 +0300

    bootloader build fix

--- inc/audio.h
+++ inc/audio.h
@@ -674,11 +674,14 @@ void hamradio_set_geqrx(uint_fast8_t v);
 
 void audio_rx_equalizer(FLOAT_t *buffer, uint_fast16_t size);
 
+#if WITHIF4DSP
+
 void modem_init(void);
 void modem_test(void);
 void modem_fill(IFADCvalue_t * buff);
 void modem_parse(const IFADCvalue_t * buff);
 FLOAT32P_t xget_float_monofreq(void);	// modem test LO quadratures
+#endif /* WITHIF4DSP */
 
 //////////////////////////
 /// IIR
