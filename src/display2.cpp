a83f6ae74fb3ea0f67331a95f5cb0011adf38ee7
Genadi V. Zawidowski <mgs2001@mail.ru>
2026-09-24 08:57:50 +0300

    style

--- src/display2.cpp
+++ src/display2.cpp
@@ -7014,7 +7014,6 @@ display2_wfl_init(
 		for (i = 0; i < PALETTESIZE; ++ i)
 		{
 			const COLOR24_T c = colorgradient(i, PALETTESIZE - 1);
-			//const COLOR24_T c = GetSignalColor(i, PALETTESIZE - 1);
 			wfpalette [i] = TFTRGB(COLOR24_R(c), COLOR24_G(c), COLOR24_B(c));
 		}
 	}
